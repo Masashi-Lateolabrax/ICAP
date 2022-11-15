bdc2a003b4964e6f4809ec7ff605b272902b6aae
Masashi <m2153100220@std.kitami-it.ac.jp>
2022-11-15 13:59:02 +0900

    全方位センサの計算式を非破壊的に変更しました．

--- environments/sensor.py
+++ environments/sensor.py
@@ -2,18 +2,28 @@ import numpy
 
 
 class OmniSensor:
-    def __init__(self, center, rot_mat, one_third_point: float, eps: float = 1.0):
-        self._one_third_point = 1.0 / one_third_point
+    def __init__(self, center, rot_mat, one_third_point: float, min_value: float = 1.0):
+        if min_value <= 0:
+            raise ValueError(f"'min_value' must be grater than 0 but it is {min_value}.")
+        if one_third_point <= 0:
+            raise ValueError(f"'one_third_point' must be grater than 0 but it is {one_third_point}.")
+
+        self._a = 2.0 / one_third_point
+        self._min = min_value
+
         self._center = center
         self._inv_rot_mat = numpy.linalg.inv(rot_mat)
-        self._eps = 1.0 / eps
+
         self.value = numpy.zeros(2)
 
     def reset(self):
         self.value[:] = 0
 
     def sense(self, pos):
-        ref_pos = numpy.dot(self._inv_rot_mat, pos - self._center)[:2]
-        dist = numpy.linalg.norm(ref_pos, ord=2)
-        if dist != 0:
-            self.value += ref_pos * self._eps / (2 * self._one_third_point * dist * dist + dist)
+        ref_pos = numpy.dot(self._inv_rot_mat, pos - self._center)
+        d = numpy.linalg.norm(ref_pos, ord=2)
+        if d != 0:
+            ref_direction = ref_pos[:2] / numpy.linalg.norm(ref_pos[:2], ord=2)
+            self.value += self._min / (self._a * d + 1.0) * ref_direction
+        else:
+            self.value += self._min
