43bbfe3c545ad31625f580b6c8810d6adc6ec95c
Masashi <m2153100220@std.kitami-it.ac.jp>
2022-11-21 02:27:30 +0900

    採餌行動用の環境を作りました．

---
+++ environments/collect_feed.py
@@ -0,0 +1,495 @@
+import mujoco
+import numpy
+
+from studyLib import optimizer, wrap_mjc, miscellaneous, nn_tools
+from studyLib.optimizer import EnvInterface, MuJoCoEnvInterface
+
+
+def _gen_env(
+        nest_pos: (float, float),
+        robot_pos: list[(float, float)],
+        obstacle_pos: list[(float, float)],
+        feed_pos: list[(float, float)],
+):
+    generator = wrap_mjc.MuJoCoXMLGenerator("co-behavior")
+
+    generator.add_option({"timestep": 0.03333})
+    generator.add_asset().add_texture({
+        "type": "skybox",
+        "builtin": "gradient",
+        "rgb1": "0.3 0.5 0.7",
+        "rgb2": "0 0 0",
+        "width": "512",
+        "height": "512",
+    })
+
+    worldbody = generator.get_body()
+    act = generator.add_actuator()
+
+    # Create Ground
+    worldbody.add_geom({
+        "type": "plane",
+        "size": "0 0 0.05",
+        "rgba": "1.0 1.0 1.0 0.5",
+        "condim": "1",
+        "priority": "0"
+    })
+
+    # Create Nest
+    worldbody.add_geom({
+        "type": "cylinder",
+        "pos": f"{nest_pos[0]} {nest_pos[1]} -3",
+        "size": "50 1",
+        "rgba": "0.0 1.0 0.0 1",
+    })
+
+    # Create Obstacles
+    for i, op in enumerate(obstacle_pos):
+        worldbody.add_geom({
+            "name": f"obstacle{i}",
+            "type": "cylinder",
+            "size": "50 10",
+            "rgba": "1 0 0 1",
+            "pos": f"{op[0]} {op[1]} 20"
+        })
+
+    # Create Feeds
+    for i, fp in enumerate(feed_pos):
+        feed_body = worldbody.add_body({
+            "name": f"feed{i}",
+            "pos": f"{fp[0]} {fp[1]} 20"
+        })
+        feed_body.add_freejoint()
+        feed_body.add_geom({
+            "type": "cylinder",
+            "size": "100 10",
+            "mass": "260000",
+            "rgba": "0 1 1 1",
+            "condim": "3",
+            "priority": "1",
+            "friction": "0.5 0.0 0.0",
+            "solimp": "0.9 0.95 0.001 0.5 2",
+            "solref": "0.5 1"
+        })
+
+    # Create Robots
+    for i, rp in enumerate(robot_pos):
+        robot_body = worldbody.add_body({
+            "name": f"robot{i}",
+            "pos": f"{rp[0]} {rp[1]} 6",
+            "axisangle": f"0 0 1 0",
+        })
+        robot_body.add_freejoint()
+        robot_body.add_geom({
+            "type": "cylinder",
+            "size": "17.5 5",  # 幅35cm，高さ10cm
+            "density": "7.874",  # 鉄の密度(7.874 g/cm^3)
+            "rgba": "1 1 0 0.3",
+            "condim": "1",
+            "solimp": "0.9 0.95 0.001 0.5 2",
+            "solref": "0.3 1"
+        })
+
+        right_wheel_body = robot_body.add_body({"pos": "-10 0 -0.5"})
+        right_wheel_body.add_joint({"name": f"joint_robot{i}_right", "type": "hinge", "axis": "-1 0 0"})
+        right_wheel_body.add_geom({
+            "type": "cylinder",
+            "size": "5 5",
+            "density": "1",  # 水の密度(1 g/cm^3)
+            "axisangle": "0 1 0 90",
+            "condim": "6",
+            "priority": "1",
+            "solimp": "0.9 0.95 0.001 0.5 2",
+            "solref": "0.3 1"
+        })
+
+        left_wheel_body = robot_body.add_body({"pos": "10 0 -0.5"})
+        left_wheel_body.add_joint({"name": f"joint_robot{i}_left", "type": "hinge", "axis": "-1 0 0"})
+        left_wheel_body.add_geom({
+            "type": "cylinder",
+            "size": "5 5",
+            "density": "1",
+            "axisangle": "0 1 0 90",
+            "condim": "6",
+            "priority": "1",
+            "solimp": "0.9 0.95 0.001 0.5 2",
+            "solref": "0.3 1"
+        })
+
+        front_wheel_body = robot_body.add_body({"pos": "0 15 -4.0"})
+        front_wheel_body.add_joint({"type": "ball"})
+        front_wheel_body.add_geom({
+            "type": "sphere",
+            "size": "1.5",
+            "density": "1",
+            "condim": "1",
+            "solimp": "0.9 0.95 0.001 0.5 2",
+            "solref": "0.3 1"
+        })
+
+        rear_wheel_body = robot_body.add_body({"pos": "0 -15 -4.0"})
+        rear_wheel_body.add_joint({"type": "ball"})
+        rear_wheel_body.add_geom({
+            "type": "sphere",
+            "size": "1.5",
+            "density": "1",
+            "condim": "1",
+            "solimp": "0.9 0.95 0.001 0.5 2",
+            "solref": "0.3 1"
+        })
+
+        act.add_velocity({
+            "name": f"act_robot{i}_left",
+            "joint": f"joint_robot{i}_left",
+            "gear": "80",
+            "ctrllimited": "true",
+            "ctrlrange": "-50000 50000",
+            "kv": "1"
+        })
+        act.add_velocity({
+            "name": f"act_robot{i}_right",
+            "joint": f"joint_robot{i}_right",
+            "gear": "80",
+            "ctrllimited": "true",
+            "ctrlrange": "-50000 50000",
+            "kv": "1"
+        })
+
+    return generator.generate()
+
+
+class _Obstacle:
+    def __init__(self, model: wrap_mjc.WrappedModel, number: int):
+        self._pos = model.get_geom(f"obstacle{number}").get_pos()
+
+    def get_pos(self):
+        return self._pos.copy()
+
+
+class _Feed:
+    def __init__(self, model: wrap_mjc.WrappedModel, number: int):
+        self._body = model.get_body(f"feed{number}")
+
+    def get_pos(self):
+        return self._body.get_xpos().copy()
+
+
+class _RobotBrain:
+    def __init__(self, para):
+        self._calculator = nn_tools.Calculator(9)
+
+        self._calculator.add_layer(nn_tools.AffineLayer(18))
+        self._calculator.add_layer(nn_tools.TanhLayer(18))
+
+        self._calculator.add_layer(nn_tools.AffineLayer(7))
+        self._calculator.add_layer(nn_tools.TanhLayer(7))
+
+        self._calculator.add_layer(nn_tools.AffineLayer(3))
+        self._calculator.add_layer(nn_tools.SigmoidLayer(3))
+
+        if not (para is None):
+            self._calculator.load(para)
+
+    def num_dim(self):
+        return self._calculator.num_dim()
+
+    def calc(self, array):
+        return self._calculator.calc(array)
+
+
+class _Robot:
+    def __init__(self, brain: _RobotBrain, model: wrap_mjc.WrappedModel, number: int):
+        self._brain = brain
+        self._body = model.get_body(f"robot{number}")
+        self._left_act = model.get_act(f"act_robot{number}_left")
+        self._right_act = model.get_act(f"act_robot{number}_right")
+        self._accustomed = 0.0
+        self._accustomed_rate = 0.2
+
+    def get_pos(self) -> numpy.ndarray:
+        return self._body.get_xpos().copy()
+
+    def get_orientation(self) -> numpy.ndarray:
+        rot_mat = numpy.zeros(9).reshape(3, 3)
+        mujoco.mju_quat2Mat(rot_mat.ravel(), self._body.get_xquat())
+        return rot_mat
+
+    def get_direction(self) -> numpy.ndarray:
+        return numpy.dot(self.get_orientation(), [0.0, 1.0, 0.0])
+
+    def act(
+            self,
+            pheromone_value: float,
+            nest_pos: numpy.ndarray,
+            robot_pos: list[numpy.ndarray],
+            obstacle_pos: list[numpy.ndarray],
+            feed_pos: list[numpy.ndarray]
+    ):
+        from environments import sensor
+
+        pos = self.get_pos()
+        mat = self.get_orientation()
+
+        ref_nest_pos = (nest_pos - pos)[:2]
+        nest_dist = numpy.linalg.norm(ref_nest_pos, ord=2)
+        if nest_dist > 0.0:
+            nest_direction = ref_nest_pos / nest_dist
+        else:
+            nest_direction = numpy.zeros(2)
+
+        rs = sensor.OmniSensor(pos, mat, 100)
+        for rp in robot_pos:
+            rs.sense(rp)
+
+        os = sensor.OmniSensor(pos, mat, 100)
+        for op in obstacle_pos:
+            os.sense(op)
+
+        fs = sensor.OmniSensor(pos, mat, 100)
+        for fp in feed_pos:
+            fs.sense(fp)
+
+        input_ = numpy.concatenate(
+            [nest_direction, rs.value, os.value, fs.value, [pheromone_value - self._accustomed]]
+        )
+        ctrl = self._brain.calc(input_)
+
+        self._accustomed = (1.0 - self._accustomed_rate) * self._accustomed + self._accustomed_rate * pheromone_value
+
+        self._left_act.ctrl = 30000 * ctrl[0]
+        self._right_act.ctrl = 30000 * ctrl[1]
+        return 30 * ctrl[2]
+
+
+def _evaluate(
+        brain: _RobotBrain,
+        nest_pos: (float, float),
+        robot_pos: list[(float, float)],
+        obstacle_pos: list[(float, float)],
+        feed_pos: list[(float, float)],
+        sv: float,
+        evaporate: float,
+        diffusion: float,
+        decrease: float,
+        timestep: int,
+        camera: wrap_mjc.Camera = None,
+        window: miscellaneous.Window = None
+) -> float:
+    from environments import pheromone
+
+    xml = _gen_env(nest_pos, robot_pos, obstacle_pos, feed_pos)
+    model = wrap_mjc.WrappedModel(xml)
+
+    if camera is not None:
+        model.set_camera(camera)
+
+    if window is None:
+        pheromone_field = pheromone.PheromoneField(0, 350, 10, 1, 80, 100, sv, evaporate, diffusion, decrease, None)
+    else:
+        pheromone_field = pheromone.PheromoneField(0, 350, 10, 1, 80, 100, sv, evaporate, diffusion, decrease, model)
+
+    robots = [_Robot(brain, model, i) for i in range(0, len(robot_pos))]
+    obstacles = [_Obstacle(model, i) for i in range(0, len(obstacle_pos))]
+    feeds = [_Feed(model, i) for i in range(0, len(feed_pos))]
+    nest_pos = numpy.array([nest_pos[0], nest_pos[1], 0])
+    obstacle_pos = [o.get_pos() for o in obstacles]
+
+    for _ in range(0, 30):
+        model.step()
+
+    loss = 0.0
+    for t in range(0, timestep):
+        # Simulate
+        robot_pos = [r.get_pos() for r in robots]
+        feed_pos = [f.get_pos() for f in feeds]
+        for r, rp in zip(robots, robot_pos):
+            pheromone_value = pheromone_field.get_gas(rp[0], rp[1])
+            secretion = r.act(pheromone_value, nest_pos, robot_pos, obstacle_pos, feed_pos)
+            pheromone_field.add_liquid(rp[0], rp[1], secretion)
+
+        model.step()
+        for _ in range(5):
+            pheromone_field.update_cells(0.03333 / 5)
+
+        # Render MuJoCo Scene
+        if window is not None:
+            if not window.render(model, camera):
+                exit()
+            pheromone_field.update_panels()
+            model.draw_text(f"{loss}", 0, 0, (0, 0, 0))
+            window.flush()
+
+        # Calculate loss
+        loss_dt = 0.0
+        for fp in feed_pos:
+            min_dist = float("inf")
+            for rp in robot_pos:
+                d = numpy.linalg.norm(fp - rp, ord=2)
+                if d < min_dist:
+                    min_dist = d
+            loss_dt += 0.001 * min_dist + numpy.linalg.norm(fp, ord=2)
+        loss += loss_dt
+
+    return loss
+
+
+class Environment(optimizer.MuJoCoEnvInterface):
+    def __init__(
+            self,
+            nest_pos: (float, float),
+            robot_pos: list[(float, float)],
+            obstacle_pos: list[(float, float)],
+            feed_pos: list[(float, float)],
+            sv: float,
+            evaporate: float,
+            diffusion: float,
+            decrease: float,
+            timestep: int
+    ):
+        self.nest_pos: (float, float) = nest_pos
+        self.robot_pos: list[(float, float)] = robot_pos
+        self.obstacle_pos: list[(float, float)] = obstacle_pos
+        self.feed_pos: list[(float, float)] = feed_pos
+        self.sv: float = sv
+        self.evaporate: float = evaporate
+        self.diffusion: float = diffusion
+        self.decrease: float = decrease
+        self.timestep: int = timestep
+
+    def calc(self, para) -> float:
+        brain = _RobotBrain(para)
+        return _evaluate(
+            brain,
+            self.nest_pos,
+            self.robot_pos,
+            self.obstacle_pos,
+            self.feed_pos,
+            self.sv,
+            self.evaporate,
+            self.diffusion,
+            self.decrease,
+            self.timestep,
+            None,
+            None
+        )
+
+    def calc_and_show(self, para, window: miscellaneous.Window, camera: wrap_mjc.Camera) -> float:
+        brain = _RobotBrain(para)
+        return _evaluate(
+            brain,
+            self.nest_pos,
+            self.robot_pos,
+            self.obstacle_pos,
+            self.feed_pos,
+            self.sv,
+            self.evaporate,
+            self.diffusion,
+            self.decrease,
+            self.timestep,
+            camera,
+            window
+        )
+
+
+class EnvCreator(optimizer.MuJoCoEnvCreator):
+    def __init__(self):
+        self.nest_pos: (float, float) = (0, 0)
+        self.robot_pos: list[(float, float)] = [(0, 0)]
+        self.obstacle_pos: list[(float, float)] = [(0, 0)]
+        self.feed_pos: list[(float, float)] = [(0, 0)]
+        self.sv: float = 0.0
+        self.evaporate: float = 0.0
+        self.diffusion: float = 0.0
+        self.decrease: float = 0.0
+        self.timestep: int = 100
+
+    def save(self):
+        import struct
+
+        packed = [struct.pack("<dd", self.nest_pos[0], self.nest_pos[1])]
+
+        packed.extend([struct.pack("<I", len(self.robot_pos))])
+        packed.extend([struct.pack("<dd", p[0], p[1]) for p in self.robot_pos])
+
+        packed.extend([struct.pack("<I", len(self.obstacle_pos))])
+        packed.extend([struct.pack("<dd", p[0], p[1]) for p in self.obstacle_pos])
+
+        packed.extend([struct.pack("<I", len(self.feed_pos))])
+        packed.extend([struct.pack("<dd", p[0], p[1]) for p in self.feed_pos])
+
+        packed.extend([struct.pack("<ddddI", self.sv, self.evaporate, self.diffusion, self.decrease, self.timestep)])
+
+        return b"".join(packed)
+
+    def load(self, data: bytes, offset: int = 0) -> int:
+        import struct
+
+        # 巣の座標
+        s = offset
+        e = 16
+        self.nest_pos = struct.unpack("<dd", data[s:e])[0:2]
+
+        # ロボットの座標
+        s = e
+        e = s + 4
+        num = struct.unpack("<I", data[s:e])[0]
+        for _ in range(0, num):
+            s = e
+            e = s + 16
+            rp = struct.unpack("<dd", data[s:e])[0:2]
+            self.robot_pos.append(rp)
+
+        # 障害物の座標
+        s = e
+        e = s + 4
+        num = struct.unpack("<I", data[s:e])[0]
+        for _ in range(0, num):
+            s = e
+            e = s + 16
+            op = struct.unpack("<dd", data[s:e])[0:2]
+            self.obstacle_pos.append(op)
+
+        # 餌の座標
+        s = e
+        e = s + 4
+        num = struct.unpack("<I", data[s:e])[0]
+        for _ in range(0, num):
+            s = e
+            e = s + 16
+            fp = struct.unpack("<dd", data[s:e])[0:2]
+            self.obstacle_pos.append(fp)
+
+        s = e
+        e = s + 8 + 8 + 8 + 8 + 4
+        self.sv, self.evaporate, self.diffusion, self.decrease, self.timestep = struct.unpack("<ddddI", data[s:e])[0:5]
+
+        return e - offset
+
+    def dim(self) -> int:
+        return _RobotBrain(None).num_dim()
+
+    def create(self) -> EnvInterface:
+        return Environment(
+            self.nest_pos,
+            self.robot_pos,
+            self.obstacle_pos,
+            self.feed_pos,
+            self.sv,
+            self.evaporate,
+            self.diffusion,
+            self.decrease,
+            self.timestep
+        )
+
+    def create_mujoco_env(self) -> MuJoCoEnvInterface:
+        return Environment(
+            self.nest_pos,
+            self.robot_pos,
+            self.obstacle_pos,
+            self.feed_pos,
+            self.sv,
+            self.evaporate,
+            self.diffusion,
+            self.decrease,
+            self.timestep
+        )
