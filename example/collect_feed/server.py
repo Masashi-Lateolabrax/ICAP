fcce583b3b1eab1afcbaa49ddf28d2876ee915d4
Masashi <m2153100220@std.kitami-it.ac.jp>
2022-11-22 21:29:04 +0900

    example/collect_feed/server.pyを書きました．

--- example/collect_feed/server.py
+++ example/collect_feed/server.py
@@ -1,47 +1,39 @@
 import numpy
-from environments import utils, back_enemy
+from environments import utils
+from environments.collect_feed import EnvCreator
 from studyLib import wrap_mjc, miscellaneous
 
 if __name__ == "__main__":
     def main():
-        nest_pos = (0, 0)
-        robot_pos = [
-            [(x, 0) for x in range(-8, 9, 4)],  # 5体
-            [(x, 0) for x in range(-6, 7, 4)],  # 4体
-            [(x, 0) for x in range(-4, 5, 4)],  # 3体
-            [(x, 0) for x in range(-2, 3, 4)]  # 2体
-        ]
-        enemy_pos = [(0, -30)]
-        enemy_weight = 4500
+        env_creator = EnvCreator()
+        env_creator.nest_pos = (0, 0)
+        env_creator.robot_pos = [(0, 0), (40, 0), (-40, 0)]
+        env_creator.obstacle_pos = [(0, 150)]
+        env_creator.feed_pos = [(0, 500)]
+        env_creator.sv = 10.0
+        env_creator.evaporate = 20.0
+        env_creator.diffusion = 35.0
+        env_creator.decrease = 0.1
 
-        generation = 100
+        generation = 300
         population = 100
-        timestep = int(10 / 0.03333)
-
-        env = back_enemy.Environment(
-            nest_pos,
-            robot_pos,
-            enemy_pos,
-            enemy_weight,
-            timestep,
-        )
+        mu = 20
+        env_creator.timestep = int(60 / 0.033333)
 
         width: int = 640
         height: int = 480
         scale: int = 2
         window = miscellaneous.Window(width * scale, height * scale)
-        camera = wrap_mjc.Camera((0, 0, 0), 100, 0, 90)
+        camera = wrap_mjc.Camera((0, 350, 0), 1200, 0, 90)
 
-        # para, hist = utils.cmaes_optimize(generation, population, 3.0, env, True, (window, camera))
-        para, hist = utils.cmaes_optimize_server(generation, population, mu, 3.0, env, 52325, True, (window, camera))
+        para, hist = utils.cmaes_optimize_server(generation, population, mu, 0.3, env_creator, 52325, True)
         numpy.save("best_para.npy", para)
         hist.save("history.log")
 
         para = numpy.load("best_para.npy")
-        print(
-            "Result : ",
-            utils.show_mujoco_env(env, para, window, camera)
-        )
+        env = env_creator.create_mujoco_env()
+        score = utils.show_mujoco_env(env, para, window, camera)
+        print(f"Result : {score}")
 
 
     main()
