1348204705579f645a95f3cf5af9f1b81f3e21a4
Masashi <m2153100220@std.kitami-it.ac.jp>
2022-11-23 00:46:09 +0900

    追加したレイヤーを公開しました．

--- studyLib/nn_tools/__init__.py
+++ studyLib/nn_tools/__init__.py
@@ -2,8 +2,12 @@ from .liner_algebra import la
 from .calculator import Calculator
 
 from .layer.affine_layer import AffineLayer
+from .layer.iner_dot_layer import InnerDotLayer
+from .layer.rbf_layer import GaussianRadialBasisLayer
 
 from .activator.gaussian_layer import GaussianLayer
+from .activator.is_max import IsMaxLayer
+from .activator.is_min import IsMinLayer
 from .activator.relu_layer import ReluLayer
 from .activator.sigmoid_layer import SigmoidLayer
 from .activator.tanh_layer import TanhLayer
