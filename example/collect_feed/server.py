7eb643176411d8f0825338bb30deafaba07e4e36
Masashi <m2153100220@std.kitami-it.ac.jp>
2022-11-23 14:24:53 +0900

    exampleを変更に対応させました．

--- example/collect_feed/server.py
+++ example/collect_feed/server.py
@@ -7,9 +7,17 @@ if __name__ == "__main__":
     def main():
         env_creator = EnvCreator()
         env_creator.nest_pos = (0, 0)
-        env_creator.robot_pos = [(0, 0), (40, 0), (-40, 0)]
-        env_creator.obstacle_pos = [(0, 150)]
-        env_creator.feed_pos = [(0, 500)]
+        env_creator.robot_pos = [
+            (-40, 30), (0, 30), (40, 30),
+            (-40, -30), (0, -30), (40, -30),
+        ]
+        env_creator.obstacle_pos = [(0, 300)]
+        env_creator.feed_pos = [(0, 700), (0, 1200)]
+
+        env_creator.pheromone_field_pos = (0, 700)
+        env_creator.pheromone_field_panel_size = 20
+        env_creator.pheromone_field_shape = (60, 80)
+
         env_creator.sv = 10.0
         env_creator.evaporate = 20.0
         env_creator.diffusion = 35.0
@@ -21,18 +29,17 @@ if __name__ == "__main__":
         env_creator.timestep = int(60 / 0.033333)
 
         width: int = 640
-        height: int = 480
-        scale: int = 2
+        height: int = 640
+        scale: int = 1
         window = miscellaneous.Window(width * scale, height * scale)
-        camera = wrap_mjc.Camera((0, 350, 0), 1200, 0, 90)
+        camera = wrap_mjc.Camera((0, 700, 0), 2000, 90, 90)
 
-        para, hist = utils.cmaes_optimize_server(generation, population, mu, 0.3, env_creator, 52325, True)
+        para, hist = utils.cmaes_optimize_server(generation, population, mu, 0.3, env_creator, 52325, True, True)
         numpy.save("best_para.npy", para)
         hist.save("history.log")
 
         para = numpy.load("best_para.npy")
-        env = env_creator.create_mujoco_env()
-        score = utils.show_mujoco_env(env, para, window, camera)
+        score = utils.show_mujoco_env(env_creator, para, window, camera)
         print(f"Result : {score}")
 
 
