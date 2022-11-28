948e94c2484fc107634774b140cf665a3d9da9a5
Masashi <m2153100220@std.kitami-it.ac.jp>
2022-11-29 00:09:00 +0900

    RobotBrainを変更しました．

--- environments/collect_feed.py
+++ environments/collect_feed.py
@@ -234,11 +234,14 @@ class RobotBrain:
     def __init__(self, para):
         self._calculator = nn_tools.Calculator(11)
 
+        self._calculator.add_layer(nn_tools.AffineLayer(50))
+        self._calculator.add_layer(nn_tools.TanhLayer(50))
+
         self._calculator.add_layer(nn_tools.AffineLayer(30))
         self._calculator.add_layer(nn_tools.TanhLayer(30))
 
-        self._calculator.add_layer(nn_tools.AffineLayer(30))
-        self._calculator.add_layer(nn_tools.IsMaxLayer(30))
+        self._calculator.add_layer(nn_tools.AffineLayer(10))
+        self._calculator.add_layer(nn_tools.IsMaxLayer(10))
 
         self._calculator.add_layer(nn_tools.InnerDotLayer(3))
         self._calculator.add_layer(nn_tools.TanhLayer(3))
