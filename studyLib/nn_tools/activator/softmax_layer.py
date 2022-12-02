872636ec1d3ba08e557bcc37609ca514cb418b5c
Masashi <mutuya.contact@gmail.com>
2022-12-02 12:01:49 +0900

    Softmaxを実装しました．

---
+++ studyLib/nn_tools/activator/softmax_layer.py
@@ -0,0 +1,19 @@
+from collections.abc import Sequence
+from studyLib.nn_tools import interface, la
+
+
+class SigmoidLayer(interface.CalcActivator):
+    def __init__(self, num_node: int):
+        super().__init__(num_node)
+
+    def calc(self, input_: la.ndarray) -> la.ndarray:
+        return la.exp(input_) / la.sum(la.exp(input_))
+
+    def num_dim(self) -> int:
+        return 0
+
+    def load(self, offset: int, array: Sequence) -> int:
+        return 0
+
+    def save(self, array: list) -> None:
+        pass
