dbba228712d692c2be706984c9f2ae787f0d9b25
Masashi <mutuya.contact@gmail.com>
2022-12-05 20:12:07 +0900

    デバッグ出力を切りました．

--- environments/collect_feed.py
+++ environments/collect_feed.py
@@ -458,7 +458,6 @@ class Environment(optimizer.MuJoCoEnvInterface):
         dt_loss_feed_nest *= 1e-4 / len(self.feeds)
         dt_loss_feed_robot *= 1.0 / (len(self.feeds) * len(self.robots))
         dt_loss_obstacle_robot *= 1e11 / (len(self.obstacle_pos) * len(self.robots))
-        print(dt_loss_feed_nest, dt_loss_feed_robot, dt_loss_obstacle_robot)
         self.loss += dt_loss_feed_nest + dt_loss_feed_robot + dt_loss_obstacle_robot
 
         return self.loss
