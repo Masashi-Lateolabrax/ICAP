1972bc71bfe783fd1029160c9ba0626312a97d15
Masashi <mutuya.contact@gmail.com>
2022-12-08 17:39:28 +0900

    FilterLayerを実装しました．

--- studyLib/nn_tools/__init__.py
+++ studyLib/nn_tools/__init__.py
@@ -3,6 +3,7 @@ from .other.liner_algebra import la
 from .calculator import Calculator
 
 from .layer.affine_layer import AffineLayer
+from .layer.filter_layer import FilterLayer
 from .layer.fold_layer import AddFoldLayer
 from .layer.iner_dot_layer import InnerDotLayer
 from .layer.parallel_layer import ParallelLayer
---
+++ studyLib/nn_tools/layer/filter_layer.py
@@ -0,0 +1,27 @@
+from collections.abc import Sequence
+from studyLib.nn_tools import interface, la
+
+
+class FilterLayer(interface.CalcLayer):
+    def __init__(self, pass_indexes: list[int]):
+        super().__init__(len(pass_indexes))
+        self._mask = []
+        self._pass_indexes = pass_indexes
+
+    def init(self, num_input: int) -> None:
+        self._mask = [False] * num_input
+        for i in self._pass_indexes:
+            self._mask[i] = True
+
+    def calc(self, input_: la.ndarray, output: la.ndarray) -> int:
+        la.copyto(output, input_[self._mask])
+        return self.num_node
+
+    def num_dim(self) -> int:
+        return 0
+
+    def load(self, offset: int, array: Sequence) -> int:
+        return 0
+
+    def save(self, array: list) -> None:
+        pass
