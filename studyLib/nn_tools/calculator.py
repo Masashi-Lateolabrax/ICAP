661f8ceadfa60b2f90a23792afcc9792546aa4a3
Masashi <m2153100220@std.kitami-it.ac.jp>
2022-12-21 18:28:52 +0900

    バッファリングの効率化。

--- studyLib/nn_tools/calculator.py
+++ studyLib/nn_tools/calculator.py
@@ -2,11 +2,15 @@ from studyLib.nn_tools import interface, la
 
 
 class Calculator:
-    def __init__(self, num_input: int):
+    class CalcBuffer:
+        def __init__(self):
+            self.buf1: la.ndarray = la.zeros(0)
+            self.buf2: la.ndarray = la.zeros(0)
+
+    def __init__(self, num_input: int, buf: CalcBuffer = None):
         self._layer: list[interface.CalcLayer] = []
         self._num_input = num_input
-        self.buf1 = la.zeros(num_input)
-        self.buf2 = la.zeros(0)
+        self._buf = Calculator.CalcBuffer() if buf is None else buf
 
     def get_layer(self, i: int) -> interface.CalcLayer:
         return self._layer[i]
@@ -30,22 +34,24 @@ class Calculator:
 
     def calc(self, input_):
         size = len(input_)
-        la.copyto(self.buf1[0:size], input_)
+        if len(self._buf.buf1) < size:
+            self._buf.buf1 = la.zeros(size)
+        la.copyto(self._buf.buf1[0:size], input_)
 
         for i, layer in enumerate(self._layer):
             if i % 2 == 0:
-                if len(self.buf2) < layer.num_node:
-                    self.buf2 = la.zeros(layer.num_node)
-                size = layer.calc(self.buf1[0:size], self.buf2[0:layer.num_node])
+                if len(self._buf.buf2) < layer.num_node:
+                    self._buf.buf2 = la.zeros(layer.num_node)
+                size = layer.calc(self._buf.buf1[0:size], self._buf.buf2[0:layer.num_node])
             else:
-                if len(self.buf1) < layer.num_node:
-                    self.buf1 = la.zeros(layer.num_node)
-                size = layer.calc(self.buf2[0:size], self.buf1[0:layer.num_node])
+                if len(self._buf.buf1) < layer.num_node:
+                    self._buf.buf1 = la.zeros(layer.num_node)
+                size = layer.calc(self._buf.buf2[0:size], self._buf.buf1[0:layer.num_node])
 
         if len(self._layer) % 2 == 0:
-            return self.buf1[0:size]
+            return self._buf.buf1[0:size]
         else:
-            return self.buf2[0:size]
+            return self._buf.buf2[0:size]
 
     def load(self, array):
         dim = self.num_dim()
