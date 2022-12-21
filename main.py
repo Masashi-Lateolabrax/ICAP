6b7f579f9467ce5e24561b6c093079b285c5482a
Masashi <m2153100220@std.kitami-it.ac.jp>
2022-12-21 23:12:08 +0900

    最適化の設定をしました。

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from studyLib import wrap_mjc, miscellaneous
 
 
 def set_env_creator(env_creator: EnvCreator):
-    print(f"DIMENSION : {env_creator.dim()}")
-
     env_creator.nest_pos = (0, 0)
     env_creator.robot_pos = [
         (-45, 45), (0, 45), (45, 45),
@@ -20,13 +18,15 @@ def set_env_creator(env_creator: EnvCreator):
     env_creator.pheromone_field_panel_size = 20
     env_creator.pheromone_field_shape = (60, 80)
     env_creator.show_pheromone_index = 0
+    env_creator.pheromone_iteration = 5
 
     env_creator.sv = [10.0]
     env_creator.evaporate = [5.0]
     env_creator.diffusion = [30.0]
     env_creator.decrease = [0.01]
 
-    env_creator.timestep = int(30 / 0.033333)
+    env_creator.timestep = 0.033333
+    env_creator.time = 30
 
 
 if __name__ == "__main__":
@@ -34,12 +34,15 @@ if __name__ == "__main__":
         env_creator = EnvCreator()
         set_env_creator(env_creator)
 
-        generation = 300
-        population = 150  # int(3.0 * numpy.log(env_creator.dim())) * 4
-        mu = 10  # int(3.0 * numpy.log(env_creator.dim())) * 2
+        dim = env_creator.dim()
+        generation = 500
+        population = 300  # int(3.0 * numpy.log(env_creator.dim())) * 4
+        mu = 30  # int(3.0 * numpy.log(env_creator.dim())) * 2
         sigma = 0.3
         centroid = None
-        print(f"population : {population}, parent number : {mu}")
+        print(f"DIMENSION : {dim}")
+        print(f"population : {population}({int(3.0 * numpy.log(dim)) * 4})")
+        print(f"parent number : {mu}({int(3.0 * numpy.log(dim)) * 2})")
 
         # Resume
         # from studyLib import optimizer
@@ -55,6 +58,9 @@ if __name__ == "__main__":
         # Resume
         # centroid = numpy.load("best_para.npy")
 
+        # Random Initialize
+        # para = numpy.random.randn(env_creator.dim())
+
         # para, hist = utils.cmaes_optimize(generation, population, mu, sigma, centroid, env_creator, 1, True)
         para, hist = utils.cmaes_optimize_server(generation, population, mu, sigma, centroid, env_creator, 52325, True)
         numpy.save("best_para.npy", para)
@@ -65,20 +71,12 @@ if __name__ == "__main__":
         scale: int = 1
         window = miscellaneous.Window(len(env_creator.sv) * width * scale, height * scale)
         camera = wrap_mjc.Camera((0, 600, 0), 2500, 90, 90)
-        window.set_recorder(miscellaneous.Recorder("result.mp4", 30, len(env_creator.sv) * width, height))
+        window.set_recorder(
+            miscellaneous.Recorder("result.mp4", int(1.0 / env_creator.timestep), len(env_creator.sv) * width, height)
+        )
 
-        envs = []
-        for i in range(0, len(env_creator.sv)):
-            env_creator.show_pheromone_index = i
-            env = env_creator.create_mujoco_env(para, window, camera)
-            envs.append(env)
-
-        timestep = envs[0].timestep
-        score = float("nan")
-        for t in range(0, timestep):
-            for i, env in enumerate(envs):
-                score = env.calc_step()
-                env.render((width * i, 0, width, height), i == len(envs) - 1)
+        env = env_creator.create_mujoco_env(para, window, camera)
+        score = env.calc_and_show()
         print(f"Result : {score}")
 
 
