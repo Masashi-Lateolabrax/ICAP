7fd145ae18387e60b6f344cc43e0904e1ab0753e
Masashi <m2153100220@std.kitami-it.ac.jp>
2022-12-23 02:51:18 +0900

    Change optimize setting.

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import numpy
+
 from environments import utils
 from environments.collect_feed import EnvCreator
-from studyLib import wrap_mjc, miscellaneous
+from studyLib import wrap_mjc, miscellaneous, optimizer
 
 
 def set_env_creator(env_creator: EnvCreator):
+    env_creator.timestep = 0.033333
+    env_creator.time = 5
+
     env_creator.nest_pos = (0, 0)
     env_creator.robot_pos = [
         (-45, 45), (0, 45), (45, 45),
@@ -21,50 +25,72 @@ def set_env_creator(env_creator: EnvCreator):
     env_creator.pheromone_iteration = 5
 
     env_creator.sv = [10.0]
-    env_creator.evaporate = [5.0]
-    env_creator.diffusion = [30.0]
-    env_creator.decrease = [0.01]
+    env_creator.evaporate = [1.0 / env_creator.timestep]
+    env_creator.diffusion = [1.0 / env_creator.timestep]
+    env_creator.decrease = [0.01 / env_creator.timestep]
 
-    env_creator.timestep = 0.033333
-    env_creator.time = 30
+
+def optimize(
+        generation: int, population: int,
+        mu: int, sigma: float, centroid,
+        env_creator: EnvCreator,
+        server_client: bool = False,
+) -> (numpy.ndarray, optimizer.Hist):
+    dim = env_creator.dim()
+    print(f"DIMENSION : {dim}")
+    print(f"population : {population}({int(3.0 * numpy.log(dim)) * 4})")
+    print(f"parent number : {mu}({int(3.0 * numpy.log(dim)) * 2})")
+
+    if len(centroid) < dim:
+        centroid = numpy.zeros(dim)
+
+    if server_client:
+        para, hist = utils.cmaes_optimize_server(generation, population, mu, sigma, centroid, env_creator, 52325, True)
+    else:
+        para, hist = utils.cmaes_optimize(generation, population, mu, sigma, centroid, env_creator, 2, True)
+
+    numpy.save("best_para.npy", para)
+    hist.save("history")
+
+    return para, hist
+
+
+def load_best_para_from_hist(hist_path) -> (numpy.ndarray, float):
+    hist = optimizer.Hist(0, 0, 0)
+    hist.load(hist_path)
+
+    best_score = float("inf")
+    para = numpy.zeros(0)
+    sigma = 0.0
+    for q in hist.queues:
+        if q.min_score < best_score:
+            para = q.min_para.copy()
+            sigma = q.sigma
+            best_score = q.min_score
+
+    return para, sigma
 
 
 if __name__ == "__main__":
     def main():
+        para = []
+        sigma = 0.3
         env_creator = EnvCreator()
         set_env_creator(env_creator)
 
-        dim = env_creator.dim()
-        generation = 500
-        population = 300  # int(3.0 * numpy.log(env_creator.dim())) * 4
-        mu = 30  # int(3.0 * numpy.log(env_creator.dim())) * 2
-        sigma = 0.3
-        centroid = None
-        print(f"DIMENSION : {dim}")
-        print(f"population : {population}({int(3.0 * numpy.log(dim)) * 4})")
-        print(f"parent number : {mu}({int(3.0 * numpy.log(dim)) * 2})")
-
         # Resume
-        # from studyLib import optimizer
-        # hist = optimizer.Hist(0, 0, 0)
-        # hist.load("./TMP_HIST.npz")
-        # best_score = float("inf")
-        # for q in hist.queues:
-        #     if q.min_score < best_score:
-        #         centroid = q.min_para.copy()
-        #         sigma = q.sigma
-        #         best_score = q.min_score
+        # para = numpy.load("best_para.npy")
+        # para, sigma = load_best_para_from_hist("./TMP_HIST.log.npz")
 
-        # Resume
-        # centroid = numpy.load("best_para.npy")
-
-        # Random Initialize
-        # para = numpy.random.randn(env_creator.dim())
-
-        # para, hist = utils.cmaes_optimize(generation, population, mu, sigma, centroid, env_creator, 1, True)
-        para, hist = utils.cmaes_optimize_server(generation, population, mu, sigma, centroid, env_creator, 52325, True)
-        numpy.save("best_para.npy", para)
-        hist.save("history")
+        para, hist = optimize(
+            generation=1000,
+            population=100,
+            mu=30,
+            sigma=sigma,
+            centroid=para,
+            env_creator=env_creator,
+            server_client=True
+        )
 
         width: int = 500
         height: int = 700
