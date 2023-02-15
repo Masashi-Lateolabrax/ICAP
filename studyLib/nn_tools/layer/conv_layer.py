e5eb402fc67abc925d09bb75825b3b50dd6bf2ac
Masashi <m2153100220@std.kitami-it.ac.jp>
2023-02-15 11:45:51 +0900

    Refactored.

--- studyLib/nn_tools/layer/conv_layer.py
+++ studyLib/nn_tools/layer/conv_layer.py
@@ -25,7 +25,7 @@ class _ConvLayer(interface.CalcLayer, ABC):
 
 
 class Conv1DLayer(_ConvLayer):
-    def __init__(self, num_node: int, window: interface.CalcLayer, window_size: int, num_pad: int):
+    def __init__(self, num_node: int, window_size: int, window: interface.CalcLayer, num_pad: int):
         super().__init__(num_node, window, window_size, num_pad)
         self.stride = 0
         self._in_buf = la.zeros(1)
