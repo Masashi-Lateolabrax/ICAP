dbc8132d8def5d7f53abef221afb719036502af8
Masashi <m2153100220@std.kitami-it.ac.jp>
2023-02-16 15:35:51 +0900

    Fixed Conv1DLayer's error.

--- studyLib/nn_tools/layer/conv_layer.py
+++ studyLib/nn_tools/layer/conv_layer.py
@@ -4,12 +4,12 @@ from studyLib.nn_tools import interface, la
 
 
 class _ConvLayer(interface.CalcLayer, ABC):
-    def __init__(self, num_node: int, window: interface.CalcLayer, window_size: int, num_pad: int):
-        super().__init__(num_node)
+    def __init__(self, num_window: int, window: interface.CalcLayer, window_size: int, num_pad: int):
+        super().__init__(num_window * window.num_node)
         self._window = window
-        self._num_window = int(num_node / window.num_node)
-        self.window_size = window_size
-        self.num_pad = num_pad
+        self._num_window = num_window
+        self._window_size = window_size
+        self._num_pad = num_pad
         self._num_input = 0
 
         self._window.init(window_size)
@@ -25,35 +25,49 @@ class _ConvLayer(interface.CalcLayer, ABC):
 
 
 class Conv1DLayer(_ConvLayer):
-    def __init__(self, num_output: int, window_size: int, window: interface.CalcLayer, num_pad: int):
+    def __init__(
+            self,
+            num_window: int,
+            window_size: int,
+            stride: int,
+            window: interface.CalcLayer,
+            num_pad: int
+    ):
         if window_size < num_pad:
             print("'num_pad' is lager than 'window_size'. It is incorrect.")
 
-        super().__init__(num_output, window, window_size, num_pad)
-        self.stride = 0
-        self._in_buf = la.zeros(1)
-        self._out_buf = la.zeros(num_output)
+        super().__init__(num_window, window, window_size, num_pad)
+        self.stride = stride
+        self._in_buf = la.zeros(self._window_size)
+        self._out_buf = la.zeros(self.num_node)
 
     def init(self, num_input: int) -> None:
         self._num_input = num_input
-        self._in_buf = la.zeros(self._num_input)
-        self.stride = int((self._num_input + self.num_pad * 2 - self.window_size) / (self._num_window - 1))
+        stride: float = (self._num_input + self._num_pad * 2 - self._window_size) / (self._num_window - 1)
+
+        if not stride.is_integer():
+            print(
+                f"Failed to calculate stride size({stride}). "
+                f"({self._num_input}, {self._num_pad}, {self._window_size}, {self._num_window})"
+            )
+        elif self.stride != int(stride):
+            print("Invalid parameters are passed.")
 
     def calc(self, input_: la.ndarray, output: la.ndarray) -> int:
         input_index = 0
         out_buf_index = 0
 
         for i in range(self._num_window):
-            if i == 0:
-                self._in_buf[:self.num_pad] = 0
-                self._in_buf[self.num_pad:] = input_[input_index:input_index + self.window_size - self.num_pad]
-                input_index += self.window_size - self.num_pad
-            elif i == self._num_window - 1:
-                self._in_buf[:-self.num_pad] = input_[input_index:input_index + self.window_size - self.num_pad]
-                self._in_buf[-self.num_pad:] = 0
-                input_index += self.window_size - self.num_pad
+            if self._num_pad > 0 and (i == 0 or i == self._num_window - 1):
+                if i == 0:
+                    self._in_buf[:self._num_pad] = 0
+                    self._in_buf[self._num_pad:] = input_[input_index:input_index + self._window_size - self._num_pad]
+                    input_index += self.stride - self._num_pad
+                else:
+                    self._in_buf[:-self._num_pad] = input_[input_index:input_index + self._window_size - self._num_pad]
+                    self._in_buf[-self._num_pad:] = 0
             else:
-                self._in_buf[:] = input_[input_index:input_index + self.window_size]
+                self._in_buf[:] = input_[input_index:input_index + self._window_size]
                 input_index += self.stride
 
             self._window.calc(
