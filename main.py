0d770999f2f4b6968b1b04cba0eb43d44853194f
Masashi <m2153100220@std.kitami-it.ac.jp>
2023-02-28 01:41:02 +0900

    Experiment setting has been changed.

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from studyLib import miscellaneous, wrap_mjc
 
 
 def set_env_creator(env_creator: EnvCreator):
-    env_creator.timestep = 0.06
-    env_creator.time = 60
-    env_creator.think_interval = 15
+    env_creator.timestep = 0.05
+    env_creator.time = 120
+    env_creator.think_interval = 10
 
     theta = 0
 
