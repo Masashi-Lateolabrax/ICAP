d32935291fad12ac2b8bacaa36836dc619d4af42
Masashi <m2153100220@std.kitami-it.ac.jp>
2023-04-12 21:22:08 +0900

    The setting of experiment has been changed.

--- environments/collect_feed.py
+++ environments/collect_feed.py
@@ -415,9 +415,9 @@ class RobotBrain:
                     nn_tools.FilterLayer([i for i in range(28 + 3)]),
                     nn_tools.AffineLayer(10),
                     nn_tools.TanhLayer(10),
-                    nn_tools.AffineLayer(10),
-                    nn_tools.IsMaxLayer(10),
-                    nn_tools.BufLayer(10)
+                    nn_tools.AffineLayer(3),
+                    nn_tools.IsMaxLayer(3),
+                    nn_tools.BufLayer(3)
                 ],
                 [
                     nn_tools.FilterLayer([31]),
@@ -428,8 +428,11 @@ class RobotBrain:
         self._calculator.add_layer(nn_tools.ParallelLayer(
             [
                 [
-                    nn_tools.AffineLayer(11),
-                    nn_tools.TanhLayer(11),
+                    nn_tools.AffineLayer(5),
+                    nn_tools.TanhLayer(5),
+
+                    nn_tools.AffineLayer(5),
+                    nn_tools.TanhLayer(5),
 
                     nn_tools.AffineLayer(3),
                     nn_tools.SigmoidLayer(3)
