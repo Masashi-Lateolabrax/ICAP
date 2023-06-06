0b5fa909e7bf605f6612606847acf5990cb4f26f
Masashi <m2153100220@std.kitami-it.ac.jp>
2023-06-06 16:12:00 +0900

    Robots' brain has been changed.

--- environments/collect_feed.py
+++ environments/collect_feed.py
@@ -419,9 +419,9 @@ class RobotBrain:
                     nn_tools.AffineLayer(7),
                     nn_tools.TanhLayer(7),
 
-                    nn_tools.AffineLayer(5),
-                    nn_tools.IsMaxLayer(5),
-                    nn_tools.BufLayer(5)
+                    nn_tools.AffineLayer(3),
+                    nn_tools.IsMaxLayer(3),
+                    nn_tools.BufLayer(3)
                 ],
                 [
                     nn_tools.FilterLayer([13]),
@@ -432,8 +432,8 @@ class RobotBrain:
         self._calculator.add_layer(nn_tools.ParallelLayer(
             [
                 [
-                    nn_tools.AffineLayer(6),
-                    nn_tools.TanhLayer(6),
+                    nn_tools.AffineLayer(4),
+                    nn_tools.TanhLayer(4),
 
                     nn_tools.AffineLayer(3),
                     nn_tools.TanhLayer(3),
