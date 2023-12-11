625905ff703f272b3b485e0b0d012f765263a185
MasashiSUZUKI <masashi.lateolabrax@gmail.com>
2023-12-11 09:15:56 +0900

    refactor: Pheromone Class.

--- src/Pheromone/pheromone.py
+++ src/Pheromone/pheromone.py
@@ -2,6 +2,50 @@ import torch
 
 
 class Pheromone:
+
+    @staticmethod
+    def calc_weights_for_cells(x: float, y: float):
+        """
+        This method calculate weights for cells around [x, y].
+        This use the bi linear method to calculate the weights.
+        """
+
+        result = {
+            "upper_left": {"index": (0, 0), "weight": 0.0, "inv_weight": 0.0},
+            "lower_right": {"index": (0, 0), "weight": 0.0, "inv_weight": 0.0},
+            "upper_right": {"index": (0, 0), "weight": 0.0, "inv_weight": 0.0},
+            "lower_left": {"index": (0, 0), "weight": 0.0, "inv_weight": 0.0},
+        }
+
+        if x - int(x) < 0.5:
+            left_index = (int(x) - 1, int(y))
+        else:
+            left_index = (int(x), int(y))
+
+        if y - int(y) < 0.5:
+            upper_index = (int(x), int(y) - 1)
+        else:
+            upper_index = (int(x), int(y))
+
+        result["upper_left"]["index"] = upper_left_index = (left_index[0], upper_index[1])
+        result["lower_right"]["index"] = lower_right_index = (upper_left_index[0] + 1, upper_left_index[1] + 1)
+        result["upper_right"]["index"] = (lower_right_index[0], upper_left_index[1])
+        result["lower_left"]["index"] = (upper_left_index[0], lower_right_index[1])
+
+        a = x - (upper_left_index[0] + 0.5)
+        b = y - (upper_left_index[1] + 0.5)
+
+        # result["upper_left"]["inv_weight"] = a * b
+        # result["lower_right"]["inv_weight"] = (1 - a) * (1 - b)
+        # result["upper_right"]["inv_weight"] = (1 - a) * b
+        # result["lower_left"]["inv_weight"] = a * (1 - b)
+        result["upper_left"]["weight"] = (1 - a) * (1 - b)
+        result["lower_right"]["weight"] = a * b
+        result["upper_right"]["weight"] = a * (1 - b)
+        result["lower_left"]["weight"] = (1 - a) * b
+
+        return result
+
     def __init__(
             self,
             width: int, height: int, d: float,
@@ -23,64 +67,51 @@ class Pheromone:
             [0.0, 1.0, 0.0],
         ]]]) * self.diffusion / torch.pow(self.d, 2)
 
-    def _check_index_is_valid(self, x: int, y: int):
+    def _check_index_is_valid(self, x, y, raise_error=True):
         size = self.liquid.size()
-        if x < 0 or size[1] < x:
-            raise f"'x' must be in [0, {size[1]}]."
-        elif y < 0 or size[2] < y:
-            raise f"'y' must be in [0, {size[2]}]."
+        if x < 0 or size[1] <= x:
+            res = False
+        elif y < 0 or size[2] <= y:
+            res = False
+        else:
+            res = True
 
-    def get_liquid_value(self, x: int, y: int) -> float:
-        self._check_index_is_valid(x, y)
+        if raise_error and not res:
+            raise "Invalid index has been passed!"
+
+        return res
+
+    def get_raw_liquid_value(self, x: int, y: int, raise_error=True) -> float:
+        if not self._check_index_is_valid(x, y, raise_error):
+            return 0.0
         return self.liquid[0, x, y]
 
-    def get_gas_value(self, x: int, y: int):
+    def get_liquid_value(self, x: float, y: float) -> float:
         self._check_index_is_valid(x, y)
+        value = 0.0
+        for iw in self.calc_weights_for_cells(x, y).values():
+            i = iw["index"]
+            w = iw["weight"]
+            value += self.get_raw_liquid_value(i[0], i[1], raise_error=False) * w
+        return value
+
+    def get_raw_gas_value(self, x: int, y: int, raise_error=True):
+        if not self._check_index_is_valid(x, y, raise_error):
+            return 0.0
         return self.gas[0, x, y]
 
-    @staticmethod
-    def calc_weight_for_cells(x: float, y: float):
-        """
-        This method calculate weights for cells around [x, y].
-        This use the bi linear method to calculate the weights.
-        """
-
-        result = {
-            "upper_left": {"index": (0, 0), "weight": 0.0},
-            "lower_right": {"index": (0, 0), "weight": 0.0},
-            "upper_right": {"index": (0, 0), "weight": 0.0},
-            "lower_left": {"index": (0, 0), "weight": 0.0},
-        }
-
-        if x - int(x) < 0.5:
-            left_index = (int(x) - 1, int(y))
-        else:
-            left_index = (int(x), int(y))
-
-        if y - int(y) < 0.5:
-            upper_index = (int(x), int(y) - 1)
-        else:
-            upper_index = (int(x), int(y))
-
-        result["upper_left"]["index"] = upper_left_index = (left_index[0], upper_index[1])
-        result["lower_right"]["index"] = lower_right_index = (upper_left_index[0] + 1, upper_left_index[1] + 1)
-        result["upper_right"]["index"] = (lower_right_index[0], upper_left_index[1])
-        result["lower_left"]["index"] = (upper_left_index[0], lower_right_index[1])
-
-        a = x - (upper_left_index[0] + 0.5)
-        b = y - (upper_left_index[1] + 0.5)
-
-        result["upper_left"]["weight"] = (1 - a) * (1 - b)
-        result["lower_right"]["weight"] = a * b
-        result["upper_right"]["weight"] = a * (1 - b)
-        result["lower_left"]["weight"] = (1 - a) * b
-
-        return result
-
-    def add_liquid(self, x: int, y: int, value: float):
+    def get_gas_value(self, x: float, y: float):
         self._check_index_is_valid(x, y)
-        weights = self.calc_weight_for_cells(x, y)
+        value = 0.0
+        for iw in self.calc_weights_for_cells(x, y).values():
+            i = iw["index"]
+            w = iw["weight"]
+            value += self.get_raw_gas_value(i[0], i[1], raise_error=False) * w
+        return value
 
+    def add_liquid(self, x: float, y: float, value: float):
+        self._check_index_is_valid(x, y)
+        weights = self.calc_weights_for_cells(x, y)
         for iw in weights.values():
             i = iw["index"]
             w = iw["weight"]
