1a7fd2be4bfa65ddfde9e214cf55835e307ea075
masashi <masashi.lateolabrax@gmail.com>
2024-04-24 11:18:56 +0900

    impl: robot's brain.

--- src/main.py
+++ src/main.py
@@ -2,30 +2,13 @@ import numpy as np
 import tkinter as tk
 
 import mujoco
-
-from torch import nn
+import torch
 
 import utils
 from viewer import App, ViewerHandler
 from environment import gen_xml
 from distance_measure import DistanceMeasure
-
-
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super(NeuralNetwork, self).__init__()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(28 * 28, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 10),
-            nn.ReLU()
-        )
-
-    def forward(self, x):
-        logits = self.linear_relu_stack(x)
-        return logits
+from brain import NeuralNetwork
 
 
 class Main(ViewerHandler):
@@ -35,8 +18,9 @@ class Main(ViewerHandler):
 
         n = 64
         self._measure = DistanceMeasure(n)
-        self._calc_sight_buf = np.zeros((1, n, 3), dtype=np.uint8)
         self._calc_sight_buf = np.zeros((1, n + 1, 3), dtype=np.uint8)
+
+        self.brain = NeuralNetwork()
 
         self._rot_buf = np.zeros((4, 1), dtype=np.float64)
         self._direction_buf = np.zeros((3, 1), dtype=np.float64)
@@ -79,48 +63,22 @@ class Main(ViewerHandler):
             act_rot = d.actuator(f"bot{i}.act.rot")
             act_move_x = d.actuator(f"bot{i}.act.pos_x")
             act_move_y = d.actuator(f"bot{i}.act.pos_y")
-            sight = self.calc_sight(m, d, bot_body_id)
-
-            # act_rot.ctrl[0] += 0.5 * m.opt.timestep
-            # movement = 1.2 * m.opt.timestep
-
-            if self._time > 1200:
-                movement = 0.0
-                rotation = 0.0
-            elif self._time > 900:
-                movement = 0.3
-                rotation = 0.0
-            elif self._time > 600:
-                movement = 0.0
-                rotation = -1.0
-            elif self._time > 300:
-                movement = 0.5
-                rotation = 0.0
-            else:
-                movement = 0.0
-                rotation = 1.0
 
             mujoco.mju_axisAngle2Quat(self._rot_buf, [0, 0, 1], act_rot.length)
-            mujoco.mju_rotVecQuat(self._move_buf, [0, 1, 0], self._rot_buf)
             mujoco.mju_rotVecQuat(self._direction_buf, [0, 1, 0], self._rot_buf)
 
             sight = self.calc_sight(m, d, bot_body_id, bot_body, self._direction_buf)
+            x = torch.from_numpy(self._calc_sight_buf[0, :, :]).float()
+            x.transpose_(0, 1)
+            y = self.brain.forward(x)
+            yi = torch.argmax(y)
 
-            if self._time > 1200:
-                movement = 0.0
-                rotation = 0.0
-            elif self._time > 900:
-                movement = 0.3
-                rotation = 0.0
-            elif self._time > 600:
-                movement = 0.0
-                rotation = -1.0
-            elif self._time > 300:
-                movement = 0.5
+            if yi == 0:
+                movement = 1.2 * y[yi].item()
                 rotation = 0.0
             else:
                 movement = 0.0
-                rotation = 1.0
+                rotation = 0.5 * y[yi].item()
 
             act_rot.ctrl[0] += 0.5 * rotation * m.opt.timestep
             act_move_x.ctrl[0] += 1.2 * self._direction_buf[0] * movement * m.opt.timestep
