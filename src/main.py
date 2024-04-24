ba7ba683afca000a3cf1bab558bdae838d808a2f
masashi <masashi.lateolabrax@gmail.com>
2024-04-24 17:30:49 +0900

    update: adjust codes for CMAES Environments.

--- src/main.py
+++ src/main.py
@@ -1,101 +1,70 @@
-import numpy as np
 import tkinter as tk
 
 import mujoco
-import torch
 
-import utils
 from viewer import App, ViewerHandler
-from environment import gen_xml
 from distance_measure import DistanceMeasure
 from brain import NeuralNetwork
+from optimizer import CMAES
+from environment import ECreator
+from robot import Robot
 
 
 class Main(ViewerHandler):
-    def __init__(self, nbot: int):
+    def __init__(self, nbot: int, para):
         self.nbot = nbot
+        self.bots = []
+
         self._renderer: mujoco.Renderer | None = None
 
-        n = 64
-        self._measure = DistanceMeasure(n)
-        self._calc_sight_buf = np.zeros((1, n + 1, 3), dtype=np.uint8)
+        self._measure = DistanceMeasure(64)
 
-        self.brain = NeuralNetwork()
+        self._brain = NeuralNetwork()
+        self._brain.load_para(para)
 
-        self._rot_buf = np.zeros((4, 1), dtype=np.float64)
-        self._direction_buf = np.zeros((3, 1), dtype=np.float64)
         self._time = 0
 
     def customize_tk(self, tk_top: tk.Tk):
         pass
 
-    def calc_sight(self, m: mujoco.MjModel, d: mujoco.MjData, bot_body_id: int, bot_body,
-                   bot_direction: np.ndarray):
-        ids, dists = self._measure.measure(m, d, bot_body_id, bot_body, bot_direction)
-
-        self._calc_sight_buf.fill(0)
-        color = np.zeros((3,), dtype=np.float32)
-        for j, (id_, dist) in enumerate(zip(ids, dists)):
-            if id_ < 0:
-                continue
-
-            name = mujoco.mj_id2name(m, mujoco.mjtObj.mjOBJ_GEOM, id_)
-            if "bot" in name:
-                color[:] = [255, 255, 0]
-            elif "goal" in name:
-                color[:] = [0, 255, 0]
-
-            self._calc_sight_buf[0, j, :] = color / ((dist * 0.1 + 1) ** 2)
-
-        self._calc_sight_buf[0, -1, :] = self._calc_sight_buf[0, 0, :]
-        return self._calc_sight_buf
-
     def step(self, m: mujoco.MjModel, d: mujoco.MjData, gui: tk.Tk):
         if self._renderer is None:
             self._renderer = mujoco.Renderer(m, 240, 240)
+        if len(self.bots) == 0:
+            self.bots = [Robot(m, d, i, self._brain) for i in range(self.nbot)]
 
-        self._time += 1
         cam_name = gui.children["!infoview"].camera_names.get()
 
-        for i in range(self.nbot):
-            bot_body_id = mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, f"bot{i}.body")
-            bot_body = d.body(bot_body_id)
-            act_rot = d.actuator(f"bot{i}.act.rot")
-            act_move_x = d.actuator(f"bot{i}.act.pos_x")
-            act_move_y = d.actuator(f"bot{i}.act.pos_y")
+        for bot in self.bots:
+            direction = bot.calc_direction()
+            sight = self._measure.measure_with_img(
+                m, d,
+                bot.bot_body_id, bot.bot_body,
+                direction
+            )
+            bot.exec(sight)
 
-            mujoco.mju_axisAngle2Quat(self._rot_buf, [0, 0, 1], act_rot.length)
-            mujoco.mju_rotVecQuat(self._direction_buf, [0, 1, 0], self._rot_buf)
-
-            sight = self.calc_sight(m, d, bot_body_id, bot_body, self._direction_buf)
-            x = torch.from_numpy(self._calc_sight_buf[0, :, :]).float()
-            x.transpose_(0, 1)
-            y = self.brain.forward(x)
-            yi = torch.argmax(y)
-
-            if yi == 0:
-                movement = 1.2 * y[yi].item()
-                rotation = 0.0
-            else:
-                movement = 0.0
-                rotation = 0.5 * y[yi].item()
-
-            act_rot.ctrl[0] += 0.5 * rotation * m.opt.timestep
-            act_move_x.ctrl[0] += 1.2 * self._direction_buf[0] * movement * m.opt.timestep
-            act_move_y.ctrl[0] += 1.2 * self._direction_buf[1] * movement * m.opt.timestep
-
-            if cam_name == f"bot{i}.camera":
+            if cam_name == bot.cam_name:
                 gui.children["!lidarview"].render(sight)
 
 
 def entry_point():
     n = 1
+
+    dim = sum([p.numel() for p in NeuralNetwork().parameters() if p.requires_grad])
+    cmaes = CMAES(dim, 3, 3, max_thread=1)
+    env_creator = ECreator(n, n)
+    cmaes.optimize(env_creator)
+    cmaes.get_history().save("history.npz")
+
+    import utils
+    from environment import gen_xml
     bot_pos = utils.generate_circles(n, 0.3 * 1.01, 5)
     goal_pos = utils.generate_circles(n, 0.4 * 1.01, 5)
     app = App(
-        gen_xml([(0, 0, 0)], goal_pos),
+        gen_xml(bot_pos, goal_pos),
         500, 500,
-        Main(n)
+        Main(1, cmaes.get_best_para())
     )
     app.mainloop()
 
---
+++ src/robot.py
@@ -0,0 +1,60 @@
+import mujoco
+import torch
+import numpy as np
+from brain import NeuralNetwork
+
+
+class Robot:
+    def __init__(self, model: mujoco.MjModel, data: mujoco.MjData, i, brain: NeuralNetwork):
+        self.timestep = model.opt.timestep
+        self.cam_name = f"bot{i}.camera"
+
+        self._current_frame = 0
+        self._skip_frame = int(0.1 / self.timestep)
+
+        self.brain = brain
+
+        self.bot_body_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_BODY, f"bot{i}.body")
+        self.bot_body = data.body(self.bot_body_id)
+        self.act_rot = data.actuator(f"bot{i}.act.rot")
+        self.act_move_x = data.actuator(f"bot{i}.act.pos_x")
+        self.act_move_y = data.actuator(f"bot{i}.act.pos_y")
+
+        self._quat_buf = np.zeros((4, 1), dtype=np.float64)
+        self._direction_buf_for_sight = np.zeros((3, 1), dtype=np.float64)
+        self._direction_buf_for_act = np.zeros((3, 1), dtype=np.float64)
+
+        self.movement = 0.0
+        self.rotation = 0.0
+
+    def calc_direction(self):
+        mujoco.mju_rotVecQuat(self._direction_buf_for_sight, [0, 1, 0], self.bot_body.xquat)
+        mujoco.mju_axisAngle2Quat(self._quat_buf, [0, 0, 1], self.act_rot.length)
+        mujoco.mju_rotVecQuat(self._direction_buf_for_act, [0, 1, 0], self._quat_buf)
+        return self._direction_buf_for_sight
+
+    def exec(self, sight, act=True):
+        self._current_frame += 1
+        if self._skip_frame < self._current_frame:
+            self._current_frame = 0
+
+            x = torch.from_numpy(sight[0, :, :]).float()
+            x /= 255.0
+            x.transpose_(0, 1)
+            y = self.brain.forward(x)
+            yi = torch.argmax(y[0:3])
+
+            if yi == 0:
+                self.movement = 1.2 * y[3].item()
+                self.rotation = 0.0
+            elif yi == 1:
+                self.movement = 0.0
+                self.rotation = 0.5 * y[3].item()
+            else:
+                self.movement = 0.0
+                self.rotation = -0.5 * y[3].item()
+
+        if act:
+            self.act_rot.ctrl[0] += 0.5 * self.rotation * self.timestep
+            self.act_move_x.ctrl[0] += 1.2 * self._direction_buf_for_act[0] * self.movement * self.timestep
+            self.act_move_y.ctrl[0] += 1.2 * self._direction_buf_for_act[1] * self.movement * self.timestep
