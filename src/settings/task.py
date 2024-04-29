0e00288ede00bc402d8ce0127d299adfd3c1d86f
mSuzuki <masashi.lateolabrax@gmail.com>
2024-04-30 07:54:14 +0900

    exp: changes the evaluation method from median to max.

--- src/settings/task.py
+++ src/settings/task.py
@@ -178,4 +178,4 @@ class Task(TaskInterface):
             self.mujoco.init_mujoco(t)
             for _ in range(int(15 / self.mujoco.timestep + 0.5)):
                 evaluations[t] += self.calc_step()
-        return np.median(evaluations)
+        return evaluations.max()
