c4ab887e364746c0839bd9164591c1040045d686
masashi <masashi.lateolabrax@gmail.com>
2024-05-10 02:41:55 +0900

    exp: increase LiDAR precision.
     the LiDAR precision increase from 64 to 314.
    
     implements the shrink mechanism from 314 to 38.
    
    CMAES population is 100, try_count is 10, generation is 1000.

--- src/settings/__init__.py
+++ src/settings/__init__.py
@@ -1,3 +1,4 @@
 from .utils.brain import NeuralNetwork
 from .utils.robot import Robot
 from .task_creator import TaskGenerator, Task
+from .hyper_parameters import HyperParameters
--- src/train/main.py
+++ src/train/main.py
@@ -4,7 +4,7 @@ from src.optimizer import CMAES, MultiThreadProc
 
 def main():
     dim = sum([p.numel() for p in NeuralNetwork().parameters() if p.requires_grad])
-    cmaes = CMAES(dim, 5000, 100, sigma=0.3)
+    cmaes = CMAES(dim, 1000, 50, sigma=0.3)
     for gen in range(1, 1 + cmaes.get_generation()):
         env_creator = TaskGenerator()
         cmaes.optimize_current_generation(env_creator, MultiThreadProc)
