c1c5ba2cc70141922a734824179a875dcd3eaab1
masashi <masashi.lateolabrax@gmail.com>
2024-05-12 21:58:37 +0900

    exp: using Pattern Actuator.

--- src/settings/utils/brain.py
+++ src/settings/utils/brain.py
@@ -11,13 +11,17 @@ class NeuralNetwork(nn.Module):
             nn.Flatten(0, -1),
             nn.Linear(38, 8),
             nn.Tanh(),
-            nn.Linear(8, 2),
-            nn.Tanh(),
+            nn.Linear(8, 4),
         )
+        self.af1 = nn.Softmax(dim=0)
+        self.af2 = nn.Sigmoid()
 
     def forward(self, x) -> torch.Tensor:
         x = self.sequence.forward(x)
-        return x
+        x1 = self.af1.forward(x[0:3])
+        x2 = self.af2.forward(x[3])
+        x2 = x2.unsqueeze(0)
+        return torch.cat([x1, x2])
 
     def load_para(self, para):
         with torch.no_grad():
