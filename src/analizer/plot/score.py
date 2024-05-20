68a43160051dd61287d9556192c6bf9b879a802b
masashi <masashi.lateolabrax@gmail.com>
2024-05-20 18:43:43 +0900

    impl: the plot showing max/min scores for each generation.

---
+++ src/analizer/plot/score.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from src.optimizer import Hist
+from src.utils import get_current_history
+
+
+def main():
+    history = Hist.load(
+        get_current_history("../../")
+    )
+
+    x = np.arange(0, len(history.queues), dtype=np.uint)
+    data = np.zeros((len(history.queues), 2))
+    for i, q in enumerate(history.queues):
+        data[i, 0] = q.min_score
+        data[i, 1] = q.max_score
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(x, data[:, 0], c="Blue")
+    ax.plot(x, data[:, 1], c="Red")
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
