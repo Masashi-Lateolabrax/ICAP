07ec6fa243418b9473b1948686572873f393a677
masashi <masashi.lateolabrax@gmail.com>
2024-05-22 02:20:18 +0900

    exp: increase 'TURN_SPEED' and 'EPISODE'.

--- src/settings/hyper_parameters.py
+++ src/settings/hyper_parameters.py
@@ -1,6 +1,6 @@
 class HyperParameters:
     GENERATION = 1000
-    POPULATION = 50
+    POPULATION = 30
     SIGMA = 0.3
     EPISODE = 30
     TIMESTEP = 0.01
@@ -8,5 +8,5 @@ class HyperParameters:
     NUM_GOAL = 1
     TRY_COUNT = 20
     MOVE_SPEED = 1.2
-    TURN_SPEED = 0.3
+    TURN_SPEED = 1.0
     NUM_LIDAR = 314
