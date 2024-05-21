fd81be7361df824be8fe25debc9dbc0b012a95e5
masashi <masashi.lateolabrax@gmail.com>
2024-05-22 02:03:49 +0900

    update: add the graph showing the last parameters.

--- src/analizer/plot/movement.py
+++ src/analizer/plot/movement.py
@@ -9,19 +9,35 @@ def main():
     history = Hist.load(
         get_current_history("../../")
     )
+    dim = range(0, history.dim)  # range(0, history.dim)
+    generation = range(0, len(history.queues))  # range(0, len(history.queues))
 
-    prev_point = history.queues[0].min_para
-    x = np.arange(1, len(history.queues), dtype=np.uint)
-    data = np.zeros((len(history.queues) - 1,))
-    for i, q in enumerate(history.queues):
-        if i == 0:
-            continue
-        data[i - 1] = np.linalg.norm(prev_point - q.min_para)
-        prev_point = q.min_para
+    n = generation.stop - generation.start
+
+    movements = np.zeros((n - 1,))
+    for i in range(0, n - 1):
+        s = history.queues[generation.start + i + 1].min_para - history.queues[generation.start + i].min_para
+        movements[i] = np.linalg.norm(s)
+
+    parameters = history.queues[generation.stop - 1].min_para
+    std = np.std(parameters)
 
     fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1)
-    ax.plot(x, data, c="Blue")
+
+    ax1 = fig.add_subplot(1, 2, 1)
+    x = np.arange(generation.start, generation.stop - 1, dtype=np.uint64)
+    ax1.plot(x, movements)
+    ax1.set_xlabel("generations")
+    ax1.set_ylabel("movements")
+
+    ax2 = fig.add_subplot(1, 2, 2)
+    x = np.arange(dim.start, dim.stop, dtype=np.uint64)
+    ax2.bar(x, history.queues[generation.stop - 1].min_para)
+    ax2.plot(x, [std * 3] * len(x), c="Red")
+    ax2.plot(x, [std * -3] * len(x), c="Red")
+    ax2.set_xlabel("parameters")
+    ax2.set_ylabel("values")
+
     plt.show()
 
 
