a6d0073d13376cc33cb97a852e7fa7d698b417f8
mSuzuki <masashi.lateolabrax@gmail.com>
2024-06-05 11:42:03 +0900

    exp: verify the timestep of the pushing-food-with-pheromone task.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from exp.pushing_food_with_pheromone import main
 
-# Date: 2024-06-04 15:55
+# Date: 2024-06-05 11:42
 # Task: pushing-food-with-pheromone
 # Agent: 3
 # Food: 1
-# Objective: This experiment verify the task.
+# Objective: Test the timestep with a value of 0.01.
 
 if __name__ == '__main__':
     main()
