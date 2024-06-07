a7395df2e83255e4d3f31e479c60e606d0779767
masashi <d3237100024@std.kitami-it.ac.jp>
2024-06-07 20:29:31 +0900

    exp: experiment with limited robots that cannot turn their wheels backwards under the long time episode.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from exp.pushing_food_with_pheromone import main
 
-# Date: 2024-06-05 11:42
+# Date: 2024-06-07 20:13
 # Task: pushing-food-with-pheromone
 # Agent: 3
 # Food: 1
-# Objective: Test the timestep with a value of 0.01.
+# Objective: Train the robots to collect food using pheromone.
+# Contribution:
+#   - Limited Robots. They are allowed to turn their wheels only forward.
 
 if __name__ == '__main__':
     main()
