8a1d803b31a6837c08b6444634abafa1f6cae5b7
masashi <d3237100024@std.kitami-it.ac.jp>
2024-06-07 22:00:35 +0900

    exp: Experiment with the limited robots, which their wheels can turn only forward, under the long duration task.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from exp.pushing_food_with_pheromone import main
 
-# Date: 2024-06-07 20:13
+# Date: 2024-06-07 21:57
 # Task: pushing-food-with-pheromone
 # Agent: 3
 # Food: 1
 # Objective: Train the robots to collect food using pheromone.
 # Contribution:
 #   - Limited Robots. They are allowed to turn their wheels only forward.
+#   - Long duration task. The duration of the episode is 60 seconds.
 
 if __name__ == '__main__':
     main()
