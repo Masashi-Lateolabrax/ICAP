564a55a1acd7125f0964e85157314bd6a23beba5
mSuzuki <masashi.lateolabrax@gmail.com>
2024-06-17 02:03:36 +0900

    This task is **exactly** the same as a task of the old version.

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@ from exp.pushing_food_with_pheromone import main
 #   - The number of agent: 9
 #   - The number of food: 2
 #   - Optimizing generation: 300
-#   - This task is exactly the same as a task of the old version.
+#   - This task is **exactly** the same as a task of the old version.
 #
-# Date: 2024-06-14 16:46
+# Date: 2024-06-17 02:03
 
 if __name__ == '__main__':
     main()
