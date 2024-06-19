9a802b19620ddaffd3269ef7e6f4c1cf39895921
masashi <masashi.lateolabrax@gmail.com>
2024-06-19 15:49:49 +0900

    mod: debug function.

--- src/exp/pushing_food_with_pheromone/settings/brain.py
+++ src/exp/pushing_food_with_pheromone/settings/brain.py
@@ -1,31 +1,56 @@
 import torch
 from torch import nn
 
+from lib.analizer.neural_network import Debugger
+
 
 class NeuralNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, debug=False):
         super(NeuralNetwork, self).__init__()
+
+        self.debugger = Debugger(debug)
 
         self.sequence1 = nn.Sequential(
             nn.Flatten(0, -1),
+            self.debugger.create_investigator("s1l0"),
+
             nn.Linear(6, 12),
+            self.debugger.create_investigator("s1l1"),
             nn.Tanh(),
+
             nn.Linear(12, 6),
+            self.debugger.create_investigator("s1l2"),
             nn.Tanh(),
+
             nn.Linear(6, 3),
+            self.debugger.create_investigator("s1l3"),
             nn.Tanh(),
+
+            self.debugger.create_investigator("s1l4"),
         )
         self.sequence2 = nn.Sequential(
+            self.debugger.create_investigator("s2l0"),
+
             nn.Linear(1, 5),
+            self.debugger.create_investigator("s2l1"),
             nn.Tanh(),
+
             nn.Linear(5, 3),
+            self.debugger.create_investigator("s2l2"),
             nn.Tanh(),
+
+            self.debugger.create_investigator("s2l3"),
+        )
+        self.sequence3 = nn.Sequential(
+            self.debugger.create_investigator("s3l0"),
+            nn.Sigmoid(),
+            self.debugger.create_investigator("s3l1")
         )
 
     def forward(self, sight, pheromone) -> torch.Tensor:
         x1 = self.sequence1.forward(sight)
         x2 = self.sequence2.forward(pheromone)
-        return nn.functional.sigmoid(x1 + x2)
+        return self.sequence3.forward(x1 + x2)
 
     def load_para(self, para):
         with torch.no_grad():
