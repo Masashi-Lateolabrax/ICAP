dc65804bd1eee254931011968f9e6c483df8344a
masashi <masashi.lateolabrax@gmail.com>
2024-06-24 02:34:46 +0900

    test: the pushing food with pheromone task.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
-from exp.pushing_food import main
+from exp.pushing_food_with_pheromone import main
 
-# 2024-05-31 20:12
+# Task: CHECK THE PROGRAM
+#
+# Objective: Executing reproduction experiment.
+#
+# Features:
+#   - The number of agent: 9
+#   - The number of food: 2
+#   - Optimizing generation: 500
+#   - Optimizing population: 20
+#   - Increase Disabled the updating dummy objects for speed up.
+#
+# Date: 2024-06-23 12:41
 
 if __name__ == '__main__':
     main()
