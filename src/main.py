11463f58ccb9e62e5d97e974d07576a75f51b4ad
mSuzuki <d3237100024@std.kitami-it.ac.jp>
2024-06-23 12:43:22 +0900

    exp: pushing-food-with-pheromone. For speed up, disabled the updating dummy objects.

--- src/main.py
+++ src/main.py
@@ -8,10 +8,9 @@ from exp.pushing_food_with_pheromone import main
 #   - The number of agent: 9
 #   - The number of food: 2
 #   - Optimizing generation: 300
-#   - set the walls.
-#   - The amount of sensed pheromone are normalized.
+#   - Disabled the updating dummy objects for speed up.
 #
-# Date: 2024-06-21 02:14
+# Date: 2024-06-23 12:41
 
 if __name__ == '__main__':
     main()
