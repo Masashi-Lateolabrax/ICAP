32910b47676db66d2e5e74bc7026256e1fc47305
masashi <masashi.lateolabrax@gmail.com>
2024-06-24 12:12:28 +0900

    exp: the pushing food with pheromone task. Increased the number of generations for optimization.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from exp.pushing_food_with_pheromone import main
 
-# Task: CHECK THE PROGRAM
+# Task: pushing-food-with-pheromone
 #
 # Objective: Executing reproduction experiment.
 #
 # Features:
 #   - The number of agent: 9
 #   - The number of food: 2
-#   - Optimizing generation: 500
+#   - Optimizing generation: 700
 #   - Optimizing population: 20
-#   - Increase the generation and population of optimization.
+#   - Increase the number of generations for the optimization.
 #
-# Date: 2024-06-23 12:41
+# Date: 2024-06-23 12:12
 
 if __name__ == '__main__':
     main()
