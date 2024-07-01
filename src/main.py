9157025671ab7a7b7661868890974e34ff790136
masashi <masashi.lateolabrax@gmail.com>
2024-07-01 14:20:28 +0900

    exp: pushing-food-with-pheromone

--- src/main.py
+++ src/main.py
@@ -2,16 +2,18 @@ from exp.pushing_food_with_pheromone import main
 
 # Task: pushing-food-with-pheromone
 #
-# Objective: Executing reproduction experiment.
+# Objective: Detecting the elements contributing to the emergence of pheromone roles.
 #
 # Features:
 #   - The number of agent: 9
 #   - The number of food: 2
-#   - Optimizing generation: 700
-#   - Optimizing population: 20
-#   - Increase the number of generations for the optimization.
+#   - Optimizing generation: 500
+#   - Optimizing population: 100
+#   - Optimizing elite: 10
+#   - Normalized the input of pheromone
+#   - Remove the activation functions from the output layers in the NN modules
 #
-# Date: 2024-06-23 12:12
+# Date: 2024-07-01 14:20
 
 if __name__ == '__main__':
     main()
