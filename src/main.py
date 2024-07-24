81130924d30855e8aa47e8d7c0114f6bbac85203
masashi <masashi.lateolabrax@gmail.com>
2024-07-24 10:28:37 +0900

    exp: Pheromone Property Analysis - Evaporation vs. Liquid

--- src/main.py
+++ src/main.py
@@ -1,26 +1,26 @@
-from exp.the_simplest_task import main
+import os.path
+
+from scheme.pheromone_property_analysis.evaporation_vs_liquid import main
 
 # # Task
-# the simplest task
+# Pheromone Property Analysis
+#   Evaporation vs. Liquid
 #
 # # Objective
-# Detecting the factors that disrupt robot learning.
+# Analysing the maximum evaporation speed related to the amount of secreted pheromone.
 #
 # # Features
-#   - Agent: 1
-#   - Goal: 1
-#   - Optimizing generation: 300
-#   - Optimizing population: 17
-#   - Optimizing elite: 9
-#   - Episode: 120
-#   - LiDER perception: 0.0001
-#   - Loss function: gaussian distribution
+#   - EVAPORATION: 20.0
+#   - DECREASE: 0.1
+#   - DIFFUSION: 35.0
+#   - SATURATION_VAPOR: 10.0
+#   - LIQUID: [ 10,50,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000 ]
 #
 # # Difference
-#   - Increase the range of sensor perception
+#   - NONE
 #
 # # Date
-# 2024-07-03 13:12
+# 2024-07-24 10:27
 
 if __name__ == '__main__':
-    main()
+    main(os.path.dirname(__file__))
