update: Fix DataCollector class for calculation of dif_liquid.

src/scheme/pheromone_property_analysis/experiment/evaporation_vs_liquid/_data_collector.py
@@ -5,13 +5,15 @@ from lib.utils import BaseDataCollector
 from lib.analizer.recorder import Recorder
 
 from .settings import Settings
+from ._task import AnalysisEnvironment
 
 
 class DataCollector(BaseDataCollector):
     def __init__(self, working_directory):
         size = Settings.World.NUM_CELL
-        episode = int(Settings.Task.EPISODE_LENGTH / Settings.Simulation.PHEROMONE_TIMESTEP + 0.5)
-        self.liquid = np.zeros((episode, size[0], size[1]))
+        episode = int(Settings.Task.EPISODE_LENGTH / Settings.Simulation.TIMESTEP + 0.5)
+
+        self.dif_liquid = np.zeros(episode)
         self.gas = np.zeros((episode, size[0], size[1]))
 
         camera = mujoco.MjvCamera()
@@ -31,7 +33,13 @@ class DataCollector(BaseDataCollector):
     def get_episode_length(self) -> int:
         return self._recorder.get_episode_length()
 
-    def _record(self, task, time: int, evaluation: float):
-        self.liquid[time, :, :] = task.pheromone.get_all_liquid()
+    def pre_record(self, task, time: int):
+        self._recorder.pre_record(task, time)
+
+    def record(self, task: AnalysisEnvironment, time: int, evaluation: float):
+        self.dif_liquid[time] = task.dif_liquid
         self.gas[time, :, :] = task.pheromone.get_all_gas()
-        self._recorder._record(task, time, evaluation)
+        self._recorder.record(task, time, evaluation)
+
+    def release(self):
+        self._recorder.release()