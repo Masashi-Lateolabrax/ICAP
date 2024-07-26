09122e511718ff4e5521cf67663bdf23b925e051
masashi <masashi.lateolabrax@gmail.com>
2024-07-27 06:02:43 +0900

    exp: Pheromone Property Analysis; Evaporation vs. Liquid

--- src/main.py
+++ src/main.py
@@ -14,15 +14,16 @@ from scheme.pheromone_property_analysis.evaporation_vs_liquid import main
 #   - DECREASE: 0.1
 #   - DIFFUSION: 35.0
 #   - SATURATION_VAPOR: 10.0
-#   - LIQUID: [ 10,100,1000,10000,100000 ]
+#   - LIQUID: [ 10,100,200,400,600,800,1000,10000,100000 ]
 #   - TIMESTEP: 0.03
+#   - EPISODE: 180
+#   - SECRETING_PERIOD: 60
 #
 # # Difference
-#   - LIQUID
-#   - TIMESTEP
+#   - SECRETING_PERIOD
 #
 # # Date
-# 2024-07-24 12:21
+# 2024-07-26 18:35
 
 if __name__ == '__main__':
     main(os.path.dirname(__file__))
