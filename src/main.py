3865297de26104aaa96125b4cbe8c62b6003dd94
masashi <masashi.lateolabrax@gmail.com>
2024-07-27 23:11:26 +0900

    exp: Pheromone Property Analysis; Evaporation

--- src/main.py
+++ src/main.py
@@ -7,22 +7,23 @@ from scheme.pheromone_property_analysis.evaporation import main
 #   Evaporation
 #
 # # Objective
-# Analysing the effect of the evaporation coefficient effect to evaporation speed.
+# Analysing the effect of the evaporation coefficient effect on evaporation speed.
 #
 # # Features
-#   - EVAPORATION: [ 1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0 ]
+#   - EVAPORATION: [ 1.0,5.0,10.0,50.0,100.0,500.0,1000.0,5000.0,10000.0 ]
 #   - DECREASE: 0.1
 #   - DIFFUSION: 35.0
 #   - SATURATION_VAPOR: 10.0
-#   - LIQUID: 100000
+#   - LIQUID: 1000000000
 #   - TIMESTEP: 0.03
 #   - EPISODE: 120
 #
-# # Difference
-#   - Nothing
+# # Changes
+#   - Expand the EVAPORATION
+#   - To ensure a sufficient amount of LIQUID, increase LIQUID.
 #
 # # Date
-# 2024-07-27 09:49
+# 2024-07-27 23:11
 
 if __name__ == '__main__':
     main(os.path.dirname(__file__))
