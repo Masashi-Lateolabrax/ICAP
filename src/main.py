22d46459d0ac23761be4da61a85d48f33b39e088
masashi <masashi.lateolabrax@gmail.com>
2024-07-30 05:15:52 +0900

    exp: Pheromone Property Analysis; Evaporation

--- src/main.py
+++ src/main.py
@@ -14,16 +14,16 @@ from scheme.pheromone_property_analysis.evaporation import main
 #   - EVAPORATION: [i for i in range(5,151,5)]
 #   - DECREASE: 0.1
 #   - DIFFUSION: 35.0
-#   - SATURATION_VAPOR: 10.0
+#   - SATURATION_VAPOR: 15.0
 #   - LIQUID: 1000000000
 #   - TIMESTEP: 0.03
 #   - EPISODE: 120
 #
 # # Changes
-#   - Units of Evaporation Speed [/step -> /seconds]
+#   - Change SATURATION_VAPOR to 15.0.
 #
 # # Date
-# 2024-07-30 04:28
+# 2024-07-30 05:14
 
 if __name__ == '__main__':
     main(time.strftime("%Y%m%d_%H%M%S"), os.path.dirname(__file__))
