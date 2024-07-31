d52f59a30e1678eaabf6aff6c4453f45bc00f6a0
mSuzuki <masashi.lateolabrax@gmail.com>
2024-07-31 21:49:38 +0900

    exp: Pheromone Property Analysis; Evaporation Speed Curve

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import os.path
 import time
 
-from scheme.pheromone_property_analysis.evaporation import main
+from scheme.pheromone_property_analysis.sv_vs_ev_speed_curve import main
 
 # # Task
 # Pheromone Property Analysis
-#   Evaporation
+#   Evaporation Speed Curve
 #
 # # Objective
 # Analysing the effect of the evaporation coefficient effect on evaporation speed.
@@ -14,16 +14,16 @@ from scheme.pheromone_property_analysis.evaporation import main
 #   - EVAPORATION: [i for i in range(5,151,5)]
 #   - DECREASE: 0.1
 #   - DIFFUSION: 35.0
-#   - SATURATION_VAPOR: 10.0
+#   - SATURATION_VAPOR: [i for i in range(1,10)]
 #   - LIQUID: 1000000000
 #   - TIMESTEP: 0.03
 #   - EPISODE: 120
 #
 # # Changes
-#   - Units of Evaporation Speed [/step -> /seconds]
+#   - Nothing
 #
 # # Date
-# 2024-07-30 04:28
+# 2024-07-31 21:49
 
 if __name__ == '__main__':
     main(time.strftime("%Y%m%d_%H%M%S"), os.path.dirname(__file__))
