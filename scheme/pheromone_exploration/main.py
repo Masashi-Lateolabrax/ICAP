c3d88947eda923116940276d19f886e5ba700355
mSuzuki <masashi.lateolabrax@gmail.com>
2024-09-25 19:58:25 +0900

    rem: Remove analyse_charactor() and prepare_workdir().

--- scheme/pheromone_exploration/main.py
+++ scheme/pheromone_exploration/main.py
@@ -6,20 +6,6 @@ import warnings
 from settings import Settings
 
 
-def prepare_workdir(current_dir):
-    from datetime import datetime
-    from libs.utils import get_head_hash
-
-    head_hash = get_head_hash()[0:8]
-    current_time_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-    workdir = os.path.join(current_dir, f"results/{current_time_str}_{head_hash}")
-
-    if not os.path.exists(workdir):
-        os.makedirs(workdir)
-
-    return workdir
-
-
 def prepare_dir(current_dir, specify: str = None):
     from libs.utils import get_head_hash
 
@@ -52,23 +38,6 @@ def main(workdir):
         data_inc, data_dec = src.dump(case_dir, para)
         src.record(data_inc, data_dec, case_dir)
         src.analysis2(case_dir, data_inc, data_dec)
-
-
-def analyse_charactor(workdir):
-    import scheme.pheromone_exploration.src as src
-
-    para = src.convert_characteristic(
-        sv=10,
-        evaporation=20,
-        decrease=0.1,
-        diffusion=35
-    )
-
-    data_inc = src.IncreaseData(para)
-    data_dec = src.DecreaseData(data_inc)
-
-    src.recode(para, data_inc, data_dec, workdir)
-    src.analysis(workdir, para, data_inc, data_dec)
 
 
 if __name__ == '__main__':
