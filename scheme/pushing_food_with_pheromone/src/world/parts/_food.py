a82d405e8bbd18dbfa7a8fdcf0a01d9ed85fbce1
masashi <masashi.lateolabrax@gmail.com>
2024-10-08 14:54:31 +0900

    add: Food:is_valid()

--- scheme/pushing_food_with_pheromone/src/world/parts/_food.py
+++ scheme/pushing_food_with_pheromone/src/world/parts/_food.py
@@ -6,3 +6,6 @@ from ._environment import Environment
 class Food(GeomObject):
     def __init__(self, env: Environment, food_id: int):
         super().__init__(env.get_model(), env.get_data(), f"food{food_id}")
+
+    def is_valid(self):
+        return self.get_geom().xpos[2] > 0
