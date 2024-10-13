dbdc50cfe1df000a818f9840df8c1f0fa166b1de
masashi <masashi.lateolabrax@gmail.com>
2024-10-14 04:49:20 +0900

    mov: Remove exceed codes.

--- scheme/pushing_food_with_pheromone/main.py
+++ scheme/pushing_food_with_pheromone/main.py
@@ -2,32 +2,7 @@ import warnings
 from datetime import datetime
 import shutil
 
-import numpy as np
-
 import os
-import copy
-import pickle
-import concurrent.futures
-from multiprocessing import Manager
-
-
-def prepare_workdir(current_dir, specify: str = None):
-    from libs.utils import get_head_hash
-
-    head_hash = get_head_hash()[0:8]
-
-    if specify is None:
-        current_time_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-        workdir = os.path.join(current_dir, f"results/{current_time_str}_{head_hash}")
-    else:
-        workdir = os.path.join(current_dir, f"results/{specify}")
-        if head_hash != specify[-8:]:
-            warnings.warn("Your specified git hash don't match the HEAD's git hash.")
-
-    if not os.path.exists(workdir):
-        os.makedirs(workdir)
-
-    return workdir
 
 
 def prepare_dir(current_dir, specify: str = None):
@@ -55,48 +30,6 @@ def main(workdir):
 
     best_para = src.optimization(workdir)
     src.sampling(workdir, best_para)
-
-
-def rec_only(workdir):
-    from libs.optimizer import Hist
-
-    import scheme.pushing_food_with_pheromone.src as src
-
-    git_hash = os.path.basename(workdir)[-8:]
-
-    hist = Hist.load(os.path.join(workdir, f"history_{git_hash}.npz"))
-
-    # for i in [len(hist.queues) - 1]:
-    #     gen_dir = os.path.join(workdir, str(i))
-    #     os.makedirs(gen_dir, exist_ok=True)
-    #     para = hist.queues[i].min_para
-    #     src.record(para, gen_dir)
-
-    for name in ["test"]:
-        gen_dir = os.path.join(workdir, name)
-        os.makedirs(gen_dir, exist_ok=True)
-        para = hist.get_max().max_para
-        src.record(para, gen_dir)
-
-
-def analysis_only(work_dir):
-    from libs.optimizer import Hist
-
-    import scheme.pushing_food_with_pheromone.src as src
-
-    git_hash = os.path.basename(work_dir)[-8:]
-
-    hist = Hist.load(os.path.join(work_dir, f"history_{git_hash}.npz"))
-
-    # for i in [len(hist.queues) - 1]:
-    #     gen_dir = os.path.join(work_dir, str(i))
-    #     os.makedirs(gen_dir, exist_ok=True)
-    #     src.analysis2(gen_dir, hist)
-
-    for name in ["test"]:
-        gen_dir = os.path.join(work_dir, name)
-        os.makedirs(gen_dir, exist_ok=True)
-        src.analysis2(gen_dir, hist)
 
 
 def sampling(workdir):
