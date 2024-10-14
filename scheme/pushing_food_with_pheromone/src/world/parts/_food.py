2ddf101da968d0ab918210204a6400321d661867
masashi <masashi.lateolabrax@gmail.com>
2024-10-15 04:26:37 +0900

    mod: Refactor

--- scheme/pushing_food_with_pheromone/src/world/parts/_food.py
+++ scheme/pushing_food_with_pheromone/src/world/parts/_food.py
@@ -6,6 +6,3 @@ from ._environment import Environment
 class Food(BodyObject):
     def __init__(self, env: Environment, food_id: int):
         super().__init__(env.get_model(), env.get_data(), f"food{food_id}")
-
-    def is_valid(self):
-        return self.get_body().xpos[2] > 0
