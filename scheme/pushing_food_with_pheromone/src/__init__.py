e6c27331cad91f7982c6c80b38f329e9f02e8f65
masashi <masashi.lateolabrax@gmail.com>
2024-10-14 21:55:50 +0900

    mod: Clean up the removed functions from __init__.py .

--- scheme/pushing_food_with_pheromone/src/__init__.py
+++ scheme/pushing_food_with_pheromone/src/__init__.py
@@ -3,5 +3,4 @@ from .logger import Logger, LogLoader
 from .task import Task
 from .task_generator import TaskGenerator
 
-from .entry_point import optimization, analysis, record, sampling, plot_loss, plot_element_evaluation
-# from .entry_point import analysis2
+from .entry_point import optimization, sampling
