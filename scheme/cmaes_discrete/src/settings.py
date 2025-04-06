88a6fb2bd43f86e0b128e1424168fbd09960202a
masashi <masashi.lateolabrax@gmail.com>
2025-04-06 11:36:52 +0900

    mod: Change the number of robot from 3 to 1.

--- scheme/cmaes_discrete/src/settings.py
+++ scheme/cmaes_discrete/src/settings.py
@@ -21,5 +21,5 @@ Settings.Robot.THINK_INTERVAL = 1
 Settings.Robot.ARGMAX_SELECTION = False
 Settings.Robot.FoodSensor.GAIN = 1
 
-Settings.Robot.NUM = 3
+Settings.Robot.NUM = 1
 Settings.Food.NUM = 1
