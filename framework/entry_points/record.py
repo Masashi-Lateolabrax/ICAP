2f27bca1c471fec9d854699dd3be76c0c7cf88a1
masashi <masashi.lateolabrax@gmail.com>
2025-04-07 23:21:57 +0900

    mod: Add the dump functionality to the record function.

--- framework/entry_points/record.py
+++ framework/entry_points/record.py
@@ -1,7 +1,6 @@
-import os
-
 import mujoco
 
+import framework
 from libs import optimizer
 from libs.utils.data_collector import Recorder
 
@@ -10,9 +9,29 @@ from ..interfaceis import BrainBuilder
 from ..task_generator import TaskGenerator
 
 
-def record(settings: Settings, save_dir, para: optimizer.Individual, brain_builder: BrainBuilder):
+def record(
+        settings: Settings,
+        save_dir,
+        para: optimizer.Individual,
+        brain_builder: BrainBuilder,
+        debug=False
+) -> framework.Dump | None:
+    """
+    Record the simulation of an individual.
+
+    Args:
+        settings (Settings): The settings object.
+        save_dir (str): The directory to save the recorded data.
+        para (optimizer.Individual): The individual to be recorded.
+        brain_builder (BrainBuilder): The brain builder object.
+        debug (bool): Whether to run in debug mode.
+
+    Returns:
+        framework.Dump | None: The dump object if debug is True, otherwise None.
+    """
+
     generator = TaskGenerator(settings, brain_builder)
-    task = generator.generate(para, debug=True)
+    task = generator.generate(para, debug=debug)
 
     camera = mujoco.MjvCamera()
     camera.elevation = -90
@@ -23,10 +42,12 @@ def record(settings: Settings, save_dir, para: optimizer.Individual, brain_build
         settings.Simulation.TIME_LENGTH,
         settings.Simulation.RENDER_WIDTH,
         settings.Simulation.RENDER_HEIGHT,
-        os.path.dirname(save_dir),
+        save_dir,
         camera,
         Settings.Simulation.MAX_GEOM
     )
 
     recorder.run(task)
     recorder.release()
+
+    return task.get_dump_data() if debug else None
