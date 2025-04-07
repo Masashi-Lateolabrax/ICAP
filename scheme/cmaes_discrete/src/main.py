4615eb6c2505b9eae673f968a9a24cc72de34674
masashi <masashi.lateolabrax@gmail.com>
2025-04-07 14:16:12 +0900

    mod: Made the necessary changes to the main code according to the Logger update.

--- scheme/cmaes_discrete/src/main.py
+++ scheme/cmaes_discrete/src/main.py
@@ -26,7 +26,7 @@ def main():
     settings.Robot.ARGMAX_SELECTION = True
     if logger.is_empty():
         logger = Logger.load(os.path.join(save_dir, log_file_name))
-    para = logger.get_min().min_para
+    para = logger.get_min().min_ind
     dump = framework.entry_points.record(settings, save_dir, para, brain_builder, debug=True)
 
     analysis.plot_loss(settings, dump, os.path.join(save_dir, "loss.png"))
