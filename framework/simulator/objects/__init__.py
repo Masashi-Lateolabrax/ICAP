f71bdb7d1cf03e2082a3181d15e0e3c2f594c26e
masashi <masashi.lateolabrax@gmail.com>
2025-04-09 03:40:20 +0900

    mod: Update __init__.py to import CRobot and CRobotBuilder

--- framework/simulator/objects/__init__.py
+++ framework/simulator/objects/__init__.py
@@ -1,3 +1,3 @@
-from .robot import Robot, RobotBuilder, RobotInput
+from .robot import Robot, RobotBuilder, RobotInput, CRobot, CRobotBuilder
 from .food import Food, FoodBuilder, ReFood
 from .nest import Nest, NestBuilder
--- framework/simulator/objects/robot/__init__.py
+++ framework/simulator/objects/robot/__init__.py
@@ -2,5 +2,10 @@ from .actuator import Actuator
 from .robot_property import RobotProperty
 from .robot_input import RobotInput
 
+from .const import ROBOT_SIZE
+
 from .robot import Robot
-from .robot_builder import ROBOT_SIZE, RobotBuilder
+from .robot_builder import RobotBuilder
+
+from .crobot import CRobot
+from .crobot_builder import CRobotBuilder
