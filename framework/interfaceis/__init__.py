6b93b9af1b4e9066d6346410520edeca2f066e72
masashi <masashi.lateolabrax@gmail.com>
2025-04-09 01:56:12 +0900

    mod: Introduce CBrainInterface and CBrainBuilder classes

--- framework/interfaceis/__init__.py
+++ framework/interfaceis/__init__.py
@@ -1,2 +1,2 @@
-from .robot.brain import BrainJudgement, BrainInterface, BrainBuilder
+from .robot.brain import BrainJudgement, BrainInterface, BrainBuilder, CBrainInterface, CBrainBuilder
 from .loss import Loss
