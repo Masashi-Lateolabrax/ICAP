6806b4e359e79146b8eabc9f39fe1a471de5ed50
masashi <masashi.lateolabrax@gmail.com>
2025-04-09 02:49:01 +0900

    add: Create settings

---
+++ scheme/cmaes_crobot_simple/src/settings.py
@@ -0,0 +1,26 @@
+from framework import Settings
+from loss import Loss
+
+Settings.Simulation.RENDER_WIDTH = 800
+Settings.Simulation.RENDER_HEIGHT = 800
+Settings.Simulation.RENDER_ZOOM = 10
+
+Settings.CMAES.GENERATION = 300
+Settings.CMAES.POPULATION = 1000
+Settings.CMAES.MU = int(Settings.CMAES.POPULATION / 2)
+Settings.CMAES.SIGMA = 1
+
+Settings.Simulation.TIME_STEP = 0.01
+Settings.Simulation.TIME_LENGTH = int(30 / Settings.Simulation.TIME_STEP)
+
+Settings.CMAES.LOSS = Loss()
+
+Settings.Simulation.WORLD_WIDTH = 8
+Settings.Simulation.WORLD_HEIGHT = 8
+
+Settings.Robot.THINK_INTERVAL = 1
+Settings.Robot.ARGMAX_SELECTION = False
+Settings.Robot.FoodSensor.GAIN = 1
+
+Settings.Robot.NUM = 3
+Settings.Food.NUM = 1
