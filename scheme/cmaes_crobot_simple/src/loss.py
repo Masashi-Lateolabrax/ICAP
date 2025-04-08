de5904b95e117b0b0a7498ba5f370e9ee72bac34
masashi <masashi.lateolabrax@gmail.com>
2025-04-09 02:48:28 +0900

    add: Implement Loss class

---
+++ scheme/cmaes_crobot_simple/src/loss.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+import framework
+
+
+def _calc_loss_sigma(point, value):
+    return np.sqrt(-(point ** 2) / np.log(value))
+
+
+class Loss(framework.interfaceis.Loss):
+    def __init__(self):
+        self.sigma_nest_and_food = 1
+        self.sigma_robot_and_food = _calc_loss_sigma(3, 0.1)
+
+        self.gain_nest_and_food = 0.001
+        self.gain_robot_and_food = _calc_loss_sigma(1, 0.5)
+
+    def calc_r_loss(self, robot_pos: np.ndarray, food_pos: np.ndarray) -> float:
+        # Evaluate the distance between robots and food
+        dist_r = np.sum((robot_pos[:, :2] - food_pos) ** 2, axis=1)
+        loss_r = -np.average(np.exp(-dist_r / self.sigma_robot_and_food))
+
+        return self.gain_robot_and_food * loss_r
+
+    def calc_n_loss(self, nest_pos: np.ndarray, food_pos: np.ndarray) -> float:
+        # Evaluate the distance between nest and food
+        dist_n = np.sum((food_pos - nest_pos) ** 2, axis=1)
+        loss_n = -np.average(np.exp(-dist_n / self.sigma_nest_and_food))
+
+        return self.gain_nest_and_food * loss_n
+
+    def calc_loss(self, nest_pos: np.ndarray, robot_pos: np.ndarray, food_pos: np.ndarray) -> float:
+        loss_r = self.calc_r_loss(robot_pos, food_pos)
+        loss_n = self.calc_n_loss(nest_pos, food_pos)
+        return loss_r + loss_n
+
+    def __getstate__(self):
+        return self.__dict__
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
