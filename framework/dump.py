7647a065bfff0e4b8b5f185b83eb1c29a19b4f2c
masashi <masashi.lateolabrax@gmail.com>
2025-04-12 20:27:59 +0900

    Refactor Dump class to use dataclasses for better structure and clarity

--- framework/dump.py
+++ framework/dump.py
@@ -1,27 +1,45 @@
-import copy
+import dataclasses
 
 import numpy as np
 import pickle
 
 
 class Dump:
-    def __init__(self, file_path: str = None):
-        self.robot_pos = []
-        self.robot_outputs = []
-        self.food_pos = []
+    @dataclasses.dataclass
+    class Delta:
+        robot_outputs: dict[str, np.ndarray] = dataclasses.field(default_factory=dict)
+        robot_pos: dict[str, np.ndarray] = dataclasses.field(default_factory=dict)
+        food_pos: np.ndarray = np.zeros(0)
 
+    def __init__(self, file_path: str = None):
+        self.deltas = []
         if file_path is not None:
             with open(file_path, 'rb') as f:
-                this = pickle.load(f)
-            self.robot_pos = this.robot_pos
-            self.robot_outputs = this.robot_outputs
-            self.food_pos = this.food_pos
+                self.deltas = pickle.load(f)
 
-    def dump(self, robot_pos: np.ndarray, robot_outputs: dict[str, np.ndarray], food_pos: np.ndarray):
-        self.robot_pos.append(robot_pos.copy())
-        self.robot_outputs.append(copy.copy(robot_outputs))
-        self.food_pos.append(food_pos.copy())
+        self.deltas.append(Dump.Delta())
+
+    def record_robot_pos(self, name: str, pos: np.ndarray):
+        self.deltas[-1].robot_pos[name] = pos.copy()
+
+    def record_robot_outputs(self, name: str, output: np.ndarray):
+        self.deltas[-1].robot_outputs[name] = output.copy()
+
+    def record_food_pos(self, food_pos: np.ndarray):
+        self.deltas[-1].food_pos = food_pos.copy()
+
+    def step(self):
+        self.deltas.append(Dump.Delta())
 
     def save(self, file_path):
         with open(file_path, 'wb') as f:
-            pickle.dump(self, f)
+            pickle.dump(self.deltas, f)
+
+    def __getitem__(self, item):
+        return self.deltas[item]
+
+    def __len__(self):
+        return len(self.deltas)
+
+    def __iter__(self):
+        return iter(self.deltas)
