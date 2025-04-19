3feaeeb51e54943861968b61d4fd9e811d7fccf4
masashi <masashi.lateolabrax@gmail.com>
2025-04-19 22:02:27 +0900

    mod: Refactor loss calculations to follow framework changes.

--- scheme/cmaes_crobot_simple/src/loss.py
+++ scheme/cmaes_crobot_simple/src/loss.py
@@ -1,41 +1,46 @@
 import numpy as np
 
 import framework
+from framework.simulator.objects.robot import ROBOT_SIZE
+from framework.simulator.objects.food import FOOD_SIZE
+from framework.simulator.objects.nest import NEST_SIZE
 
 
 def _calc_loss_sigma(point, value):
-    return np.sqrt(-(point ** 2) / np.log(value))
+    return -(point ** 2) / np.log(value)
 
 
-class Loss(framework.interfaceis.Loss):
-    def __init__(self):
-        self.sigma_nest_and_food = 1
-        self.sigma_robot_and_food = _calc_loss_sigma(3, 0.1)
+class Loss(framework.interfaces.Loss):
+    offset_nest_and_food = NEST_SIZE + FOOD_SIZE
+    sigma_nest_and_food = _calc_loss_sigma(4, 0.01)
+    GAIN_NEST_AND_FOOD = 1
 
-        self.gain_nest_and_food = 0.001
-        self.gain_robot_and_food = _calc_loss_sigma(1, 0.5)
+    offset_robot_and_food = ROBOT_SIZE + FOOD_SIZE
+    sigma_robot_and_food = _calc_loss_sigma(1, 0.3)
+    GAIN_ROBOT_AND_FOOD = 0.01
 
-    def calc_r_loss(self, robot_pos: np.ndarray, food_pos: np.ndarray) -> float:
-        # Evaluate the distance between robots and food
-        dist_r = np.sum((robot_pos[:, :2] - food_pos) ** 2, axis=1)
-        loss_r = -np.average(np.exp(-dist_r / self.sigma_robot_and_food))
+    def _calc_r_loss(self, robot_pos: np.ndarray, food_pos: np.ndarray) -> float:
+        distance = np.max(
+            np.linalg.norm(robot_pos[:, :2] - food_pos, axis=1) - self.offset_robot_and_food,
+            0
+        )
+        loss = -np.average(np.exp(-(distance ** 2) / self.sigma_robot_and_food))
+        return self.GAIN_ROBOT_AND_FOOD * loss
 
-        return self.gain_robot_and_food * loss_r
+    def _calc_n_loss(self, nest_pos: np.ndarray, food_pos: np.ndarray) -> float:
+        distance = np.max(
+            np.linalg.norm(food_pos - nest_pos, axis=1) - self.offset_nest_and_food,
+            0
+        )
+        loss = -np.average(np.exp(-(distance ** 2) / self.sigma_nest_and_food))
+        return self.GAIN_NEST_AND_FOOD * loss
 
-    def calc_n_loss(self, nest_pos: np.ndarray, food_pos: np.ndarray) -> float:
-        # Evaluate the distance between nest and food
-        dist_n = np.sum((food_pos - nest_pos) ** 2, axis=1)
-        loss_n = -np.average(np.exp(-dist_n / self.sigma_nest_and_food))
+    def calc_elements(self, nest_pos: np.ndarray, robot_pos: np.ndarray, food_pos: np.ndarray) -> np.ndarray:
+        return np.array([
+            self._calc_r_loss(robot_pos, food_pos),
+            self._calc_n_loss(nest_pos, food_pos)
+        ])
 
-        return self.gain_nest_and_food * loss_n
-
-    def calc_loss(self, nest_pos: np.ndarray, robot_pos: np.ndarray, food_pos: np.ndarray) -> float:
-        loss_r = self.calc_r_loss(robot_pos, food_pos)
-        loss_n = self.calc_n_loss(nest_pos, food_pos)
-        return loss_r + loss_n
-
-    def __getstate__(self):
-        return self.__dict__
-
-    def __setstate__(self, state):
-        self.__dict__.update(state)
+    @staticmethod
+    def num_elements():
+        return 2
