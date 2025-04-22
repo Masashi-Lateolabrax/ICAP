cbe328ae95a0a4f6d91214d66a0d4b9694002bfc
masashi <masashi.lateolabrax@gmail.com>
2025-04-22 15:44:14 +0900

    mod: Update analysis.py in response to framework update.

--- scheme/cmaes_crobot_simple/src/analysis.py
+++ scheme/cmaes_crobot_simple/src/analysis.py
@@ -1,5 +1,7 @@
+from libs.optimizer import Individual
+
 import framework
-from framework.anaylysis import record_in_mp4, plot_parameter_movements
+from framework.analysis import plot_parameter_movements, LabelAndColor
 
 from loss import Loss
 from settings import Settings
@@ -7,25 +9,48 @@ from brain import BrainBuilder
 from logger import Logger
 
 
-def plot_loss(file_path: str, dump: framework.Dump):
-    framework.anaylysis.plot_loss(
+def plot_loss(file_path: str, ind: Individual):
+    framework.analysis.plot_loss(
         file_path,
         Settings(),
-        dump,
-        Loss()
+        ind,
+        Loss(),
+        labels={
+            0: LabelAndColor(label="robot and food", color="blue"),
+            1: LabelAndColor(label="food and nest", color="orange"),
+        }
+    )
+
+
+def record_in_mp4(save_dir: str, logger: Logger, brain_builder: BrainBuilder):
+    settings = Settings()
+    settings.Robot.ARGMAX_SELECTION = True
+
+    framework.analysis.record_in_mp4(
+        save_dir,
+        settings,
+        logger,
+        Loss(),
+        brain_builder,
+        labels={
+            0: LabelAndColor(label="robot and food", color="blue"),
+            1: LabelAndColor(label="food and nest", color="orange"),
+        }
     )
 
 
 def test_suboptimal_individuals(
-        save_dir: str,
+        file_path: str,
         logger: Logger,
         brain_builder: BrainBuilder,
 ):
     settings = Settings()
     settings.Robot.ARGMAX_SELECTION = True
+
     task_generator = framework.TaskGenerator(settings, brain_builder)
-    return framework.anaylysis.test_suboptimal_individuals(
-        save_dir,
+
+    return framework.analysis.test_suboptimal_individuals(
+        file_path,
         Settings(),
         logger,
         task_generator
