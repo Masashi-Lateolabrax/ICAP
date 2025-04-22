f113a5aa3920d53b0dda5697268d4574e8c730cf
masashi <masashi.lateolabrax@gmail.com>
2025-04-22 12:11:03 +0900

    mod: Update analysis.py in response to framework changes.

--- scheme/multi-net/src/analysis.py
+++ scheme/multi-net/src/analysis.py
@@ -1,5 +1,5 @@
 import framework
-from framework.anaylysis import record_in_mp4, plot_parameter_movements
+from framework.analysis import plot_parameter_movements, LabelAndColor
 
 from loss import Loss
 from settings import Settings
@@ -8,11 +8,32 @@ from logger import Logger
 
 
 def plot_loss(file_path: str, dump: framework.Dump):
-    framework.anaylysis.plot_loss(
+    framework.analysis.plot_loss(
         file_path,
         Settings(),
         dump,
-        Loss()
+        Loss(),
+        labels={
+            0: LabelAndColor(label="robot and food", color="blue"),
+            1: LabelAndColor(label="food and nest", color="orange"),
+        }
+    )
+
+
+def record_in_mp4(save_dir: str, logger: Logger, brain_builder: BrainBuilder):
+    settings = Settings()
+    settings.Robot.ARGMAX_SELECTION = True
+
+    framework.analysis.record_in_mp4(
+        save_dir,
+        settings,
+        logger,
+        Loss(),
+        brain_builder,
+        labels={
+            0: LabelAndColor(label="robot and food", color="blue"),
+            1: LabelAndColor(label="food and nest", color="orange"),
+        }
     )
 
 
@@ -23,8 +44,10 @@ def test_suboptimal_individuals(
 ):
     settings = Settings()
     settings.Robot.ARGMAX_SELECTION = True
+
     task_generator = framework.TaskGenerator(settings, brain_builder)
-    return framework.anaylysis.test_suboptimal_individuals(
+
+    return framework.analysis.test_suboptimal_individuals(
         save_dir,
         Settings(),
         logger,
