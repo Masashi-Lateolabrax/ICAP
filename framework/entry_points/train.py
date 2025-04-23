2818249d79828826e3bd91319c68a2774f4c26d1
masashi <masashi.lateolabrax@gmail.com>
2025-04-24 01:59:02 +0900

    add: Implement additional_train function

--- framework/entry_points/train.py
+++ framework/entry_points/train.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from libs import optimizer
 
 from ..settings import Settings
@@ -18,3 +20,25 @@ def train(settings: Settings, logger: optimizer.Logger, brain_builder: BrainBuil
         task_generator = TaskGenerator(settings, brain_builder)
         cmaes.optimize_current_generation(task_generator, proc=optimizer.MultiThreadProc)
         # cmaes.optimize_current_generation(task_generator, proc=optimizer.OneThreadProc)
+
+
+def additional_train(
+        settings: Settings,
+        logger: optimizer.Logger,
+        brain_builder: BrainBuilder,
+        centroid: np.ndarray,
+        cmatrix: np.ndarray,
+):
+    cmaes = optimizer.CMAES(
+        dim=brain_builder.get_dim(),
+        generation=settings.CMAES.GENERATION,
+        population=settings.CMAES.POPULATION,
+        mu=settings.CMAES.MU,
+        sigma=float(np.average(cmatrix)),
+        logger=logger,
+        centroid=centroid,
+        cmatrix=cmatrix
+    )
+    for gen in range(1, 1 + cmaes.get_generation()):
+        task_generator = TaskGenerator(settings, brain_builder)
+        cmaes.optimize_current_generation(task_generator, proc=optimizer.MultiThreadProc)
