b7e8b3affb198d6e081e5621250a8a0c3217049c
masashi <masashi.lateolabrax@gmail.com>
2025-04-25 13:43:05 +0900

    mod: Rename `_calc_loss_sigma` to `calc_loss_sigma` to public it.

--- scheme/crobot_swarm_schedule/src/loss.py
+++ scheme/crobot_swarm_schedule/src/loss.py
@@ -3,20 +3,19 @@ import numpy as np
 import framework
 from framework.simulator.objects.robot import ROBOT_SIZE
 from framework.simulator.objects.food import FOOD_SIZE
-from framework.simulator.objects.nest import NEST_SIZE
 
 
-def _calc_loss_sigma(point, value):
+def calc_loss_sigma(point, value):
     return -(point ** 2) / np.log(value)
 
 
 class Loss(framework.interfaces.Loss):
     offset_nest_and_food = 0
-    sigma_nest_and_food = _calc_loss_sigma(4, 0.01)
+    sigma_nest_and_food = calc_loss_sigma(4, 0.01)
     GAIN_NEST_AND_FOOD = 1
 
     offset_robot_and_food = ROBOT_SIZE + FOOD_SIZE
-    sigma_robot_and_food = _calc_loss_sigma(1, 0.3)
+    sigma_robot_and_food = calc_loss_sigma(1, 0.3)
     GAIN_ROBOT_AND_FOOD = 0.01
 
     def _calc_r_loss(self, robot_pos: np.ndarray, food_pos: np.ndarray) -> float:
