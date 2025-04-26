9d241051ebef02dca37f5cf9e8fcea372dcbe5a3
masashi <masashi.lateolabrax@gmail.com>
2025-04-26 16:35:37 +0900

    mod: Resolve conflicts against crobot-swarm-fix branch.

---
+++ scheme/crobot_swarm_fix/src/loss.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+import framework
+from framework.simulator.const import ROBOT_SIZE, FOOD_SIZE
+
+
+def calc_loss_sigma(point, value):
+    return -(point ** 2) / np.log(value)
+
+
+class Loss(framework.interfaces.Loss):
+    offset_nest_and_food = 0
+    sigma_nest_and_food = calc_loss_sigma(4, 0.01)
+    GAIN_NEST_AND_FOOD = 1
+
+    offset_robot_and_food = ROBOT_SIZE + FOOD_SIZE
+    sigma_robot_and_food = calc_loss_sigma(1, 0.3)
+    GAIN_ROBOT_AND_FOOD = 0.01
+
+    def _calc_r_loss(self, robot_pos: np.ndarray, food_pos: np.ndarray) -> float:
+        subs = (robot_pos[:, None, :2] - food_pos[None, :, :]).reshape(-1, 2)
+        distance = np.max(
+            np.linalg.norm(subs, axis=1) - self.offset_robot_and_food,
+            0
+        )
+        loss = -np.average(np.exp(-(distance ** 2) / self.sigma_robot_and_food))
+        return self.GAIN_ROBOT_AND_FOOD * loss
+
+    def _calc_n_loss(self, nest_pos: np.ndarray, food_pos: np.ndarray) -> float:
+        distance = np.max(
+            np.linalg.norm(food_pos - nest_pos, axis=1) - self.offset_nest_and_food,
+            0
+        )
+        loss = -np.average(np.exp(-(distance ** 2) / self.sigma_nest_and_food))
+        return self.GAIN_NEST_AND_FOOD * loss
+
+    def calc_elements(self, nest_pos: np.ndarray, robot_pos: np.ndarray, food_pos: np.ndarray) -> np.ndarray:
+        return np.array([
+            self._calc_r_loss(robot_pos, food_pos),
+            self._calc_n_loss(nest_pos, food_pos)
+        ])
+
+    @staticmethod
+    def num_elements():
+        return 2
---
+++ scheme/crobot_swarm_fix/src/main.py
@@ -0,0 +1,67 @@
+import os.path
+import datetime
+
+from libs import utils
+import framework
+
+from logger import Logger
+from brain import BrainBuilder
+from settings import Settings, set_positions
+
+import analysis
+
+
+def main():
+    git_hash = utils.get_head_hash()[0:8]
+    save_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        datetime.datetime.now().strftime("%Y%m%d_%H%M%S-") + git_hash
+    )
+    # save_dir = "20250423_103010-7457c6a6"
+    os.makedirs(save_dir, exist_ok=True)
+
+    log_file_name = "result.pkl"
+
+    settings = Settings()
+    set_positions(settings)
+
+    brain_builder = BrainBuilder(settings)
+    logger = Logger(save_dir)
+
+    ## Training.
+    if not os.path.exists(os.path.join(save_dir, log_file_name)):
+        framework.entry_points.train(settings, logger, brain_builder)
+        logger.save(log_file_name)
+
+    ## Load the logger if it is empty.
+    if logger.is_empty():
+        logger = Logger.load(os.path.join(save_dir, log_file_name))
+
+    analyze_results(logger, os.path.join(save_dir, "stage1"), brain_builder)
+
+
+def analyze_results(logger, save_dir, brain_builder):
+    os.makedirs(save_dir, exist_ok=True)
+
+    ## Plot the movements of the parameters.
+    file_path = os.path.join(save_dir, "parameter_movement.png")
+    if not os.path.exists(file_path):
+        analysis.plot_parameter_movements(file_path, logger)
+
+    file_path = os.path.join(save_dir, "test_loss.png")
+    if not os.path.exists(file_path):
+        analysis.test_suboptimal_individuals(
+            file_path, logger, brain_builder
+        )
+
+    file_path = os.path.join(save_dir, "videos")
+    if not os.path.exists(file_path):
+        analysis.record_in_mp4(
+            file_path, logger, brain_builder
+        )
+
+    # analysis.plot_max_of_parameter(os.path.join(save_dir, "parameter_info.png"), logger, 0, 100)
+
+
+if __name__ == '__main__':
+    main()
