c6df69490af251d0ad7de2d445e2976c623c61b9
masashi <masashi.lateolabrax@gmail.com>
2025-04-26 14:59:53 +0900

    fix: Resolve conflict for cmaes-crobot-simple branches

--- scheme/cmaes_crobot_simple/src/settings.py
+++ scheme/cmaes_crobot_simple/src/settings.py
@@ -7,8 +7,8 @@ Settings.Simulation.RENDER_ZOOM = 10
 
 Settings.CMAES.GENERATION = 500
 Settings.CMAES.POPULATION = 1000
-Settings.CMAES.MU = int(Settings.CMAES.POPULATION / 2)
-Settings.CMAES.SIGMA = 0.1
+Settings.CMAES.MU = 500
+Settings.CMAES.SIGMA = 0.001
 
 Settings.Simulation.TIME_STEP = 0.01
 Settings.Simulation.TIME_LENGTH = int(30 / Settings.Simulation.TIME_STEP)
