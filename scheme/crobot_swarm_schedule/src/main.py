c392b77ef2b51946112f7347b83bb44935d22974
masashi <masashi.lateolabrax@gmail.com>
2025-04-26 14:36:56 +0900

    fix: Resolve conflict for crobot-swarm-schedule branch

---
+++ scheme/crobot_swarm_schedule/src/main.py
@@ -0,0 +1,88 @@
+import os.path
+import datetime
+
+from libs import utils
+import framework
+
+from logger import Logger
+from brain import BrainBuilder
+from settings import Settings, set_positions
+
+import analysis
+
+
+def main():
+    git_hash = utils.get_head_hash()[0:8]
+    save_dir = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        datetime.datetime.now().strftime("%Y%m%d_%H%M%S-") + git_hash
+    )
+    # save_dir = "20250423_103010-7457c6a6"
+    os.makedirs(save_dir, exist_ok=True)
+
+    log_file_name1 = "result1.pkl"
+    log_file_name2 = "result2.pkl"
+
+    settings = Settings()
+    brain_builder = BrainBuilder(settings)
+    logger1 = Logger(save_dir)
+    logger2 = Logger(save_dir)
+
+    ## Training.
+    if not os.path.exists(os.path.join(save_dir, log_file_name1)):
+        tmp_generation = settings.CMAES.GENERATION
+
+        settings.CMAES.GENERATION = int(tmp_generation * 0.2)
+        set_positions(settings)
+        framework.entry_points.train(settings, logger1, brain_builder)
+        logger1.save(log_file_name1)
+
+        settings.CMAES.GENERATION = int(tmp_generation * 0.8)
+        settings.Robot.POSITION = []
+        settings.Food.POSITION = []
+        framework.entry_points.additional_train(
+            settings, logger2, brain_builder,
+            centroid=logger1.centroid,
+            cmatrix=logger1.cmatrix,
+        )
+        logger2.save(log_file_name2)
+
+    ## Load the logger if it is empty.
+    if logger1.is_empty():
+        logger1 = Logger.load(os.path.join(save_dir, log_file_name1))
+    if logger2.is_empty():
+        logger2 = Logger.load(os.path.join(save_dir, log_file_name2))
+
+    set_positions(settings)
+    analyze_results(logger1, os.path.join(save_dir, "stage1"), brain_builder)
+
+    settings.Robot.POSITION = []
+    settings.Food.POSITION = []
+    analyze_results(logger2, os.path.join(save_dir, "stage2"), brain_builder)
+
+
+def analyze_results(logger, save_dir, brain_builder):
+    os.makedirs(save_dir, exist_ok=True)
+
+    ## Plot the movements of the parameters.
+    file_path = os.path.join(save_dir, "parameter_movement.png")
+    if not os.path.exists(file_path):
+        analysis.plot_parameter_movements(file_path, logger)
+
+    file_path = os.path.join(save_dir, "test_loss.png")
+    if not os.path.exists(file_path):
+        analysis.test_suboptimal_individuals(
+            file_path, logger, brain_builder
+        )
+
+    file_path = os.path.join(save_dir, "videos")
+    if not os.path.exists(file_path):
+        analysis.record_in_mp4(
+            file_path, logger, brain_builder
+        )
+
+    # analysis.plot_max_of_parameter(os.path.join(save_dir, "parameter_info.png"), logger, 0, 100)
+
+
+if __name__ == '__main__':
+    main()
--- scheme/crobot_swarm_schedule/src/settings.py
+++ scheme/crobot_swarm_schedule/src/settings.py
@@ -1,47 +1,44 @@
 import numpy as np
 
-from framework.simulator.const import Settings, ROBOT_SIZE
+from framework import Settings
+from framework.simulator.objects.robot import ROBOT_SIZE
 
-from loss import Loss, calc_loss_sigma
+from loss import Loss
 
 Settings.Simulation.RENDER_WIDTH = 800
 Settings.Simulation.RENDER_HEIGHT = 800
 Settings.Simulation.RENDER_ZOOM = 19
 Settings.Simulation.MAX_GEOM = 1000
 
-Settings.CMAES.GENERATION = 100
+Settings.CMAES.GENERATION = 300
 Settings.CMAES.POPULATION = 100
 Settings.CMAES.MU = 50
 Settings.CMAES.SIGMA = 0.1
 
 Settings.Simulation.TIME_STEP = 0.01
-Settings.Simulation.TIME_LENGTH = int(30 / Settings.Simulation.TIME_STEP)
+Settings.Simulation.TIME_LENGTH = int(60 / Settings.Simulation.TIME_STEP)
 
 Settings.CMAES.LOSS = Loss()
-Settings.CMAES.LOSS.sigma_nest_and_food = calc_loss_sigma(4, 0.01)
 Settings.CMAES.LOSS.GAIN_NEST_AND_FOOD = 1
-Settings.CMAES.LOSS.sigma_robot_and_food = calc_loss_sigma(1, 0.3)
 Settings.CMAES.LOSS.GAIN_ROBOT_AND_FOOD = 0.5
 
 Settings.Simulation.WORLD_WIDTH = 15
 Settings.Simulation.WORLD_HEIGHT = 15
 
 Settings.Robot.THINK_INTERVAL = 0.1
-Settings.Robot.OtherRobotSensor.GAIN = (lambda d, v: (1 - v) / (v * d))(2, 0.1)
-Settings.Robot.FoodSensor.GAIN = (lambda d, v: (1 - v) / (v * d))(4, 0.1)
+Settings.Robot.FoodSensor.GAIN = 1
 
 Settings.Robot.NUM = 7 * 7
 Settings.Food.NUM = 4
 
 
 def set_positions(settings: Settings):
-    settings.Robot.POSITION = []
-    area = (np.sqrt(settings.Robot.NUM) - 1) * (ROBOT_SIZE + 0.05)
-    for x in np.linspace(-area, area, int(np.sqrt(settings.Robot.NUM)), endpoint=True):
-        for y in np.linspace(area, -area, int(np.sqrt(settings.Robot.NUM)), endpoint=True):
-            settings.Robot.POSITION.append((x, y, 0))
+    area = (np.sqrt(Settings.Robot.NUM) - 1) * (ROBOT_SIZE + 0.05)
+    for x in np.linspace(-area, area, int(np.sqrt(Settings.Robot.NUM)), endpoint=True):
+        for y in np.linspace(area, -area, int(np.sqrt(Settings.Robot.NUM)), endpoint=True):
+            Settings.Robot.POSITION.append((x, y, 0))
 
-    settings.Food.POSITION = [
+    Settings.Food.POSITION = [
         (0, 3.5),
         (3.5, 0),
         (0, -3.5),
