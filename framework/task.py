ee7aa78a07021e151ed4733391e868cb67715bb2
masashi <d3237100024@std.kitami-it.ac.jp>
2025-04-28 00:21:45 +0900

    mod: Update robot position retrieval to use robot_positions dictionary

--- framework/task.py
+++ framework/task.py
@@ -79,7 +79,7 @@ class Task(optimizer.MjcTaskInterface):
                 print(f"Robot {robot_name} has an invalid output (NaN or Inf).")
             return float("inf")
 
-        robot_pos = np.array([robot.position for robot in self.robots.values()])
+        robot_pos = np.array([v for v in robot_positions.values()])
         food_pos = self.food.all_positions()
         loss = self.settings.CMAES._loss(self.nest.position, robot_pos, food_pos)
 
