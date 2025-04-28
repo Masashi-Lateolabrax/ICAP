412148a6377807e913b7d5081ca750c13d7a9e5a
masashi <masashi.lateolabrax@gmail.com>
2025-04-28 16:28:34 +0900

    mod: Update comments for clarity in settings.py

--- scheme/cmaes_crobot_swarm_simple/src/settings.py
+++ scheme/cmaes_crobot_swarm_simple/src/settings.py
@@ -4,49 +4,49 @@ from framework.simulator.const import Settings, ROBOT_SIZE
 
 from loss import Loss, calc_loss_sigma
 
-# Rendering settings
+## Rendering settings
 Settings.Simulation.Render.RENDER_WIDTH = 800
 Settings.Simulation.Render.RENDER_HEIGHT = 900
 Settings.Simulation.Render.RENDER_ZOOM = 25
 Settings.Simulation.Render.MAX_GEOM = 1000
 
-# Light settings
+## Light settings
 Settings.Simulation.Render.LIGHT_AMBIENT = (1, 1, 1)
 Settings.Simulation.Render.LIGHT_DIFFUSE = (1, 1, 1)
 Settings.Simulation.Render.LIGHT_SPECULAR = (1, 1, 1)
 
-# World Size
+## World Size
 Settings.Simulation.WORLD_WIDTH = 16
 Settings.Simulation.WORLD_HEIGHT = 18
 
-# Optimization settings
-Settings.CMAES.GENERATION = 1000
+## Optimization settings
+Settings.CMAES.GENERATION = 500
 Settings.CMAES.POPULATION = 100
 Settings.CMAES.MU = 50
 Settings.CMAES.SIGMA = 0.3
 
-# Simulation settings
+## Simulation settings
 Settings.Simulation.TIME_STEP = 0.01
-Settings.Simulation.TIME_LENGTH = int(60 / Settings.Simulation.TIME_STEP)
+Settings.Simulation.TIME_LENGTH = int(45 / Settings.Simulation.TIME_STEP)
 
-# Loss settings
+## Loss settings
 Settings.CMAES.LOSS = Loss()
-## Nest and Food
-Settings.CMAES.LOSS.sigma_nest_and_food = 2
-Settings.CMAES.LOSS.GAIN_NEST_AND_FOOD = 7
-## Robot and Food
-Settings.CMAES.LOSS.sigma_robot_and_food = 2
-Settings.CMAES.LOSS.GAIN_ROBOT_AND_FOOD = 1
+### Nest and Food
+Settings.CMAES.LOSS.sigma_nest_and_food = calc_loss_sigma(4, 0.01)
+Settings.CMAES.LOSS.GAIN_NEST_AND_FOOD = 1
+### Robot and Food
+Settings.CMAES.LOSS.sigma_robot_and_food = calc_loss_sigma(1, 0.3)
+Settings.CMAES.LOSS.GAIN_ROBOT_AND_FOOD = 0.1
 
-# Robot Settings
+## Robot Settings
 Settings.Robot.THINK_INTERVAL = 0.3
-Settings.Robot.OtherRobotSensor.GAIN = 2 / 0.7
-Settings.Robot.FoodSensor.GAIN = 2 / 5
+Settings.Robot.OtherRobotSensor.GAIN = (lambda d, v: (1 - v) / (v * d))(2, 0.1)
+Settings.Robot.FoodSensor.GAIN = (lambda d, v: (1 - v) / (v * d))(4, 0.1)
 
-# Food Settings
-Settings.Food.REPLACEMENT = False
+## Food Settings
+Settings.Food.REPLACEMENT = True
 
-# Number of Robots and Food
+## Number of Robots and Food
 Settings.Robot.NUM = 3 * 3
 Settings.Food.NUM = 2
 
