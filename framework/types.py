c3099cae9f413373b26507b117069ed34db6ebeb
masashi <masashi.lateolabrax@gmail.com>
2025-06-30 14:18:54 +0900

    add: Introduce `RobotSpec` and `RobotValues` classes for organizing MuJoCo robot specifications and runtime data

--- framework/types.py
+++ framework/types.py
@@ -1,5 +1,7 @@
 import dataclasses
 
+import mujoco
+
 
 @dataclasses.dataclass
 class Position:
@@ -29,3 +31,31 @@ class RobotLocation:
     @property
     def y(self) -> float:
         return self.position.y
+
+
+class RobotSpec:
+    def __init__(
+            self,
+            center_site: mujoco._specs.MjsSite,
+            front_site: mujoco._specs.MjsSite,
+            free_join: mujoco._specs.MjsJoint,
+            x_act: mujoco._specs.MjsActuator,
+            y_act: mujoco._specs.MjsActuator,
+            r_act: mujoco._specs.MjsActuator
+    ):
+        self.center_site = center_site
+        self.front_site = front_site
+        self.free_join = free_join
+        self.x_act = x_act
+        self.y_act = y_act
+        self.r_act = r_act
+
+
+class RobotValues:
+    def __init__(self, data: mujoco.MjData, spec: RobotSpec):
+        self.center_site: mujoco._structs._MjDataSiteViews = data.site(spec.center_site.name)
+        self.front_site: mujoco._structs._MjDataSiteViews = data.site(spec.front_site.name)
+        self.free_join: mujoco._structs._MjDataJointViews = data.joint(spec.free_join.name)
+        self.x_act: mujoco._structs._MjDataActuatorViews = data.actuator(spec.x_act.name)
+        self.y_act: mujoco._structs._MjDataActuatorViews = data.actuator(spec.y_act.name)
+        self.r_act: mujoco._structs._MjDataActuatorViews = data.actuator(spec.r_act.name)
