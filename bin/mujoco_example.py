31c9d42d5e375bc8ebe830f1a0365e62b1080d65
masashi <masashi.lateolabrax@gmail.com>
2025-07-01 09:15:49 +0900

    add: Implement `Controller` class with neural network integration and update `mujoco_example` for control loop refactoring

--- bin/mujoco_example.py
+++ bin/mujoco_example.py
@@ -1,24 +1,36 @@
 import mujoco
 import numpy as np
+import torch
 
 from framework.backends import MujocoBackend
 from framework.utils import GenericTkinterViewer
-from framework.sensor import OmniSensor, DirectionSensor
-from framework.prelude import Settings, RobotLocation, Position
 from framework.sensor import PreprocessedOmniSensor, DirectionSensor
+from framework.controller import Controller
 from framework.prelude import Settings, RobotLocation, Position, SensorInterface
 
 
+class RobotNeuralNetwork(torch.nn.Module):
+    def __init__(self):
+        super(RobotNeuralNetwork, self).__init__()
+        self.linear1 = torch.nn.Linear(6, 3)
+        self.activation1 = torch.nn.Tanhshrink()
 
-class Controller:
-    def __init__(self, sensors: list[tuple[OmniSensor, OmniSensor, DirectionSensor]]):
-        self.sensors = sensors
+        self.linear2 = torch.nn.Linear(3, 2)
+        self.activation2 = torch.nn.Tanh()
+
+    def forward(self, input_):
+        x = self.linear1.forward(input_)
+        x = self.activation1.forward(x)
+        x = self.linear2.forward(x)
+        x = self.activation2.forward(x)
+        return x
 
 
 class SampleMujocoBackend(MujocoBackend):
     def __init__(self, settings: Settings, render: bool = False):
         super().__init__(settings, render)
         self.scores = []
+        self.controller = Controller(settings, 32, RobotNeuralNetwork())
 
         self.sensors: list[tuple[SensorInterface]] = [
             (
@@ -41,13 +53,22 @@ class SampleMujocoBackend(MujocoBackend):
             for i, robot in enumerate(self.robot_values)
         ]
 
-        self.controller = Controller(sensors)
+        self._pairs = [self.controller.register(r, f, n) for r, f, n in self.sensors]
+
+    def __del__(self):
+        for p in self._pairs:
+            p.finished = True
 
     def step(self):
-        for robot in self.robot_values:
-            robot_pos = robot._center_site.xpos[0:2]
-            xdirection = robot._front_site.xpos[0:2] - robot_pos
-            xdirection /= np.linalg.norm(xdirection)
+        for pair in self._pairs:
+            pair.need_calculation = True
+
+        self.controller.calculate()
+
+        for r, p in zip(self.robot_values, self._pairs):
+            right_wheel = p.output_buf[0]
+            left_wheel = p.output_buf[1]
+            r.act(right_wheel, left_wheel)
 
         mujoco.mj_step(self.model, self.data)
 
