d3ec4ce29a64626070ecf3add9e263246c6ec5b0
masashi <masashi.lateolabrax@gmail.com>
2025-07-04 13:31:45 +0900

    mod: Update individual serialization to use to_ndarray method for consistency

--- bin/main.py
+++ bin/main.py
@@ -1,10 +1,49 @@
-from framework.prelude import Settings
+import mujoco
+import numpy as np
+import torch
+
 from framework.optimization import connect_to_server
+from framework.backends import MujocoBackend
+from framework.utils import GenericTkinterViewer
+from framework.sensor import PreprocessedOmniSensor, DirectionSensor
+from framework.prelude import Settings, RobotLocation, Position, SensorInterface, RobotValues, Individual
 
 
-def rosenbrock_function(individual):
-    """
-    Rosenbrock function: classic optimization benchmark.
+class RobotNeuralNetwork(torch.nn.Module):
+    def __init__(self):
+        super(RobotNeuralNetwork, self).__init__()
+        self.linear1 = torch.nn.Linear(6, 3)
+        self.activation1 = torch.nn.Tanhshrink()
+
+        self.linear2 = torch.nn.Linear(3, 2)
+        self.activation2 = torch.nn.Tanh()
+
+    def forward(self, input_):
+        x = self.linear1.forward(input_)
+        x = self.activation1.forward(x)
+        x = self.linear2.forward(x)
+        x = self.activation2.forward(x)
+        return x
+
+    @property
+    def dim(self):
+        return sum(p.numel() for p in self.parameters())
+
+    def set_parameters(self, parameters: Individual):
+        assert len(parameters) == self.dim, \
+            "Parameter length does not match the network's parameter count."
+
+        torch.nn.utils.vector_to_parameters(
+            torch.tensor(parameters, dtype=torch.float32),
+            self.parameters()
+        )
+
+
+class SampleMujocoBackend(MujocoBackend):
+    def __init__(self, settings: Settings, render: bool = False):
+        super().__init__(settings, render)
+        self.scores = []
+        self.sensors: list[tuple[SensorInterface]] = self._create_sensors()
 
     Global minimum: f(1, 1, ..., 1) = 0
 
