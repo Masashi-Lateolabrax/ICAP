9f1c23ea722d6708d646b7908f395dc6a8085fd4
masashi <masashi.lateolabrax@gmail.com>
2025-07-04 15:15:58 +0900

    Refactor Loss class to use numpy arrays for robot and nest position calculations

--- bin/main.py
+++ bin/main.py
@@ -39,11 +39,14 @@ class RobotNeuralNetwork(torch.nn.Module):
 class Loss:
     def __init__(self, settings: Settings, robot_positions, food_positions, nest_position):
         self.settings = settings
-        self.r_loss = self._calc_r_loss(robot_positions, food_positions)
-        self.n_loss = self._calc_n_loss(nest_position, food_positions)
+        robot_pos_array = np.array(robot_positions)
+        food_pos_array = np.array(food_positions)
+        nest_pos_array = nest_position[0:2]
+        self.r_loss = self._calc_r_loss(robot_pos_array, food_pos_array)
+        self.n_loss = self._calc_n_loss(nest_pos_array, food_pos_array)
 
     def _calc_r_loss(self, robot_pos: np.ndarray, food_pos: np.ndarray) -> float:
-        subs = (robot_pos[:, None, :2] - food_pos[None, :, :]).reshape(-1, 2)
+        subs = (robot_pos[:, None, :] - food_pos[None, :, :]).reshape(-1, 2)
         distance = np.clip(
             np.linalg.norm(subs, axis=1) - self.settings.Loss.OFFSET_ROBOT_AND_FOOD,
             a_min=0,
@@ -53,7 +56,7 @@ class Loss:
         return self.settings.Loss.GAIN_ROBOT_AND_FOOD * loss
 
     def _calc_n_loss(self, nest_pos: np.ndarray, food_pos: np.ndarray) -> float:
-        subs = food_pos - nest_pos
+        subs = food_pos - nest_pos[None, :]
         distance = np.clip(
             np.linalg.norm(subs, axis=1) - self.settings.Loss.OFFSET_NEST_AND_FOOD,
             a_min=0,
