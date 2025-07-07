ff5593382ab455296de3637fc35b373ce5d921e8
masashi <masashi.lateolabrax@gmail.com>
2025-07-07 10:51:54 +0900

    feat: Add CommunicationResult, PacketType, and Packet imports for enhanced communication handling

--- framework/prelude.py
+++ framework/prelude.py
@@ -1,6 +1,6 @@
 from .types import (
     Individual, CalculationState, RobotValues, RobotSpec, RobotLocation, FoodSpec, FoodValues, EvaluationFunction,
-    Position, Position3d, ProcessMetrics
+    Position, Position3d, ProcessMetrics, CommunicationResult, PacketType, Packet
 )
 
 from .config import *
