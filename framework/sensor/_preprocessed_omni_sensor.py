mod: Fix angle calculation in get() method of _preprocessed_omni_sensor.py

framework/sensor/_preprocessed_omni_sensor.py
@@ -19,5 +19,5 @@ class PreprocessedOmniSensor(OmniSensor):
     def get(self) -> np.ndarray:
         raw = super().get()
         magnitude = 1 / (np.linalg.norm(raw) + 1)
-        angle = np.arctan2(raw[1], raw[0]) / (np.pi * 0.5)
+        angle = np.arctan2(-raw[0], raw[1]) / np.pi
         return np.array([magnitude, angle])

src/analysis.py
@@ -176,13 +176,11 @@ def input_animation(settings: Settings, debug_info: list[DebugInfo], file_path: 
             draw_arrowed_line(buffer, pos, nest_direction, 20, (255, 100, 0), thickness=2, tip_length=0.3)
 
             # Draw the food direction
-            angle = inputs[3] * (np.pi * 0.5) - (np.pi * 0.5)
-            food_direction = rotate_vector_2d(robot_dir, angle)
+            food_direction = rotate_vector_2d(robot_dir, inputs[3] * np.pi)
             draw_arrowed_line(buffer, pos, food_direction, 20, (255, 0, 100), thickness=2, tip_length=0.3)
 
             # Draw the other robot direction
-            angle = inputs[1] * (np.pi * 0.5) - (np.pi * 0.5)
-            food_direction = rotate_vector_2d(robot_dir, angle)
+            food_direction = rotate_vector_2d(robot_dir, inputs[1] * np.pi)
             draw_arrowed_line(buffer, pos, food_direction, 20, (255, 100, 100), thickness=2, tip_length=0.3)
 
         for food_pos, food_dir in zip(di.food_positions, di.food_directions):