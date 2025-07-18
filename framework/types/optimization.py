7f72df8158e3fc8a6b26f0611159843e25498b2e
masashi <masashi.lateolabrax@gmail.com>
2025-07-19 03:21:14 +0900

    mod: Add settings parameter to Individual class for enhanced configuration

--- framework/types/optimization.py
+++ framework/types/optimization.py
@@ -1,7 +1,9 @@
 import enum
-from typing import Callable
+from typing import Callable, Optional
 
 import numpy as np
+
+from ..prelude import Settings
 
 
 class CalculationState(enum.Enum):
@@ -12,16 +14,21 @@ class CalculationState(enum.Enum):
 
 
 class Individual(np.ndarray):
-    def __new__(cls, input_array, generation: int):
+    def __new__(cls, input_array, generation: int, settings: Optional['Settings'] = None):
         obj = np.asarray(input_array).view(cls)
         obj._fitness = float("inf")
         obj._calculation_state = CalculationState.NOT_STARTED
         obj._generation = generation
+        obj._settings = settings
         return obj
 
     @property
     def generation(self) -> int:
         return self._generation
+
+    @property
+    def settings(self) -> Optional['Settings']:
+        return self._settings
 
     def __array_finalize__(self, obj):
         if obj is None:
@@ -32,6 +39,7 @@ class Individual(np.ndarray):
         self._fitness = obj._fitness
         self._calculation_state = obj._calculation_state
         self._generation = obj._generation
+        self._settings = obj._settings
 
     def set_fitness(self, fitness: float):
         self._fitness = fitness
@@ -52,15 +60,17 @@ class Individual(np.ndarray):
         self._fitness = other._fitness
         self._calculation_state = other._calculation_state
         self._generation = other._generation
+        self._settings = other._settings
 
     def __reduce__(self):
         return (
             Individual,
-            (self.to_ndarray(), self._generation),
+            (self.to_ndarray(), self._generation, self._settings),
             (
                 self._fitness,
                 self._calculation_state,
-                self._generation
+                self._generation,
+                self._settings
             )
         )
 
@@ -68,6 +78,7 @@ class Individual(np.ndarray):
         self._fitness = state[0]
         self._calculation_state = state[1]
         self._generation = state[2]
+        self._settings = state[3]
 
     @property
     def is_ready(self) -> bool:
