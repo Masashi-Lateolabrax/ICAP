0069b44f6ed1ab34c41b15d58fdc24b5e049fc21
masashi <masashi.lateolabrax@gmail.com>
2025-07-19 15:46:23 +0900

    tmp: Refactor MujocoSTL to improve initialization and streamline mesh asset handling

--- framework/backends/mujoco_stl.py
+++ framework/backends/mujoco_stl.py
@@ -9,26 +9,23 @@ from ._mujoco import MujocoBackend
 from ..environment import (
     add_mesh_in_asset, MeshContentType,
     add_geom,
-    setup_option, setup_visual, setup_textures, add_nest,
-    add_wall,
+    add_nest, add_wall,
     add_food_object_with_mesh, add_robot_with_mesh,
     rand_robot_pos, rand_food_pos,
 )
 
 
 class MujocoSTL(MujocoBackend):
-    @staticmethod
-    def _generate_mjspec(
-            settings: Settings
-    ) -> tuple[mujoco.MjSpec, mujoco._specs.MjsSite, list[RobotSpec], list[FoodSpec]]:
-        spec = mujoco.MjSpec()
+    def __init__(self, settings: Settings, render: bool = False):
+        self.settings = settings
+        self.do_render = render
+        self.render_shape = self.settings.Render.RENDER_WIDTH, self.settings.Render.RENDER_HEIGHT
 
-        setup_option(spec, settings)
-        setup_visual(spec, settings)
-        setup_textures(spec, settings)
+        self.spec = self._generate_base_mjspec(self.settings)
 
+        # Add STL mesh assets
         food_mesh = add_mesh_in_asset(
-            spec,
+            self.spec,
             name="food_mesh",
             file=os.path.abspath(os.path.join(settings.Storage.ASSET_DIRECTORY, "food-object.stl")),
             content_type=MeshContentType.STL,
@@ -36,17 +33,17 @@ class MujocoSTL(MujocoBackend):
         )
 
         robot_mesh = add_mesh_in_asset(
-            spec,
+            self.spec,
             name="robot_mesh",
             file=os.path.abspath(os.path.join(settings.Storage.ASSET_DIRECTORY, "robot-object.stl")),
             content_type=MeshContentType.STL,
             inertia=mujoco.mjtMeshInertia.mjMESH_INERTIA_CONVEX
         )
 
-        add_wall(spec, settings)
+        add_wall(self.spec, settings)
 
         add_geom(
-            spec.worldbody,
+            self.spec.worldbody,
             geom_type=mujoco.mjtGeom.mjGEOM_PLANE,
             pos=(0, 0, 0),
             size=(
@@ -59,7 +56,7 @@ class MujocoSTL(MujocoBackend):
             condim=GROUND_COLLISION_CONDIM
         )
 
-        nest_spec = add_nest(spec, settings)
+        nest_spec = add_nest(self.spec, settings)
 
         invalid_area: list[tuple[Position, float]] = []
 
@@ -78,7 +75,7 @@ class MujocoSTL(MujocoBackend):
                 (position, settings.Food.RADIUS)
             )
             food_specs.append(
-                add_food_object_with_mesh(spec, settings, food_mesh, i, position)
+                add_food_object_with_mesh(self.spec, settings, food_mesh, i, position)
             )
 
         # Create robots
@@ -96,13 +93,23 @@ class MujocoSTL(MujocoBackend):
                 (position.position, settings.Robot.RADIUS)
             )
             robot_specs.append(
-                add_robot_with_mesh(spec, settings, robot_mesh, i, position)
+                add_robot_with_mesh(self.spec, settings, robot_mesh, i, position)
             )
 
-        return spec, nest_spec, robot_specs, food_specs
+        # Instantiate the MuJoCo model and data
+        self.model: mujoco.MjModel = self.spec.compile()
+        self.data: mujoco.MjData = mujoco.MjData(self.model)
 
-    def __init__(self, settings: Settings, render: bool = False):
-        super().__init__(settings, render)
+        # Extract important value references
+        self.nest_site = self.data.site(nest_spec.name)
+        self.robot_values = [
+            RobotValues(settings.Robot.DISTANCE_BETWEEN_WHEELS, settings.Robot.MAX_SPEED, self.data, s)
+            for s in robot_specs
+        ]
+        self.food_values = [FoodValues(self.data, s) for s in food_specs]
+
+        # Miscellaneous initializations
+        self.camera = mujoco.MjvCamera()
 
     def step(self):
         mujoco.mj_step(self.model, self.data)
