8b6bc4b2e6a8c31100668f07aeca6ec736a0ad9d
masashi <masashi.lateolabrax@gmail.com>
2025-07-23 14:58:52 +0900

    mod: Increase optimization population to 500 and generations to 1000

--- src/settings.py
+++ src/settings.py
@@ -38,8 +38,8 @@ MySettings.Render.CAMERA_LOOKAT = (0.0, 0.0, 0.0)
 # Optimization settings
 ###################################################################################
 MySettings.Optimization.DIMENSION = None  # Will be set later based on the neural network dimension
-MySettings.Optimization.POPULATION = 100
-MySettings.Optimization.GENERATION = 500
+MySettings.Optimization.POPULATION = 500
+MySettings.Optimization.GENERATION = 1000
 MySettings.Optimization.SIGMA = 0.1
 ###################################################################################
 
