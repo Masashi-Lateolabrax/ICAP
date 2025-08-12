9d14d32bc627b03c4f354e849da4cadc063111c4
masashi <masashi.lateolabrax@gmail.com>
2025-08-12 15:38:42 +0900

    mod: Remove unused MujocoBackend and MujocoSTL class

--- framework/backends/__init__.py
+++ framework/backends/__init__.py
@@ -1,5 +1,3 @@
 from .basic import BasicMuJoCoSimulator
 from .basic_simulator import BasicSimulator
 from .simple_anime_for_test import SimpleAnimatedBackend
-from ._mujoco import MujocoBackend
-from .mujoco_stl import MujocoSTL
