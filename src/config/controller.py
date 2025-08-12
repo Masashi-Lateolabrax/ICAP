640cf20590b63e6640734993c36e76e6d8be161f
masashi <masashi.lateolabrax@gmail.com>
2025-08-12 14:56:26 +0900

    mod: Update neural network architecture; [6,12,4,2] > [6,12,12,6,2]

--- src/config/controller.py
+++ src/config/controller.py
@@ -8,11 +8,13 @@ class Controller(torch.nn.Module):
         super(Controller, self).__init__()
 
         self.sequential = torch.nn.Sequential(
-            torch.nn.Linear(6, 4),
+            torch.nn.Linear(6, 12),
             torch.nn.Mish(),
-            torch.nn.Linear(4, 3),
+            torch.nn.Linear(12, 12),
             torch.nn.Mish(),
-            torch.nn.Linear(3, 2),
+            torch.nn.Linear(12, 6),
+            torch.nn.Mish(),
+            torch.nn.Linear(6, 2),
             torch.nn.Sigmoid()
         )
 
