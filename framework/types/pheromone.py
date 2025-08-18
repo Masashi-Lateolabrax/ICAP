2689419b9fa23e0353f1206d1b18e0549eee7637
masashi <masashi.lateolabrax@gmail.com>
2025-08-18 16:12:21 +0900

    mod: Update ethanol material properties for improved accuracy

--- framework/types/pheromone.py
+++ framework/types/pheromone.py
@@ -10,14 +10,22 @@ class Material:
     ANTOINE_C: float
 
     def saturation_pressure(self, temperature: float) -> float:
-        """Calculate saturation pressure using the Antoine equation."""
-        return 10 ** (self.ANTOINE_A - (self.ANTOINE_B / (temperature + self.ANTOINE_C))) * 1000
+        """
+        Calculate saturation pressure using the Antoine equation.
+
+        Returns:
+            float: Saturation pressure in kPa.
+        """
+        return 10 ** (self.ANTOINE_A - (self.ANTOINE_B / (temperature + self.ANTOINE_C))) * 100
 
 
+# Reference(Gas Constant): https://physics.nist.gov/cgi-bin/cuu/Value?r|search_for=gas+constant
+# Reference(Ethanol): https://webbook.nist.gov/cgi/cbook.cgi?ID=C64175&Mask=4&Type=ANTOINE&Plot=on#ANTOINE
+# Temperature from 292.77 to 366.63
 ETHANOL = Material(
-    gas_constant=8.31446262,
-    molar_mass=46.07,
-    ANTOINE_A=8.04494,
-    ANTOINE_B=1554.3,
-    ANTOINE_C=222.65
+    gas_constant=8.314462618, # J/(mol·K)
+    molar_mass=46.0684, # g/mol
+    ANTOINE_A=5.24677,
+    ANTOINE_B=1598.673,
+    ANTOINE_C=-46.424
 )
