ee4a67742613ddd740b2f415bcff4d55c63b70e5
masashi <masashi.lateolabrax@gmail.com>
2025-08-19 14:00:25 +0900

    add: Implement add_pheromone method to update pheromone values in BasicEnvironment

--- framework/backends/basic_environment.py
+++ framework/backends/basic_environment.py
@@ -152,6 +152,22 @@ class BasicEnvironment(BasicMuJoCoSimulator, ABC):
             )
             self._pheromone_cells = [s.get_cell(self.model) for s in pheromone_cell_specs]
 
+    def add_pheromone(self, x: float, y: float, value: float):
+        if len(self._pheromone_cells) == 0:
+            return
+
+        lu_cell = self._pheromone_cells[0]
+        rd_cell = self._pheromone_cells[-1]
+        pos = np.array([x, y])
+        rpos = (pos - lu_cell.pos[:2]) / (rd_cell.pos[:2] - lu_cell.pos[:2])
+        index_x = rd_cell.index_x * rpos[0]
+        index_y = rd_cell.index_y * rpos[1]
+        index_x = np.clip(index_x, 0, rd_cell.index_x)
+        index_y = np.clip(index_y, 0, rd_cell.index_y)
+
+        cell = self._pheromone_cells[int(index_x * rd_cell.index_y + index_y)]
+        cell.add_value += value
+
     def render(self, img_buf: np.ndarray, pos: tuple[float, float, float], lookat: tuple[float, float, float]):
         if not self._do_render:
             return
