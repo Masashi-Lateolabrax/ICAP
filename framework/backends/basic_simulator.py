0a36aa66a244c8d9706a078ec66dc789bc58b94f
masashi <masashi.lateolabrax@gmail.com>
2025-08-19 13:39:46 +0900

    add: Implement pheromone cell management and addition in BasicSimulator

--- framework/backends/basic_simulator.py
+++ framework/backends/basic_simulator.py
@@ -1,6 +1,9 @@
 from abc import ABC
 
+import numpy as np
+
 from ..prelude import *
+from ..pheromone import PheromoneFieldCell
 from .basic_environment import BasicEnvironment
 
 
@@ -14,3 +17,20 @@ class BasicSimulator(BasicEnvironment, ABC):
             for s in self.robot_specs
         ]
         self.food_values = [FoodValues(self.data, s) for s in self.food_specs]
+        self.pheromone_cells: list[PheromoneFieldCell] = [s.get_cell(self.model) for s in self.pheromone_cell_specs]
+
+    def add_pheromone(self, x: float, y: float, value: float):
+        if len(self.pheromone_cells) == 0:
+            return
+
+        lu_cell = self.pheromone_cells[0]
+        rd_cell = self.pheromone_cells[-1]
+        pos = np.array([x, y])
+        rpos = (pos - lu_cell.pos[:2]) / (rd_cell.pos[:2] - lu_cell.pos[:2])
+        index_x = rd_cell.index_x * rpos[0]
+        index_y = rd_cell.index_y * rpos[1]
+        index_x = np.clip(index_x, 0, rd_cell.index_x)
+        index_y = np.clip(index_y, 0, rd_cell.index_y)
+
+        cell = self.pheromone_cells[int(index_x * rd_cell.index_y + index_y)]
+        cell.add_value += value
