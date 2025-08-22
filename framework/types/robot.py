0a4661579657f7647b5c007581928724f9cd9261
masashi <masashi.lateolabrax@gmail.com>
2025-08-23 00:22:04 +0900

    feat: Refactor BatchedRobots class with JAX compatibility and enhanced state management

--- framework/types/robot.py
+++ framework/types/robot.py
@@ -1,21 +1,11 @@
+import dataclasses
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+
 import mujoco
-import numpy as np
-
-from .position import Position
-
-
-class RobotLocation:
-    def __init__(self, x: float, y: float, theta: float):
-        self.position = Position(x, y)
-        self.theta = theta  # Unit is radian
-
-    @property
-    def x(self) -> float:
-        return self.position.x
-
-    @property
-    def y(self) -> float:
-        return self.position.y
+from mujoco import mjx
 
 
 class RobotSpec:
@@ -23,7 +13,7 @@ class RobotSpec:
             self,
             center_site: mujoco._specs.MjsSite,
             front_site: mujoco._specs.MjsSite,
-            free_join: mujoco._specs.MjsJoint,
+            free_joint: mujoco._specs.MjsJoint,
             x_act: mujoco._specs.MjsActuator,
             y_act: mujoco._specs.MjsActuator,
             z_act: mujoco._specs.MjsActuator,
@@ -31,63 +21,202 @@ class RobotSpec:
     ):
         self.center_site = center_site
         self.front_site = front_site
-        self.free_join = free_join
+        self.free_joint = free_joint
         self.x_act = x_act
         self.y_act = y_act
         self.z_act = z_act
         self.r_act = r_act
 
 
-class RobotValues:
-    def __init__(self, d: float, velocity: float, data: mujoco.MjData, spec: RobotSpec):
-        self._center_site: mujoco._structs._MjDataSiteViews = data.site(spec.center_site.name)
-        self._front_site: mujoco._structs._MjDataSiteViews = data.site(spec.front_site.name)
-        self._free_join: mujoco._structs._MjDataJointViews = data.joint(spec.free_join.name)
-        self._x_act: mujoco._structs._MjDataActuatorViews = data.actuator(spec.x_act.name)
-        self._y_act: mujoco._structs._MjDataActuatorViews = data.actuator(spec.y_act.name)
-        self._z_act: mujoco._structs._MjDataActuatorViews = data.actuator(spec.z_act.name)
-        self._r_act: mujoco._structs._MjDataActuatorViews = data.actuator(spec.r_act.name)
+@dataclasses.dataclass
+class RobotIDs:
+    center_site_id: jnp.ndarray
+    front_site_id: jnp.ndarray
+    free_joint_id: jnp.ndarray
+    x_actuator_id: jnp.ndarray
+    y_actuator_id: jnp.ndarray
+    z_actuator_id: jnp.ndarray
+    r_actuator_id: jnp.ndarray
 
-        self._d = d
-        self._v = velocity
-        self._matrix = np.array([
-            [self._v * 0.5, self._v * 0.5],
-            [- self._v / self._d, self._v / self._d]
-        ])
 
-        self._xdirection_buf = np.zeros(3)
-        self._direction_buf = np.zeros(3)
+@dataclasses.dataclass
+class BatchedRobotIDs:
+    center_site_ids: jnp.ndarray
+    front_site_ids: jnp.ndarray
+    free_joint_ids: jnp.ndarray
+    x_actuator_ids: jnp.ndarray
+    y_actuator_ids: jnp.ndarray
+    z_actuator_ids: jnp.ndarray
+    r_actuator_ids: jnp.ndarray
+
+    @classmethod
+    def from_specs(cls, model: mujoco.MjModel | mjx.Model, specs: list[RobotSpec]) -> 'BatchedRobotIDs':
+        center_site_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_SITE, spec.center_site.name) for spec in specs]
+        front_site_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_SITE, spec.front_site.name) for spec in specs]
+        free_joint_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_JOINT, spec.free_joint.name) for spec in specs]
+        x_actuator_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, spec.x_act.name) for spec in specs]
+        y_actuator_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, spec.y_act.name) for spec in specs]
+        z_actuator_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, spec.z_act.name) for spec in specs]
+        r_actuator_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_ACTUATOR, spec.r_act.name) for spec in specs]
+
+        return cls(
+            center_site_ids=jnp.array(center_site_ids, dtype=jnp.int32),
+            front_site_ids=jnp.array(front_site_ids, dtype=jnp.int32),
+            free_joint_ids=jnp.array(free_joint_ids, dtype=jnp.int32),
+            x_actuator_ids=jnp.array(x_actuator_ids, dtype=jnp.int32),
+            y_actuator_ids=jnp.array(y_actuator_ids, dtype=jnp.int32),
+            z_actuator_ids=jnp.array(z_actuator_ids, dtype=jnp.int32),
+            r_actuator_ids=jnp.array(r_actuator_ids, dtype=jnp.int32)
+        )
+
+    def __getitem__(self, index: int):
+        return RobotIDs(
+            center_site_id=self.center_site_ids[index],
+            front_site_id=self.front_site_ids[index],
+            free_joint_id=self.free_joint_ids[index],
+            x_actuator_id=self.x_actuator_ids[index],
+            y_actuator_id=self.y_actuator_ids[index],
+            z_actuator_id=self.z_actuator_ids[index],
+            r_actuator_id=self.r_actuator_ids[index]
+        )
+
+
+class BatchedRobots:
+    @staticmethod
+    def __calc_power_and_torque(
+            xdirections: jax.Array, ctrl: jax.Array,
+            matrix_T: jax.Array
+    ):
+        power_and_torque = ctrl @ matrix_T
+        move = xdirections * power_and_torque[:, 0]
+        torque = power_and_torque[:, 1]
+        return move, torque
+
+    @staticmethod
+    def __update_mjx_data_ctrl(
+            data_: mjx.Data, xdirections: jax.Array, ctrl: jax.Array,
+            matrix_T: jax.Array, x_actuator_ids: jax.Array, y_actuator_ids: jax.Array, r_actuator_ids: jax.Array,
+    ):
+        move, torque = BatchedRobots.__calc_power_and_torque(xdirections, ctrl, matrix_T)
+
+        new_ctrl = data_.ctrl.at[x_actuator_ids].set(move[:, 0])
+        new_ctrl = new_ctrl.at[y_actuator_ids].set(move[:, 1])
+        new_ctrl = new_ctrl.at[r_actuator_ids].set(torque)
+
+        return data_.replace(ctrl=new_ctrl)
+
+    @staticmethod
+    def __extract_state(
+            data: mujoco.MjData | mjx.Data,
+            center_site_ids: jax.Array,
+            front_site_ids: jax.Array
+    ) -> tuple[jax.Array, jax.Array]:
+        """Extract positions and directions from mujoco data."""
+        positions = data.site_xpos[center_site_ids, :2]
+        front_pos = data.site_xpos[front_site_ids, :2]
+        sub = front_pos - positions
+        xdirections = sub / (jnp.linalg.norm(sub, axis=1, keepdims=True) + 1e-6)
+        return positions, xdirections
+
+    def __init__(
+            self,
+            data: mujoco.MjData | mjx.Data,
+            batched_ids: BatchedRobotIDs,
+            d: float,
+            velocity: float,
+    ):
+        self.positions = data.site_xpos[batched_ids.center_site_ids, :2]
+
+        front_site_pos = data.site_xpos[batched_ids.front_site_ids, :2]
+        sub = front_site_pos - self.positions
+        self.xdirections = sub / (jnp.linalg.norm(sub, axis=1, keepdims=True) + 1e-6)
+
+        matrix_T = jnp.array([
+            [velocity * 0.5, velocity * 0.5],
+            [- velocity / d, velocity / d]
+        ]).T
+        self._calc_power_and_torque = jax.jit(partial(
+            BatchedRobots.__calc_power_and_torque,
+            matrix_T=matrix_T
+        ))
+
+        self.x_actuator_ids = batched_ids.x_actuator_ids.copy()
+        self.y_actuator_ids = batched_ids.y_actuator_ids.copy()
+        self.r_actuator_ids = batched_ids.r_actuator_ids.copy()
+
+        self._update_mjx_data_ctrl = jax.jit(partial(
+            BatchedRobots.__update_mjx_data_ctrl,
+            matrix_T=matrix_T,
+            x_actuator_ids=self.x_actuator_ids,
+            y_actuator_ids=self.y_actuator_ids,
+            r_actuator_ids=self.r_actuator_ids
+        ))
+
+        self._extract_state = partial(
+            BatchedRobots.__extract_state,
+            center_site_ids=batched_ids.center_site_ids,
+            front_site_ids=batched_ids.front_site_ids
+        )
+        self._jit_extract_state = jax.jit(self._extract_state)
 
     @property
-    def site(self):
-        return self._center_site
+    def num_robots(self) -> int:
+        return self.positions.shape[0]
 
-    @property
-    def xpos(self):
-        return self._center_site.xpos[0:2]
+    def update(self, data: mujoco.MjData | mjx.Data):
+        if isinstance(data, mjx.Data):
+            self.positions, self.xdirections = self._jit_extract_state(data)
+        elif isinstance(data, mujoco.MjData):
+            self.positions, self.xdirections = self._extract_state(data)
 
-    def _xdirection(self):
-        self._xdirection_buf[:] = self._front_site.xpos[0:3]
-        self._xdirection_buf -= self._center_site.xpos[0:3]
-        self._xdirection_buf[2] = 0
-        self._xdirection_buf /= np.linalg.norm(self._xdirection_buf)
-        return self._xdirection_buf
+    def set_ctrl(self, data: mujoco.MjData | mjx.Data, ctrl: jax.Array):
+        if isinstance(data, mjx.Data):
+            data = self._update_mjx_data_ctrl(data, self.xdirections, ctrl)
 
-    @property
-    def xdirection(self):
-        return self._xdirection()[0:2]
+        elif isinstance(data, mujoco.MjData):
+            move, torque = self._calc_power_and_torque(self.xdirections, ctrl)
+            data.ctrl[self.x_actuator_ids] = move[:, 0]
+            data.ctrl[self.y_actuator_ids] = move[:, 1]
+            data.ctrl[self.r_actuator_ids] = torque
 
-    @property
-    def direction(self):
-        mujoco.mju_mulMatVec3(self._direction_buf, self._center_site.xmat, self._xdirection())
-        self._direction_buf[2] = 0
-        self._direction_buf /= np.linalg.norm(self._direction_buf)
-        return self._direction_buf[0:2]
+        return data
 
-    def act(self, right_wheel, left_wheel):
-        power_and_torque = np.dot(self._matrix, [right_wheel, left_wheel])
+    def tree_flatten(self):
+        aux_data = {
+            'x_actuator_ids': self.x_actuator_ids,
+            'y_actuator_ids': self.y_actuator_ids,
+            'r_actuator_ids': self.r_actuator_ids,
+            'calc_power_and_torque': self._calc_power_and_torque,
+            'update_mjx_data_ctrl': self._update_mjx_data_ctrl,
+            'extract_state': self._extract_state,
+            'jit_extract_state': self._jit_extract_state
+        }
+        return (self.positions, self.xdirections), aux_data
 
-        power = self.xdirection * power_and_torque[0]
-        self._x_act.ctrl[0] = power[0]
-        self._y_act.ctrl[0] = power[1]
-        self._r_act.ctrl[0] = power_and_torque[1]
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        positions, xdirections = children
+
+        # Create instance without calling __init__
+        instance = cls.__new__(cls)
+        instance.x_actuator_ids = aux_data['x_actuator_ids']
+        instance.y_actuator_ids = aux_data['y_actuator_ids']
+        instance.r_actuator_ids = aux_data['r_actuator_ids']
+        instance.positions = positions
+        instance.xdirections = xdirections
+
+        # Restore partial functions
+        instance._calc_power_and_torque = aux_data['calc_power_and_torque']
+        instance._update_mjx_data_ctrl = aux_data['update_mjx_data_ctrl']
+        instance._extract_state = aux_data['extract_state']
+        instance._jit_extract_state = aux_data['jit_extract_state']
+
+        return instance
+
+
+# Register BatchedRobots as JAX PyTree
+jax.tree_util.register_pytree_node(
+    BatchedRobots,
+    BatchedRobots.tree_flatten,
+    BatchedRobots.tree_unflatten
+)
