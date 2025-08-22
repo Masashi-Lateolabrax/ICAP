28cf2570cebc10ae8f2046fe327e41e3b6096ff0
masashi <masashi.lateolabrax@gmail.com>
2025-08-23 00:21:50 +0900

    mod: Update types module with JAX compatibility and Position enhancements

--- framework/types/__init__.py
+++ framework/types/__init__.py
@@ -1,20 +1,24 @@
-from .position import Position, Position3d
-from .robot import RobotLocation, RobotSpec, RobotValues
-from .food import FoodSpec, FoodValues, DummyFoodValues
+from .position import Position, Position3d, RobotLocation
+from .robot import RobotIDs, BatchedRobotIDs, RobotSpec, BatchedRobots
+from .food import FoodSpec, FoodIDs, BatchedFoodIDs, BatchedFood
 from .optimization import CalculationState, Individual, EvaluationFunction
 from .pheromone import Material, ETHANOL
 from .communication import PacketType, Packet, CommunicationResult, SocketState
 from .utils import SavedIndividual, Rec, IndividualRecorder
+from .jaxable import JaxableController
 
 __all__ = [
     "Position",
     "Position3d",
     "RobotLocation",
+    "RobotIDs",
+    "BatchedRobotIDs",
     "RobotSpec",
-    "RobotValues",
+    "BatchedRobots",
     "FoodSpec",
-    "FoodValues",
-    "DummyFoodValues",
+    "FoodIDs",
+    "BatchedFoodIDs",
+    "BatchedFood",
     "CalculationState",
     "Individual",
     "EvaluationFunction",
@@ -27,4 +31,5 @@ __all__ = [
     "IndividualRecorder",
     "Material",
     "ETHANOL",
+    "JaxableController",
 ]
--- framework/types/position.py
+++ framework/types/position.py
@@ -15,3 +15,18 @@ class Position3d:
 
     def to_tuple(self) -> tuple[float, float, float]:
         return self.x, self.y, self.z
+
+
+@dataclasses.dataclass
+class RobotLocation:
+    x: float
+    y: float
+    angle: float
+    
+    @property
+    def position(self) -> Position:
+        return Position(self.x, self.y)
+    
+    @property
+    def theta(self) -> float:
+        return self.angle
