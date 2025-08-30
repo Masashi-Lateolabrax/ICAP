c0581a74d9417dbc7d7a0fafd1adcc0ccf37d745
masashi <masashi.lateolabrax@gmail.com>
2025-08-30 15:15:53 +0900

    feat: Update BatchedFood to use JAX-compatible dataclass and improve data handling

--- framework/types/food.py
+++ framework/types/food.py
@@ -41,7 +41,7 @@ class BatchedFoodIDs:
 
     @classmethod
     def from_specs(cls, model: mujoco.MjModel | mjx.Model, specs: list[FoodSpec]) -> 'BatchedFoodIDs':
-        body_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_SITE, spec.body.name) for spec in specs]
+        body_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_BODY, spec.body.name) for spec in specs]
         center_site_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_SITE, spec.center_site.name) for spec in specs]
         free_joint_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_JOINT, spec.free_joint.name) for spec in specs]
         velocimeter_ids = [mjx.name2id(model, mujoco.mjtObj.mjOBJ_SENSOR, spec.velocimeter.name) for spec in specs]
@@ -62,83 +62,127 @@ class BatchedFoodIDs:
         )
 
 
+@jax_dataclass
 class BatchedFood:
-    @staticmethod
-    def __extract_from_data(
-            data: mjx.Data,
-            batched_food_ids: BatchedFoodIDs
-    ):
-        return data.site_xpos[batched_food_ids.center_site_ids, :2]
+    ids: BatchedFoodIDs
+    positions: jax.Array
+    dummy_positions: jax.Array
 
-    def __init__(
-            self,
+    @classmethod
+    def new(
+            cls,
             data: mujoco.MjData | mjx.Data,
             batched_food_ids: BatchedFoodIDs,
     ):
-        self.ids = batched_food_ids
-        self.positions = data.site_xpos[batched_food_ids.center_site_ids, :2]
-        self.dummy_positions = jnp.zeros((0, 2), dtype=jnp.float32)
-
-        self._extract_from_data = partial(
-            BatchedFood.__extract_from_data,
-            batched_food_ids=batched_food_ids
+        this = cls.__new__(cls)
+        this = this.replace(
+            ids=batched_food_ids,
+            dummy_positions=jnp.zeros((0, 3), dtype=jnp.float32)
         )
-        self._jit_extract_from_data = jax.jit(self._extract_from_data)
+        this = this.update(data)
+        return this
 
     @property
     def positions_with_dummies(self):
         return jnp.vstack([self.positions, self.dummy_positions])
 
-    def register_dummy_position(self, position: jax.Array):
-        self.dummy_positions = jnp.vstack([self.dummy_positions, position])
+    def register_dummy_position(self, position: jax.Array) -> "BatchedFood":
+        return self.replace(
+            dummy_positions=jnp.vstack([self.dummy_positions, position]),
+        )
 
-    def update(self, data: mjx.Data | mujoco.MjData):
-        if isinstance(data, mjx.Data):
-            self.positions = self._jit_extract_from_data(data)
-
-        elif isinstance(data, mujoco.MjData):
-            self.positions = self._extract_from_data(data)
+    def update(self, data: mjx.Data | mujoco.MjData) -> "BatchedFood":
+        new_positions = data.site_xpos[self.ids.center_site_ids, :3]
+        return self.replace(positions=new_positions)
 
     @staticmethod
     def set_pos(data: mujoco.MjData | mjx.Data, body_id: jax.Array, pos: jax.Array):
         if isinstance(data, mjx.Data):
-            new_xpos = data.xpos.at[body_id, :2].set(pos)
+            new_xpos = data.xpos.at[body_id, :2].set(pos[:2])
             data = data.replace(xpos=new_xpos)
 
         elif isinstance(data, mujoco.MjData):
-            data.xpos[body_id] = np.array(pos)
+            data.xpos[body_id, :2] = np.array(pos[:2])
 
         return data
 
-    def tree_flatten(self):
-        leaves = (
-            self.positions,
-            self.dummy_positions
-        )
-        aux_data = {
-            'ids': self.ids,
-            'extract_from_data': self._extract_from_data,
-            'jit_extract_from_data': self._jit_extract_from_data
-        }
-        return leaves, aux_data
-
-    @classmethod
-    def tree_unflatten(cls, aux_data, children):
-        positions, dummy_positions = children
-
-        instance = cls.__new__(cls)
-        instance.ids = aux_data['ids']
-        instance.positions = positions
-        instance.dummy_positions = dummy_positions
-        instance._extract_from_data = aux_data['extract_from_data']
-        instance._jit_extract_from_data = aux_data['jit_extract_from_data']
-
-        return instance
-
-
-# Register BatchedFood as JAX PyTree
-jax.tree_util.register_pytree_node(
-    BatchedFood,
-    BatchedFood.tree_flatten,
-    BatchedFood.tree_unflatten
-)
+    # class BatchedFood:
+    #     @staticmethod
+    #     def __extract_from_data(
+    #             data: mjx.Data,
+    #             batched_food_ids: BatchedFoodIDs
+    #     ):
+    #         return data.site_xpos[batched_food_ids.center_site_ids, :2]
+    #
+    #     def __init__(
+    #             self,
+    #             data: mujoco.MjData | mjx.Data,
+    #             batched_food_ids: BatchedFoodIDs,
+    #     ):
+    #         self.ids = batched_food_ids
+    #         self.positions = data.site_xpos[batched_food_ids.center_site_ids, :2]
+    #         self.dummy_positions = jnp.zeros((0, 2), dtype=jnp.float32)
+    #
+    #         self._extract_from_data = partial(
+    #             BatchedFood.__extract_from_data,
+    #             batched_food_ids=batched_food_ids
+    #         )
+    #         self._jit_extract_from_data = jax.jit(self._extract_from_data)
+    #
+    #     @property
+    #     def positions_with_dummies(self):
+    #         return jnp.vstack([self.positions, self.dummy_positions])
+    #
+    #     def register_dummy_position(self, position: jax.Array):
+    #         self.dummy_positions = jnp.vstack([self.dummy_positions, position])
+    #
+    #     def update(self, data: mjx.Data | mujoco.MjData):
+    #         if isinstance(data, mjx.Data):
+    #             self.positions = self._jit_extract_from_data(data)
+    #
+    #         elif isinstance(data, mujoco.MjData):
+    #             self.positions = self._extract_from_data(data)
+    #
+    #     @staticmethod
+    #     def set_pos(data: mujoco.MjData | mjx.Data, body_id: jax.Array, pos: jax.Array):
+    #         if isinstance(data, mjx.Data):
+    #             new_xpos = data.xpos.at[body_id, :2].set(pos)
+    #             data = data.replace(xpos=new_xpos)
+    #
+    #         elif isinstance(data, mujoco.MjData):
+    #             data.xpos[body_id] = np.array(pos)
+    #
+    #         return data
+    #
+    #     def tree_flatten(self):
+    #         leaves = (
+    #             self.positions,
+    #             self.dummy_positions
+    #         )
+    #         aux_data = {
+    #             'ids': self.ids,
+    #             'extract_from_data': self._extract_from_data,
+    #             'jit_extract_from_data': self._jit_extract_from_data
+    #         }
+    #         return leaves, aux_data
+    #
+    #     @classmethod
+    #     def tree_unflatten(cls, aux_data, children):
+    #         positions, dummy_positions = children
+    #
+    #         instance = cls.__new__(cls)
+    #         instance.ids = aux_data['ids']
+    #         instance.positions = positions
+    #         instance.dummy_positions = dummy_positions
+    #         instance._extract_from_data = aux_data['extract_from_data']
+    #         instance._jit_extract_from_data = aux_data['jit_extract_from_data']
+    #
+    #         return instance
+    #
+    #
+    # # Register BatchedFood as JAX PyTree
+    # jax.tree_util.register_pytree_node(
+    #     BatchedFood,
+    #     BatchedFood.tree_flatten,
+    #     BatchedFood.tree_unflatten
+    # )
