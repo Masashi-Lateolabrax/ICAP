41696e3f71f9f6789be634e3b70916ecf57e7e72
masashi <masashi.lateolabrax@gmail.com>
2025-09-03 19:05:53 +0900

    feat: Remove unused imports from __init__.py for cleaner code

--- framework/backends/__init__.py
+++ framework/backends/__init__.py
@@ -1,5 +1,3 @@
-from .basic import BasicMuJoCoSimulator
-from .basic_environment import BasicMuJoCoSimulatorWithEnv
 from .simple_anime_for_test import SimpleAnimatedBackend
 
 from .basic import BasicSimulator
