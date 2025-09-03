4b976aaa37c33744ebcf4a482f8d2539bc097a60
masashi <masashi.lateolabrax@gmail.com>
2025-09-03 18:29:44 +0900

    feat: Update Controller class to modify activation functions

--- src/config/simulator.py
+++ src/config/simulator.py
@@ -18,28 +18,32 @@ class Controller(nnx.Module):
         parameter = ParaStock(parameter)
 
         self.layer1 = nnx.Linear(
-            in_features=8,
+            in_features=16,
             out_features=8,
-            kernel_init=parameter.gen_initializer(8 * 8),
+            kernel_init=parameter.gen_initializer(16 * 8),
             bias_init=parameter.gen_initializer(8),
             rngs=rngs,
         )
         self.layer2 = nnx.Linear(
             in_features=8,
-            out_features=2,
-            kernel_init=parameter.gen_initializer(8 * 2),
-            bias_init=parameter.gen_initializer(2),
+            out_features=3,
+            kernel_init=parameter.gen_initializer(8 * 3),
+            bias_init=parameter.gen_initializer(3),
             rngs=rngs,
         )
 
     def __call__(self, x: jax.Array) -> jax.Array:
-        x = nnx.relu(self.layer1(x))
-        x = jnp.clip(self.layer2(x), -0.3, 1.0)
+        x = nnx.swish(self.layer1(x))
+        x = self.layer2(x)
+        act = jnp.clip(x[0:2], -0.3, 1.0)
+        phe = nnx.sigmoid(x[2])
+        x = x.at[0:2].set(act)
+        x = x.at[2].set(phe)
         return x
 
     @staticmethod
     def dim():
-        return (8 * 8 + 8) + (8 * 2 + 2)
+        return (16 * 8 + 8) + (8 * 3 + 3)
 
 
 @jax_dataclass
