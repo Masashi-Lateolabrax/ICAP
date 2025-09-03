c67fc3c0d6dbe9fc363b37d7f178f1721c11accd
masashi <masashi.lateolabrax@gmail.com>
2025-09-03 18:32:08 +0900

    feat: Move Controller class definition from simulator.py to controller.py

--- src/config/controller.py
+++ src/config/controller.py
@@ -2,47 +2,38 @@ import jax
 import jax.numpy as jnp
 from flax import nnx
 
-from framework.prelude import *
-
-modules = [
-    (nnx.Linear, {"in_features": 6, "out_features": 3}, {"kernel_init": 6 * 3, "bias_init": 6}),
-    (nnx.Linear, {"in_features": 3, "out_features": 2}, {"kernel_init": 3 * 2, "bias_init": 2}),
-]
+from framework.utils import ParaStock
 
 
-def gen_module(layer_index: int, parameters: Individual, offset: int, rngs: nnx.Rngs):
-    module = modules[layer_index][0]
-    keywags = modules[layer_index][1]
+class Controller(nnx.Module):
+    def __init__(self, parameter: jax.Array):
+        rngs = nnx.Rngs(0)
+        parameter = ParaStock(parameter)
 
-    divided_parameters = {}
-    for name, num_parms in modules[layer_index][2].items():
-        start_idx = offset
-        end_idx = start_idx + num_parms
-        divided_parameters[name] = jnp.array(parameters[start_idx:end_idx])
-        offset += num_parms
+        self.layer1 = nnx.Linear(
+            in_features=16,
+            out_features=8,
+            kernel_init=parameter.gen_initializer(16 * 8),
+            bias_init=parameter.gen_initializer(8),
+            rngs=rngs,
+        )
+        self.layer2 = nnx.Linear(
+            in_features=8,
+            out_features=3,
+            kernel_init=parameter.gen_initializer(8 * 3),
+            bias_init=parameter.gen_initializer(3),
+            rngs=rngs,
+        )
 
-    inits = {
-        name: lambda key, shape, dtype, p=param: p.reshape(shape).astype(dtype)
-        for name, param in divided_parameters.items()
-    }
-
-    return module(**keywags, **inits, rngs=rngs), offset
-
-
-class Controller(JaxableController):
-    def __init__(self, parameters: Individual):
-        dummy_rngs = nnx.Rngs(params=0)
-        self.dense1, offset = gen_module(0, parameters, 0, dummy_rngs)
-        self.dense2, offset = gen_module(1, parameters, offset, dummy_rngs)
-        self._dim = offset
-
-    def forward(self, x: jax.Array) -> jax.Array:
-        x = self.dense1(x)
-        x = nnx.silu(x)
-        x = self.dense2(x)
-        x = nnx.sigmoid(x)
+    def __call__(self, x: jax.Array) -> jax.Array:
+        x = nnx.swish(self.layer1(x))
+        x = self.layer2(x)
+        act = jnp.clip(x[0:2], -0.3, 1.0)
+        phe = nnx.sigmoid(x[2])
+        x = x.at[0:2].set(act)
+        x = x.at[2].set(phe)
         return x
 
-    @property
-    def dim(self):
-        return self._dim
+    @staticmethod
+    def dim():
+        return (16 * 8 + 8) + (8 * 3 + 3)
--- src/config/simulator.py
+++ src/config/simulator.py
@@ -8,42 +8,9 @@ from flax import nnx
 from flax.struct import dataclass as jax_dataclass
 
 from framework.prelude import *
-from framework.utils import ParaStock
 from framework.backends import BasicSimulatorWithEnv
 
-
-class Controller(nnx.Module):
-    def __init__(self, parameter: jax.Array):
-        rngs = nnx.Rngs(0)
-        parameter = ParaStock(parameter)
-
-        self.layer1 = nnx.Linear(
-            in_features=16,
-            out_features=8,
-            kernel_init=parameter.gen_initializer(16 * 8),
-            bias_init=parameter.gen_initializer(8),
-            rngs=rngs,
-        )
-        self.layer2 = nnx.Linear(
-            in_features=8,
-            out_features=3,
-            kernel_init=parameter.gen_initializer(8 * 3),
-            bias_init=parameter.gen_initializer(3),
-            rngs=rngs,
-        )
-
-    def __call__(self, x: jax.Array) -> jax.Array:
-        x = nnx.swish(self.layer1(x))
-        x = self.layer2(x)
-        act = jnp.clip(x[0:2], -0.3, 1.0)
-        phe = nnx.sigmoid(x[2])
-        x = x.at[0:2].set(act)
-        x = x.at[2].set(phe)
-        return x
-
-    @staticmethod
-    def dim():
-        return (16 * 8 + 8) + (8 * 3 + 3)
+from .controller import Controller
 
 
 @jax_dataclass
