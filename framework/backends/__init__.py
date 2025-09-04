027738374dabddc9bb37c336a60f47de14ae188d
masashi <masashi.lateolabrax@gmail.com>
2025-09-04 17:13:52 +0900

    feat: Import RobotOutputs in __init__.py for enhanced module accessibility

--- framework/backends/__init__.py
+++ framework/backends/__init__.py
@@ -1,5 +1,3 @@
-from .simple_anime_for_test import SimpleAnimatedBackend
-
 from .basic import BasicSimulator
-from .basic_with_env import BasicSimulatorWithEnv
+from .basic_with_env import BasicSimulatorWithEnv, RobotOutputs
 from .basic_with_ctrl import ControllerInterface, SimulatorWithCtrl
