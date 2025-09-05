e1e4c5e055905503b5e340174e6e686046c4bc76
masashi <masashi.lateolabrax@gmail.com>
2025-09-05 14:32:05 +0900

    feat: Add plotting functions for total gas and liquid pheromone over time

--- src/analysis_mod/__init__.py
+++ src/analysis_mod/__init__.py
@@ -5,6 +5,7 @@ from src.analysis_mod.for_individual.run import run, record
 from src.analysis_mod.for_individual.input_anime import input_animation
 from src.analysis_mod.for_individual.plot_input_power import plot_robot_sensor, plot_food_sensor, plot_pheromone_sensor
 from src.analysis_mod.for_individual.plot_output import plot_left_wheel_act, plot_right_wheel_act, plot_pheromone_act
+from src.analysis_mod.for_individual.plot_pheromone import plot_total_gas_pheromone, plot_total_liquid_pheromone
 
 from src.analysis_mod.for_history.collect_loss import collect_loss
 from src.analysis_mod.for_history.plot_bests import plot_fitness
