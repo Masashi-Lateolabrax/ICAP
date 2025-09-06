4a95cc0c2c28f69036e56e81aa505e6cf6b1c3b2
masashi <masashi.lateolabrax@gmail.com>
2025-09-06 19:24:21 +0900

    ai: Refactor Controller class to implement ControllerInterface and enhance input handling

--- src/config/controller.py
+++ src/config/controller.py
@@ -1,12 +1,21 @@
 import jax
 import jax.numpy as jnp
 from flax import nnx
+from typing import Self
 
 from framework.utils import ParaStock
+from framework.backends import ControllerInterface
+from framework.prelude import RobotInputs, RobotOutputs
 
 
-class Controller(nnx.Module):
+class Controller(ControllerInterface):
+    @staticmethod
+    def dim():
+        return (16 * 8 + 8) + (8 * 3 + 3)
+
     def __init__(self, parameter: jax.Array):
+        self.l2 = jnp.linalg.norm(parameter)
+
         rngs = nnx.Rngs(0)
         parameter = ParaStock(parameter)
 
@@ -28,12 +37,19 @@ class Controller(nnx.Module):
     def __call__(self, x: jax.Array) -> jax.Array:
         x = nnx.swish(self.layer1(x))
         x = self.layer2(x)
-        act = jnp.clip(x[0:2], -0.3, 1.0)
-        phe = nnx.sigmoid(x[2])
+        act = jnp.clip(x[:, 0:2], -0.3, 1.0)
+        phe = nnx.sigmoid(x[:, 2])
         x = x.at[0:2].set(act)
         x = x.at[2].set(phe)
         return x
 
-    @staticmethod
-    def dim():
-        return (16 * 8 + 8) + (8 * 3 + 3)
+    def forward(self, x: RobotInputs) -> RobotOutputs:
+        x = self.__call__(x.ray)
+        return RobotOutputs(
+            left_wheel=x[:, 0],
+            right_wheel=x[:, 1],
+            pheromone=x[:, 2],
+        )
+
+    def reset(self) -> Self:
+        return self
