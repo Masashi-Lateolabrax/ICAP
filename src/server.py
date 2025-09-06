898cc34ea4ae18b1a30cacc06131c4a7323b5077
masashi <masashi.lateolabrax@gmail.com>
2025-09-06 17:28:10 +0900

    feat: Rename print_info to print_and_save for clarity in server.py

--- src/server.py
+++ src/server.py
@@ -161,7 +161,7 @@ def optimization(port: int, timeout: int, settings: Settings):
         fitness: list[tuple[np.ndarray, float]] = [(task.parameter, task.result) for task in completed_tasks]
         cmaes.tell(fitness)
 
-        print_info(settings, i, cmaes, completed_tasks)
+        print_and_save(settings, i, cmaes, completed_tasks)
 
 
 def main(settings: Settings):
