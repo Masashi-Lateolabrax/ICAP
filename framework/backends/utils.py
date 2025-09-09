958c3228a819f8c108117f7cffad335c6e211d8f
masashi <masashi.lateolabrax@gmail.com>
2025-09-09 15:05:55 +0900

    Refactor emit_rays_static to replace fori_loop with a standard loop for improved readability

--- framework/backends/utils.py
+++ framework/backends/utils.py
@@ -81,23 +81,6 @@ def _emit_n_rays(
     return dists, ids
 
 
-@partial(jax.jit, static_argnames=["i", "body_ids", "num_rays"], inline=True)
-def _body_fn_in_emit_rays_static(
-        i, carry,
-        model, data, positions, xdirections, body_ids, num_rays
-):
-    dists_list, ids_list = carry
-    body_id = body_ids[i]
-    result = _emit_n_rays(
-        model, data,
-        positions[i], xdirections[i],
-        body_id, num_rays
-    )
-    dists_list = dists_list.at[i].set(result[0])
-    ids_list = ids_list.at[i].set(result[1])
-    return dists_list, ids_list
-
-
 @partial(jax.jit, static_argnames=["body_ids", "num_rays"], inline=True)
 def emit_rays_static(
         model: mjx.Model,
@@ -107,24 +90,14 @@ def emit_rays_static(
         body_ids: tuple[int, ...],
         num_rays: int
 ) -> tuple[jax.Array, jax.Array]:  # shape (num_robots, num_rays), (num_robots, num_rays)
-    body_fn = partial(
-        _body_fn_in_emit_rays_static,
-        model=model, data=data,
-        positions=positions, xdirections=xdirections,
-        body_ids=body_ids, num_rays=num_rays
-    )
-
-    num_robots = len(body_ids)
-    init_dists = jnp.zeros((num_robots, num_rays))
-    init_ids = jnp.zeros((num_robots, num_rays), dtype=jnp.int32)
-
-    dists, ids = jax.lax.fori_loop(
-        0,
-        num_robots,
-        body_fn,
-        (init_dists, init_ids),
-        unroll=True
-    )
+    dists = jnp.zeros((len(body_ids), num_rays), dtype=jnp.float32)
+    ids = jnp.zeros((len(body_ids), num_rays), dtype=jnp.int32)
+    for i, body_id in enumerate(body_ids):
+        dists[i], ids[i] = _emit_n_rays(
+            model, data,
+            positions[i], xdirections[i],
+            body_id, num_rays
+        )
     return dists, ids
 
 
