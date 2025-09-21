56fbf2e374bbe8c91e7e9abf34238b82b2dcc48f
masashi <masashi.lateolabrax@gmail.com>
2025-09-22 02:09:05 +0900

    mod: Replace update method with step for pheromone field in simulator.py

--- src/config/simulator.py
+++ src/config/simulator.py
@@ -127,7 +127,7 @@ class Simulator(BasicSimulator):
         if self._pheromone_field is not None:
             self.add_pheromone(robot_positions, self.output_ndarray[:, 2])
             self._pheromone_field.add_liquid_by_cell(self._pheromone_cells)
-            self._pheromone_field.update(self.settings.Simulation.TIME_STEP)
+            self._pheromone_field.step(self.settings.Simulation.TIME_STEP)
 
             max_pheromone = self._pheromone_field.get_max_value()
             self._max_pheromone = max(self._max_pheromone, max_pheromone)
