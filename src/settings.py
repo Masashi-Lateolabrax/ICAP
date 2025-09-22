5be40754b67448f98f35a0951ca3338ac1c7a533
masashi <masashi.lateolabrax@gmail.com>
2025-09-22 13:52:26 +0900

    set: Increase iterations per step for pheromone settings from 1 to 8

--- src/settings.py
+++ src/settings.py
@@ -147,7 +147,7 @@ MySettings.Pheromone.CELL_SIZE = 0.1
 MySettings.Pheromone.WIDTH_NUM = int(MySettings.Simulation.WORLD_WIDTH / MySettings.Pheromone.CELL_SIZE)
 MySettings.Pheromone.HEIGHT_NUM = int(MySettings.Simulation.WORLD_HEIGHT / MySettings.Pheromone.CELL_SIZE)
 
-MySettings.Pheromone.ITERATIONS_PER_STEP = 1
+MySettings.Pheromone.ITERATIONS_PER_STEP = 8
 
 MySettings.Pheromone.TEMPERATURE = 300
 MySettings.Pheromone.MATERIAL = ETHANOL
