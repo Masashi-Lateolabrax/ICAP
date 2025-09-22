fecae60e9dd53fb87940db3fc9d2adc6ec2bcb92
masashi <masashi.lateolabrax@gmail.com>
2025-09-22 20:12:45 +0900

    feat: Implement asynchronous task packet system for inter-process communication

---
+++ framework/ cluster/packets.py
@@ -0,0 +1,49 @@
+import asyncio
+import enum
+
+
+class AsyncTaskPacketType(enum.Enum):
+    STOP = 1
+
+
+class AsyncTaskPacket:
+    def __init__(self, type_: AsyncTaskPacketType, content):
+        self.type: AsyncTaskPacketType = type_
+        self.content = content
+
+
+class AsyncTaskTunnelChild:
+    def __init__(self, parent_queue: asyncio.Queue, child_queue: asyncio.Queue):
+        self.parent_queue = parent_queue
+        self.child_queue = child_queue
+
+    async def send_to_parent(self, packet: AsyncTaskPacket):
+        if not isinstance(packet, AsyncTaskPacket):
+            raise TypeError("packet must be an instance of AsyncTaskPacket")
+        await self.child_queue.put(packet)
+
+    async def receive_from_parent(self) -> AsyncTaskPacket:
+        data = await self.parent_queue.get()
+        if not isinstance(data, AsyncTaskPacket):
+            raise TypeError("data must be an instance of AsyncTaskPacket")
+        return data
+
+
+class AsyncTaskTunnel:
+    def __init__(self):
+        self.parent_queue = asyncio.Queue()
+        self.child_queue = asyncio.Queue()
+
+    def spawn_child(self):
+        return AsyncTaskTunnelChild(self.child_queue, self.parent_queue)
+
+    async def send_to_child(self, packet: AsyncTaskPacket):
+        if not isinstance(packet, AsyncTaskPacket):
+            raise TypeError("packet must be an instance of AsyncTaskPacket")
+        await self.parent_queue.put(packet)
+
+    async def receive_from_child(self) -> AsyncTaskPacket:
+        data = await self.child_queue.get()
+        if not isinstance(data, AsyncTaskPacket):
+            raise TypeError("data must be an instance of AsyncTaskPacket")
+        return data
