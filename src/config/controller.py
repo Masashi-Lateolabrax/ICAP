3231be01308187495d95ef298a634983b72d0684
masashi <masashi.lateolabrax@gmail.com>
2025-09-22 12:57:53 +0900

    mod: Update neural network architecture in Controller for improved performance

--- src/config/controller.py
+++ src/config/controller.py
@@ -8,13 +8,13 @@ class Controller(torch.nn.Module):
         super(Controller, self).__init__()
 
         self.sequential = torch.nn.Sequential(
-            torch.nn.Linear(7, 12),
+            torch.nn.Linear(9, 18),
             torch.nn.Mish(),
-            torch.nn.Linear(12, 12),
+            torch.nn.Linear(18, 18),
             torch.nn.Mish(),
-            torch.nn.Linear(12, 6),
+            torch.nn.Linear(18, 9),
             torch.nn.Mish(),
-            torch.nn.Linear(6, 3),
+            torch.nn.Linear(9, 3),
         )
 
         if parameters is not None:
