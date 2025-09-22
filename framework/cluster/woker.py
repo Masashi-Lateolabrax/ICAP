bdf0938a4f57c2356130282e10ed9b6bab9241d5
masashi <masashi.lateolabrax@gmail.com>
2025-09-23 00:53:49 +0900

    feat: Add send_worker_result method for sending processed results as packets

--- framework/cluster/woker.py
+++ framework/cluster/woker.py
@@ -2,6 +2,8 @@ import asyncio
 import uuid
 from typing import Optional
 
+import numpy as np
+
 from .packets import (
     AsyncTaskTunnel, AsyncTaskTunnelChild, AsyncTaskPacket, AsyncTaskPacketType, WorkerPacket
 )
@@ -53,5 +55,10 @@ class WorkerClient:
 
     async def send_worker_state(self, gpu_usage: float, working: bool):
         packet = WorkerPacket.state_packet(gpu_usage, working)
+        packet = AsyncTaskPacket.worker_packet(packet)
+        await self.tunnel.send(self._child_id, packet)
+
+    async def send_worker_result(self, result: list[tuple[np.ndarray, float]]):
+        packet = WorkerPacket.result_packet(result)
         packet = AsyncTaskPacket.worker_packet(packet)
         await self.tunnel.send(self._child_id, packet)
