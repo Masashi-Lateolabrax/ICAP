1039897939625c798685ff4223232ffe7782ce34
masashi <masashi.lateolabrax@gmail.com>
2025-09-22 20:53:28 +0900

    feat: Refactor head server to use head_routine for improved control flow

--- framework/ cluster/head.py
+++ framework/ cluster/head.py
@@ -1,22 +1,32 @@
 import asyncio
+from functools import partial
 
 from .packets import AsyncTaskPacketType, AsyncTaskPacket, AsyncTaskTunnel, AsyncTaskTunnelChild
 from .woker import Worker
 
 
-async def head_server(address, port, tunnel: AsyncTaskTunnelChild):
-    while True:
+async def head_routine(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, tunnel: AsyncTaskTunnelChild):
+    running = True
+    while running:
         packet: AsyncTaskPacket = await tunnel.receive()
 
         if packet.type == AsyncTaskPacketType.STOP:
-            break
+            running = False
 
 
 class Head:
-    def __init__(self, address, port):
+    def __init__(self):
         self.workers: list[Worker] = []
         self.tunnel = AsyncTaskTunnel()
-        self.task = asyncio.create_task(head_server(address, port, self.tunnel.spawn_child()))
+        self.server = None
+
+    async def start(self, address, port):
+        if self.server is not None:
+            raise RuntimeError("Server is already running")
+        self.server = await asyncio.start_server(
+            partial(head_routine, tunnel=self.tunnel.spawn_child()),
+            address, port
+        )
 
     async def stop(self):
         await self.tunnel.send(AsyncTaskPacket.stop_packet())
