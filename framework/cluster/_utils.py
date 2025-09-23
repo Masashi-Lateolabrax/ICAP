bfc5b5103c4e6dfffedb82553ca5ae9680f991c7
masashi <masashi.lateolabrax@gmail.com>
2025-09-23 14:42:50 +0900

    feat: Update import statement in _utils.py to reference _network module

--- framework/cluster/_utils.py
+++ framework/cluster/_utils.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from ._packets import AsyncTaskTunnelChild, AsyncTaskPacket, AsyncTaskPacketType, WorkerPacket
+from ._network import AsyncTaskTunnelChild, AsyncTaskPacket, AsyncTaskPacketType, WorkerPacket
 
 
 async def send_payload(writer: asyncio.StreamWriter, payload: WorkerPacket):
