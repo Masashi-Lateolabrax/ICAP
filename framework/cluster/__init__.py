13a7441cddca1570a1d3d02ba9dc1341f99cc2e8
masashi <masashi.lateolabrax@gmail.com>
2025-09-23 19:52:28 +0900

    feat: Import WorkerPacket and WorkerPacketType in initialization

--- framework/cluster/__init__.py
+++ framework/cluster/__init__.py
@@ -1,3 +1,3 @@
-from ._network import ResultContent, TaskContent
+from ._network import WorkerPacket, WorkerPacketType, ResultContent, TaskContent
 from ._head import Head
 from ._worker import WorkerClient
