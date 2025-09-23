d1e1085cf409e1279a45d74dcedd4acd19b9009f
masashi <masashi.lateolabrax@gmail.com>
2025-09-23 15:55:14 +0900

    feat: Add ManagedTunnel import to __init__.py for enhanced tunnel management

--- framework/cluster/_network/__init__.py
+++ framework/cluster/_network/__init__.py
@@ -1,3 +1,4 @@
 from ._contents import PingContent, ResultContent, StateContent, TaskContent
 from ._packets import AsyncTaskPacket, AsyncTaskPacketType, WorkerPacket, WorkerPacketType
 from ._tunnel import AsyncTaskTunnelChild, AsyncTaskTunnel
+from ._managed import ManagedTunnel
