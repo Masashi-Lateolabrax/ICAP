3499538f6f4277a311093cd95a5f5e4aa75f9a46
masashi <masashi.lateolabrax@gmail.com>
2025-09-23 16:35:34 +0900

    feat: Update imports in __init__.py to include SingleAsyncTaskTunnel for enhanced task management

--- framework/cluster/_network/__init__.py
+++ framework/cluster/_network/__init__.py
@@ -1,4 +1,4 @@
 from ._contents import PingContent, ResultContent, StateContent, TaskContent
 from ._packets import AsyncTaskPacket, AsyncTaskPacketType, WorkerPacket, WorkerPacketType
-from ._tunnel import AsyncTaskTunnelChild, AsyncTaskTunnel
+from ._tunnel import SingleAsyncTaskTunnel, AsyncTaskTunnel, AsyncTaskTunnelChild
 from ._managed import ManagedTunnel
