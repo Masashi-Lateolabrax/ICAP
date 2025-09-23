14c587fc1f3fe4cc4d043d9376af5517f5dac3dd
masashi <masashi.lateolabrax@gmail.com>
2025-09-23 14:16:33 +0900

    feat: Add content classes for task, ping, state, and result

---
+++ framework/cluster/_contents.py
@@ -0,0 +1,31 @@
+import dataclasses
+import uuid
+import datetime
+from typing import Optional
+
+import numpy as np
+
+
+class TaskContent:
+    def __init__(self, parameter: np.ndarray):
+        self.parameter = parameter
+
+
+@dataclasses.dataclass
+class PingContent:
+    def __init__(self, id_: uuid.UUID):
+        self.id = id_
+        self.create_time = datetime.datetime.now(tz=datetime.UTC)
+        self.response_time: Optional[datetime.datetime] = None
+
+
+@dataclasses.dataclass
+class StateContent:
+    gpu_usage: float
+    working: bool
+
+
+@dataclasses.dataclass
+class ResultContent:
+    result: list[tuple[np.ndarray, float]]
+    rejected: bool = False
