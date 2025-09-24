c85aca9718bd0e8f1f849589e1fa23abcc0c9ba4
masashi <masashi.lateolabrax@gmail.com>
2025-09-24 13:22:03 +0900

    feat: Update worker state retrieval to use asynchronous call

--- examples/optimization/server.py
+++ examples/optimization/server.py
@@ -47,7 +47,7 @@ async def main():
             all_worker_ids = set(await head.get_ids())
             waiting_ids = []
             for i in all_worker_ids:
-                res = head.get_worker_state(i)
+                res = await head.get_worker_state(i)
                 if res is not None and not res.working:
                     waiting_ids.append(i)
             worker_ids = set(waiting_ids)
