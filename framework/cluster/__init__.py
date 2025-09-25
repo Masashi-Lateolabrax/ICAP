d636b4b1679795f7dc0f57a6f48e4405ed233db5
masashi <masashi.lateolabrax@gmail.com>
2025-09-25 19:06:29 +0900

    feat: Refactor imports to use Server and Client for improved clarity

--- framework/cluster/__init__.py
+++ framework/cluster/__init__.py
@@ -1,4 +1,3 @@
-from ._network import WorkerPacket, WorkerPacketType, ResultContent, TaskContent
-from ._head import Head
-from ._worker import WorkerClient
 from ._load_balancer import LoadBalancer, Performance
+from .server import Server
+from .client import Client
