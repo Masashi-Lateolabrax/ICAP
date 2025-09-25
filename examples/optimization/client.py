ccd3b33fa9a00fb61051d8ea5a60654433552b7b
masashi <masashi.lateolabrax@gmail.com>
2025-09-26 02:42:26 +0900

    feat: Increase default max-batch-size to improve task evaluation efficiency

--- examples/optimization/client.py
+++ examples/optimization/client.py
@@ -126,7 +126,7 @@ async def main():
     parser = argparse.ArgumentParser(description="ICAP Optimization Client")
     parser.add_argument("--host", default="localhost", type=str, help="Server host address")
     parser.add_argument("--port", default=50000, type=int, help="Server port number")
-    parser.add_argument("--max-batch-size", type=int, default=1, help="Number of tasks to evaluate in batch")
+    parser.add_argument("--max-batch-size", type=int, default=8, help="Number of tasks to evaluate in batch")
     parser.add_argument("--net-timeout", type=int, default=1, help="TODO")
     parser.add_argument("--heartbeat-interval", type=int, default=5, help="Interval in seconds to send state updates")
     parser.add_argument(
