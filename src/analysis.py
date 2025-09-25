0127bd5622ff54936943f3cc1f6b92e8275ee792
Kawachon <kouki0302kou@gmail.com>
2025-09-26 04:53:53 +0900

    mod: Rename get_total_gas_pheromone to get_max_gas_pheromone

--- src/analysis.py
+++ src/analysis.py
@@ -31,7 +31,7 @@ class SimulatorForDebugging(analysis_mod.SimulatorForDebugInterface):
             robot_directions=[np.copy(r.xdirection) for r in self.simulator.robot_values],
             food_positions=[np.copy(f.xpos) for f in self.simulator.food_values],
             food_directions=[np.copy(f.direction) for f in self.simulator.food_values],
-            total_gas_pheromone=self.simulator.get_total_gas_pheromone(),
+            total_gas_pheromone=self.simulator.get_max_gas_pheromone(),
             total_liquid_pheromone=self.simulator.get_total_liquid_pheromone()
         ))
 
