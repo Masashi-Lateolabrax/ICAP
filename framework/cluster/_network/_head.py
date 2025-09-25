e60e667a841c8099136e18ad2b64b5323feac652
masashi <masashi.lateolabrax@gmail.com>
2025-09-25 15:35:44 +0900

    feat: Refactor Head class to use Tunnel instances and improve packet handling

---
+++ framework/cluster/_network/_head.py
@@ -0,0 +1,77 @@
+import asyncio
+import uuid
+
+from icecream import ic
+
+from ...prelude import *
+from ._tunnel import Tunnel
+from ._utils import relay_routine
+
+
+async def head_routine(reader: asyncio.StreamReader, writer: asyncio.StreamWriter, tunnel: Tunnel, timeout: float):
+    ic(writer.get_extra_info('peername'))
+    ic(tunnel.id)
+    while await relay_routine(reader, writer, tunnel, timeout):
+        pass
+
+
+class Head:
+    def __init__(self):
+        self.tunnel: dict[uuid.UUID, Tunnel] = {}
+        self.server = None
+
+    async def start(self, address, port, timeout: float):
+        if self.server is not None:
+            raise RuntimeError("Server is already running")
+
+        async def body_fn(reader, writer):
+            parent, child = Tunnel.create()
+            self.tunnel[parent.id] = parent
+            await head_routine(reader, writer, tunnel=child, timeout=timeout)
+
+        self.server = await asyncio.start_server(body_fn, address, port)
+
+    async def stop(self):
+        if self.server is None:
+            raise RuntimeError("Server is not running")
+
+        self.server.close()
+        await self.server.wait_closed()
+        self.server = None
+
+        stop_packet = CoroutinePacket.stop_packet()
+        for t in self.tunnel.values():
+            await t.send(stop_packet)
+        self.tunnel.clear()
+
+    async def delete(self, id_: uuid.UUID):
+        await self.tunnel[id_].send(CoroutinePacket.stop_packet())
+        del self.tunnel[id_]
+
+    async def get_ids(self) -> set[uuid.UUID]:
+        return set(self.tunnel.keys())
+
+    def receive(self) -> dict[uuid.UUID, list[CoroutinePacket]]:
+        received_packets = {}
+        for i, t in self.tunnel.items():
+            if i not in received_packets:
+                received_packets[i] = []
+
+            while True:
+                packet = t.receive()
+
+                if packet is None:
+                    break
+                if packet.type != CoroutinePacketType.CLUSTER_PACKET:
+                    continue
+                if not isinstance(packet.content, CoroutinePacket):
+                    raise ValueError("Invalid packet content. Expected CoroutinePacket.")
+
+                received_packets[i] = packet
+
+        return received_packets
+
+    async def send(self, id_: uuid.UUID, packet: CoroutinePacket):
+        if id_ not in self.tunnel:
+            raise ValueError(f"Tunnel with id {id_} does not exist.")
+        await self.tunnel[id_].send(packet)
