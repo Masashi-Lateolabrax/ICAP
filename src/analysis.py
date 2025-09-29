4e01630ac09818040285820f3c3758951642c1bf
Kawachon <kouki0302kou@gmail.com>
2025-09-30 01:34:52 +0900

    mod: Disable Pheromone Simulation.

--- src/analysis.py
+++ src/analysis.py
@@ -173,7 +173,8 @@ def main(settings: Settings):
     analysis_mod.plot_pheromone_history(pheromone_graph_path, saved_individuals)
 
     # Analyze a specific individual from a specific generation (e.g., generation 0)
-    for g in [0, len(saved_individuals) - 1]:
+    settings.Pheromone.ACTIVE = False
+    for g in [len(saved_individuals) - 1]:
         rec: Rec = saved_individuals[g]
         individual: Individual = rec.best_individual
         analyze_specific_individual(save_dir, settings, individual, seed=None)
