86544a0aaadcab796645d726eabf6b391c5a9176
Kawachon <kouki0302kou@gmail.com>
2025-10-03 21:48:45 +0900

    mod: Implement shadow

--- framework/backends/basic_environment.py
+++ framework/backends/basic_environment.py
@@ -139,6 +139,8 @@ class BasicEnvironment(BasicMuJoCoSimulator, ABC):
         self.robot_specs = robot_specs
         self.food_specs = food_specs
 
+        self._shadow = []
+
         self._pheromone_field: Optional[PheromoneField] = None
         self._pheromone_cells: list[PheromoneFieldCell] = []
         self._pheromone_cell_pos: Optional[np.ndarray] = None
@@ -205,6 +207,19 @@ class BasicEnvironment(BasicMuJoCoSimulator, ABC):
                 cell.set_color(*rgba, 0.5)
 
         super().render(img_buf, pos, lookat)
+
+        while len(self._shadow) > 150:
+            self._shadow.pop(0)
+
+        robot_mask = (img_buf[:, :, 0] > 126) * (img_buf[:, :, 1] > 126) * (img_buf[:, :, 2] < 126)
+        self._shadow.append(img_buf[:, :, :] * robot_mask[:, :, None])
+
+        shadow = np.zeros_like(img_buf)
+        for s in self._shadow:
+            shadow = np.maximum(shadow * 0.99, s)
+
+        img_buf *= np.logical_not(np.sum(shadow, axis=2) > 0)[:, :, None]
+        img_buf += shadow.astype(np.uint8)
 
     def reset(self):
         if self._pheromone_field:
