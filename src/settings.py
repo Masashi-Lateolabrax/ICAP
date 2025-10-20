1b6339f3d59a165334b4665ff568f070884bc10b
masashi <masashi.lateolabrax@gmail.com>
2025-10-20 14:57:54 +0900

    set: Update max pheromone secretion value from 0.001 to 0.005 in settings

--- src/settings.py
+++ src/settings.py
@@ -53,7 +53,7 @@ MySettings.Robot.HEIGHT = 0.1
 MySettings.Robot.RADIUS = 0.175
 MySettings.Robot.DISTANCE_BETWEEN_WHEELS = 0.175 * 2 * 0.8
 MySettings.Robot.MAX_SPEED = 0.8
-MySettings.Robot.MAX_PHEROMONE_SECRETION = 1e-3
+MySettings.Robot.MAX_PHEROMONE_SECRETION = 0.005
 MySettings.Robot.MASS = 10
 
 MySettings.Robot.COLOR = (1, 1, 0, 1)
