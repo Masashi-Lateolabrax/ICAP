82dfed24481ab08950238c71dbe74f3b57e94ee0
masashi <masashi.lateolabrax@gmail.com>
2025-11-04 08:13:36 +0900

    set: Decrease CELL_SIZE from 0.1 to 0.01 to increase the resolution of pheromone simulation.

--- src/settings.py
+++ src/settings.py
@@ -144,7 +144,7 @@ MySettings.Simulation.WALL_HEIGHT = 1
 ###################################################################################
 MySettings.Pheromone.ACTIVE = True
 
-MySettings.Pheromone.CELL_SIZE = 0.1
+MySettings.Pheromone.CELL_SIZE = 0.01
 MySettings.Pheromone.WIDTH_NUM = int(MySettings.Simulation.WORLD_WIDTH / MySettings.Pheromone.CELL_SIZE)
 MySettings.Pheromone.HEIGHT_NUM = int(MySettings.Simulation.WORLD_HEIGHT / MySettings.Pheromone.CELL_SIZE)
 
