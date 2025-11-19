c29b12fcf319db54ba1bb0690dae5698af76688f
masashi <masashi.lateolabrax@gmail.com>
2025-11-19 16:00:18 +0900

    set: Change NN structure from [51,5,5,5,3] to [51,20,5,3]

--- src/config/controller.py
+++ src/config/controller.py
@@ -13,11 +13,9 @@ class Controller(torch.nn.Module):
         # num_actions = self.action_patterns.shape[0]
         # Input: direction(2) + velocity(3) + depth(nray) + pheromone(3)
         self.sequential = torch.nn.Sequential(
-            torch.nn.Linear(2 + 3 + nray + 3, 5),
+            torch.nn.Linear(2 + 3 + nray + 3, 20),
             torch.nn.Mish(),
-            torch.nn.Linear(5, 5),
-            torch.nn.Mish(),
-            torch.nn.Linear(5, 5),
+            torch.nn.Linear(20, 5),
             torch.nn.Mish(),
             torch.nn.Linear(5, 3),  # Output action logits
             # torch.nn.Softmax(dim=-1),  # Convert to probability distribution
