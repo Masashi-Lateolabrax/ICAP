7f7f1f5e1d4f42d5e491a00ecacc29a6abb397b1
masashi <masashi.lateolabrax@gmail.com>
2025-11-22 15:13:02 +0900

    set: Refine CELL_SIZE_Z from 0.1 to 0.001

--- src/settings.py
+++ src/settings.py
@@ -145,7 +145,7 @@ MySettings.Simulation.GRAVITY = (0.0, 0.0, -9.81)
 MySettings.Pheromone.ACTIVE = True
 
 MySettings.Pheromone.CELL_SIZE = 0.1
-MySettings.Pheromone.CELL_SIZE_Z = 0.1
+MySettings.Pheromone.CELL_SIZE_Z = 0.001
 MySettings.Pheromone.NZ = 7
 MySettings.Pheromone.WIDTH_NUM = int(MySettings.Simulation.WORLD_WIDTH / MySettings.Pheromone.CELL_SIZE)
 MySettings.Pheromone.HEIGHT_NUM = int(MySettings.Simulation.WORLD_HEIGHT / MySettings.Pheromone.CELL_SIZE)
