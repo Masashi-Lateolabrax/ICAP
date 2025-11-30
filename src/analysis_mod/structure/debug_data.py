443f62bd2f39b159277359b33a10415d0c4a5b96
masashi <masashi.lateolabrax@gmail.com>
2025-11-30 20:55:39 +0900

    add: Add DebugData.load() class method for loading debug data from pickle files

--- src/analysis_mod/structure/debug_data.py
+++ src/analysis_mod/structure/debug_data.py
@@ -1,4 +1,6 @@
 import dataclasses
+import pickle
+from pathlib import Path
 
 import numpy as np
 
@@ -14,3 +16,12 @@ class DebugData:
     food_directions: list[np.ndarray]
     total_gas_pheromone: float
     total_liquid_pheromone: float
+
+    @classmethod
+    def load(cls, debug_data_path: Path) -> list['DebugData']:
+        """Load list of DebugData from pickle file."""
+        with open(debug_data_path, 'rb') as f:
+            debug_data = pickle.load(f)
+        print(f"Loaded debug data from: {debug_data_path}")
+        print(f"  Total frames: {len(debug_data)}")
+        return debug_data
